01a78eb6aed43c3a18e97911f1568e69388b0abb
NoraBitarSM <Nora.Bitar@SoftMachine.co>
2023-10-11 11:42:39 +0300

    geographic-regions-nora

--- src/repositories/SystemRepository.js
+++ src/repositories/SystemRepository.js
@@ -1,16 +1,21 @@
 const service = 'SY.asmx/'
 
 export const SystemRepository = {
-
-    get2AM: service + 'get2AM',
-    KeyValueStore: service + 'qryKVS',
-    DocumentType: {
-        qry: service + 'qryDT',
-        get: service + 'getDT',
-        set: service + 'setDT',
-        del: service + 'delDT',
-    },
-    NumberRange: {
-        snapshot: service + 'snapshotNRA'
-    }
+  get2AM: service + 'get2AM',
+  KeyValueStore: service + 'qryKVS',
+  DocumentType: {
+    qry: service + 'qryDT',
+    get: service + 'getDT',
+    set: service + 'setDT',
+    del: service + 'delDT'
+  },
+  NumberRange: {
+    snapshot: service + 'snapshotNRA'
+  },
+  GeographicRegion: {
+    qry: service + 'qryRGN',
+    get: service + 'getRGN',
+    set: service + 'setRGN',
+    del: service + 'delRGN'
+  }
 }
