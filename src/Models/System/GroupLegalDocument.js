260221ae96e46a5fa16ac60c35e76aeb64009d61
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2023-10-11 16:19:55 +0300

    GroupLegalDocument

---
+++ src/Models/System/GroupLegalDocument.js
@@ -0,0 +1,28 @@
+// ** Helpers
+import { formatDateFromApi, formatDateToApi } from 'src/lib/date-helper'
+
+// formatDateFromApi("/Date(1695513600000)/")
+
+const getNewGroupLegalDocument = () => {
+  return {
+    groupId: null,
+    incId: null,
+    required: null,
+    mandatory: null,
+    groupName: null,
+    incName: null
+  }
+}
+
+const populateGroupLegalDocument = obj => {
+  return {
+    groupId: obj.groupId,
+    incId: obj.incId,
+    required: obj.required,
+    mandatory: obj.mandatory,
+    groupName: obj.groupName,
+    incName: obj.incName
+  }
+}
+
+export { getNewGroupLegalDocument, populateGroupLegalDocument }
---
+++ src/repositories/BusinessPartnerRepository.js
@@ -0,0 +1,8 @@
+const service = 'BP.asmx/'
+
+export const BusinessPartnerRepository = {
+  qryGIN: service + 'qryGIN',
+  getGIN: service + 'getGIN',
+  setGIN: service + 'setGIN',
+  delGIN: service + 'delGIN'
+}
