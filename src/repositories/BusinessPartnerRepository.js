6531384c8bc54e19212ac870ae79ceafcccc8260
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2023-10-13 15:17:27 +0300

    groupLegalDoc

--- src/repositories/BusinessPartnerRepository.js
+++ src/repositories/BusinessPartnerRepository.js
@@ -1,8 +1,22 @@
 const service = 'BP.asmx/'
 
 export const BusinessPartnerRepository = {
-  qryGIN: service + 'qryGIN',
-  getGIN: service + 'getGIN',
-  setGIN: service + 'setGIN',
-  delGIN: service + 'delGIN'
+  GroupLegalDocument: {
+    qryGIN: service + 'qryGIN',
+    getGIN: service + 'getGIN',
+    setGIN: service + 'setGIN',
+    delGIN: service + 'delGIN'
+  },
+  CategoryID: {
+    qryINC: service + 'qryINC',
+    getINC: service + 'getINC',
+    setINC: service + 'setINC',
+    delINC: service + 'delINC'
+  },
+  Group: {
+    qryGRP: service + 'qryGRP',
+    getGRP: service + 'getGRP',
+    setGRP: service + 'setGRP',
+    delGRP: service + 'delGRP'
+  }
 }
