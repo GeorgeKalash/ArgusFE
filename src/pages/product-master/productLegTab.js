0f35366cdecc044951b11a35d564da94eb7106a6
NoraBitarSM <Nora.Bitar@SoftMachine.co>
2023-10-19 15:06:49 +0300

    productMaster-FieldsTab

--- src/pages/product-master/productLegTab.js
+++ src/pages/product-master/productLegTab.js
@@ -16,27 +16,54 @@ import CustomComboBox from 'src/components/Inputs/CustomComboBox'
 import CustomTabPanel from 'src/components/Shared/CustomTabPanel'
 import Window from 'src/components/Shared/Window'
 
+//** Helpers
+import { getFormattedNumber, validateNumberField, getNumberWithoutCommas } from 'src/lib/numberField-helper'
+
 const productLegTab = () => {
   //states
   const [windowOpen, setWindowOpen] = useState(false)
 
   //stores
   const [gridData, setGridData] = useState([])
+  const [commissionGridData, setCommissionGridData] = useState([])
 
   const columns = [
     {
       field: 'fromAmount',
       headerName: 'From Amount',
       flex: 1
+      // align: 'right',
+      // valueGetter: ({ row }) => getFormattedNumber(row?.fromAmount, 2)
     },
     {
       field: 'toAmount',
       headerName: 'To Amount',
       flex: 1
+      // align: 'right',
+      // valueGetter: ({ row }) => getFormattedNumber(row?.toAmount, 2)
     }
   ]
 
   const commissionColumns = [
+    {
+      field: 'checkBox',
+      headerName: '',
+      flex: 0.5,
+      renderCell: params => (
+        <Checkbox
+          color='primary'
+          checked={params.row.checkBox === true}
+          onChange={() => {
+            params.row.checkBox = !params.row.checkBox
+          }}
+        />
+      )
+    },
+    {
+      field: 'commissionName',
+      headerName: 'Commission Name',
+      flex: 1
+    },
     {
       field: 'commission',
       headerName: 'Commission',
@@ -45,7 +72,7 @@ const productLegTab = () => {
   ]
 
   const getGridData = ({}) => {
-    const newData = { list: [{ recordId: 1, fromAmount: 1000, toAmount: 2000 }] }
+    const newData = { list: [{ recordId: 1, fromAmount: 1000.66, toAmount: 2000.97 }] }
     setGridData({ ...newData })
   }
 
@@ -57,12 +84,12 @@ const productLegTab = () => {
   const fillCommissionStore = () => {
     const newData = {
       list: [
-        { recordId: 1, commission: 50 },
-        { recordId: 2, commission: 100 },
-        { recordId: 3, commission: 150 }
+        { commissionId: 1, commissionName: 'PCT', commission: 50 },
+        { commissionId: 2, commissionName: 'fixed', commission: 100 },
+        { commissionId: 3, commissionName: 'fixed (other charges)', commission: 150 }
       ]
     }
-    setGridData({ ...newData })
+    setCommissionGridData({ ...newData })
   }
 
   useEffect(() => {
@@ -86,10 +113,10 @@ const productLegTab = () => {
               <CustomTextField label='Reference' value={'reference 1'} readOnly={true} />
             </Grid>
             <Grid item xs={12}>
-              <CustomComboBox name='plantName' label='Plant' readOnly={true} required />
+              <CustomComboBox name='plantName' label='Plant' readOnly={false} required />
             </Grid>
             <Grid item xs={12}>
-              <CustomComboBox name='dispursal' label='Dispursal' readOnly={true} required />
+              <CustomComboBox name='dispursal' label='Dispursal' readOnly={false} required />
             </Grid>
           </Grid>
           {/* Second Column */}
@@ -135,18 +162,27 @@ const productLegTab = () => {
                   <Grid item xs={12}>
                     <CustomTextField label='Reference' value={'reference 1'} readOnly={true} />
                   </Grid>
+                  <Grid item xs={12}>
+                    <CustomComboBox name='plantName' label='Plant' readOnly={true} required />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <CustomComboBox name='dispursal' label='Dispursal' readOnly={true} required />
+                  </Grid>
                 </Grid>
                 {/* Second Column */}
                 <Grid ccontainer rowGap={2} xs={6} sx={{ px: 2 }}>
                   <Grid item xs={12}>
                     <CustomTextField label='Name' value={'name 1'} readOnly={true} />
                   </Grid>
+                  <Grid item xs={12}>
+                    <CustomComboBox name='currencyName' label='Currency' required />
+                  </Grid>
                 </Grid>
               </Grid>
               <Table
                 columns={commissionColumns}
-                gridData={gridData}
-                rowId={['recordId']}
+                gridData={commissionGridData}
+                rowId={['commissionId']}
                 api={getGridData}
                 isLoading={false}
                 pagination={false}
