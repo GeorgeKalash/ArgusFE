a21f7b74c012ae716eea8761c46806a9c1c1d01f
MustafaAbboud <mustafaabbassabboud@gmail.com>
2023-12-03 12:00:56 +0200

    ReportViewer based on api data

--- src/components/Shared/ReportViewer.js
+++ src/components/Shared/ReportViewer.js
@@ -1,29 +1,15 @@
 import { useEffect, useRef } from 'react'
 import ko from 'knockout'
 
-// import AsyncExportApproach from 'devexpress-reporting/dx-webdocumentviewer'
-// import ExportFormatID from 'devexpress-reporting/dx-webdocumentviewer'
-
-const ReportViewer = () => {
+const ReportViewer = ({ reportData, reportTemplate }) => {
   const reportUrl = ko.observable('TestReport')
   const previewModel = ko.observable()
-
-  const requestOptions = {
-    host: 'https://localhost:64673/',
-    invokeAction: 'DXXRDV'
-  }
 
   const callbacks = {
     CustomizeExportOptions: function (s, e) {
       e.HideExportOptionsPanel()
-
-      //   const model = e.GetExportOptionsModel(ExportFormatID.XLSX)
       model.documentOptions.author('Me')
     }
-
-    // BeforeRender: function (s, e) {
-    //   AsyncExportApproach(true)
-    // }
   }
 
   const exportDocument = () => {
@@ -33,11 +19,17 @@ const ReportViewer = () => {
   const viewerRef = useRef(null)
 
   useEffect(() => {
+    // Set report data
+    previewModel().Data(reportData)
+
+    // Set report template
+    previewModel().LoadDocument(reportTemplate)
+
+    // Apply bindings
     ko.applyBindings(
       {
         reportUrl: reportUrl,
         viewerModel: previewModel,
-        requestOptions: requestOptions,
         callbacks: callbacks
       },
       viewerRef.current
@@ -46,7 +38,7 @@ const ReportViewer = () => {
     return () => {
       ko.cleanNode(viewerRef.current)
     }
-  }, [])
+  }, [reportData, reportTemplate])
 
   return (
     <div>
@@ -57,60 +49,3 @@ const ReportViewer = () => {
 }
 
 export default ReportViewer
-
-// const ReportViewer = () => {
-//   const reportUrl = ko.observable('');
-//   const previewModel = ko.observable();
-
-//   const callbacks = {
-//     CustomizeExportOptions: function (s, e) {
-//       e.HideExportOptionsPanel();
-//     },
-//   };
-
-//   const viewerRef = useRef(null);
-
-//   useEffect(() => {
-//     const fetchReport = async () => {
-//       try {
-//         const response = await axios.get('/api/reports', {
-//           params: {
-//             reportName: 'TestReport',
-//           },
-//         });
-
-//         const reportData = response.data.ReportData;
-//         previewModel().SetReportData(reportData);
-//         reportUrl(reportData.ReportName);
-//       } catch (error) {
-//         console.error('Error fetching report:', error);
-//       }
-//     };
-
-//     fetchReport();
-//   }, []);
-
-//   useEffect(() => {
-//     ko.applyBindings(
-//       {
-//         reportUrl: reportUrl,
-//         viewerModel: previewModel,
-//         // requestOptions: removed this line
-//         callbacks: callbacks,
-//       },
-//       viewerRef.current
-//     );
-
-//     return () => {
-//       ko.cleanNode(viewerRef.current);
-//     };
-//   }, [reportUrl]);
-
-//   return (
-//     <div>
-//       <div ref={viewerRef} data-bind='dxReportViewer: $data' style={{ width: '100%', height: '900px' }}></div>
-//     </div>
-//   );
-// };
-
-// export default ReportViewer;
