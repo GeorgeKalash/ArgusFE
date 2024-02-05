e0c2d92d2ba72e82a459f5e411daa1c1b64f55d7
Alinadimmokdad <alimokdad961@gmail.com>
2024-02-05 23:28:10 +0200

    change account name

--- src/providers/AuthContext.js
+++ src/providers/AuthContext.js
@@ -67,7 +67,7 @@ const AuthProvider = ({ children }) => {
     const fetchData = async () => {
       const matchHostname = window.location.hostname.match(/^([a-z0-9]+)\.softmachine\.co$/)
 
-      const accountName = matchHostname ? matchHostname[1] : 'anthonys'
+      const accountName = matchHostname ? matchHostname[1] : 'byc'
 
       try {
         const response = await axios({
