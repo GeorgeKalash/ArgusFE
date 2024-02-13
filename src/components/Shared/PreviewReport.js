bbc36159a164ec1220c4c0a22c7c841bcf8dc9c5
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-02-13 09:24:48 +0200

    pdf

--- src/components/Shared/PreviewReport.js
+++ src/components/Shared/PreviewReport.js
@@ -5,10 +5,8 @@ export default function PreviewReport({ pdfURL }) {
   console.log(pdfURL)
 
   return (
-    <div>
-      <Box id='reportContainer' sx={{ flex: 1, display: 'flex', p: 2 }}>
+      <>
         <iframe title={'Preview'} src={pdfURL} width='100%' height='100%' allowFullScreen />
-      </Box>
-    </div>
+      </>
   )
 }
