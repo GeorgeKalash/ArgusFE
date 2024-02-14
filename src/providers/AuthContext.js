adb4ebbd4e084688228c911a024feca82d86afa8
RebeccaFrem02 <155422197+RebeccaFrem02@users.noreply.github.com>
2024-02-14 17:02:26 +0200

    REM-164

--- src/providers/AuthContext.js
+++ src/providers/AuthContext.js
@@ -68,7 +68,7 @@ const AuthProvider = ({ children }) => {
       const matchHostname = window.location.hostname.match(/^(.+)\.softmachine\.co$/)
 
 
-      const accountName = matchHostname ? matchHostname[1] : 'burger'
+      const accountName = matchHostname ? matchHostname[1] : 'byc-deploy'
 
 
       try {
