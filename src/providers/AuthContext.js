e40173f72e616f81d90ec5cda484153f88b0e3e7
Alinadimmokdad <alimokdad961@gmail.com>
2024-02-20 16:41:00 +0200

    change to byc

--- src/providers/AuthContext.js
+++ src/providers/AuthContext.js
@@ -67,7 +67,7 @@ const AuthProvider = ({ children }) => {
     const fetchData = async () => {
       const matchHostname = window.location.hostname.match(/^(.+)\.softmachine\.co$/)
 
-      const accountName = matchHostname ? matchHostname[1] : 'Anthonys'
+      const accountName = matchHostname ? matchHostname[1] : 'byc-deploy'
 
 
       try {
