a7e0d358d18f090b8b8aaefb2498be5f5ce6618e
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2024-03-11 13:12:23 +0200

    refactoring

--- src/pages/outwards-transfer/index.js
+++ src/pages/outwards-transfer/index.js
@@ -29,6 +29,7 @@ import { RemittanceOutwardsRepository } from 'src/repositories/RemittanceOutward
 import ProductsWindow from './Windows/ProductsWindow'
 import { CurrencyTradingClientRepository } from 'src/repositories/CurrencyTradingClientRepository'
 import { RemittanceSettingsRepository } from 'src/repositories/RemittanceRepository'
+import { useInvalidate } from 'src/hooks/resource'
 
 const OutwardsTransfer = () => {
   const { getRequest, postRequest } = useContext(RequestsContext)
@@ -43,23 +44,36 @@ const OutwardsTransfer = () => {
   const [agentsStore, setAgentsStore] = useState([])
   const [productsStore, setProductsStore] = useState([])
   const [correspondentStore, setCorrespondentStore] = useState([])
-  
+
   //states
   const [windowOpen, setWindowOpen] = useState(false)
   const [productsWindowOpen, setProductsWindowOpen] = useState(false)
   const [editMode, setEditMode] = useState(false)
   const [errorMessage, setErrorMessage] = useState(null)
-  const [selectedRow, setSelectedRow] = useState(null);
+  const [selectedRow, setSelectedRow] = useState(null)
 
-  //control
-  const [labels, setLabels] = useState(null)
-  const [access, setAccess] = useState(null)
+  async function fetchGridData(options = {}) {
+    const { _startAt = 0, _pageSize = 50 } = options
 
-  const _labels = {
-    reference: labels && labels.find(item => item.key === "1").value,
-    name: labels && labels.find(item => item.key === "2").value,
-    foreignLanguage: labels && labels.find(item => item.key === "3").value,
+    return await getRequest({
+      extension: SystemRepository.SMSTemplate.page,
+      parameters: `_startAt=${_startAt}&_pageSize=${_pageSize}&filter=`
+    })
   }
+
+  const {
+    query: { data },
+    labels: _labels,
+    access
+  } = useResourceQuery({
+    queryFn: fetchGridData,
+    endpointId: SystemRepository.Currency.qry,
+    datasetId: ResourceIds.Currencies
+  })
+
+  const invalidate = useInvalidate({
+    endpointId: SystemRepository.SMSTemplate.page
+  })
 
   const columns = [
     {
@@ -98,12 +112,9 @@ const OutwardsTransfer = () => {
       amount: yup.string().required('This field is required'),
       productId: yup.string().required('This field is required'),
       fees: yup.string().required('This field is required'),
-      baseAmount: yup.string().required('This field is required'),
-
+      baseAmount: yup.string().required('This field is required')
     }),
-    onSubmit: values => {
-
-    }
+    onSubmit: values => {}
   })
 
   const handleSubmit = () => {
@@ -111,12 +122,12 @@ const OutwardsTransfer = () => {
   }
 
   const handleProductSelection = () => {
-    const selectedRowData = productsStore?.list.find((row) => row.productId === selectedRow);
+    const selectedRowData = productsStore?.list.find(row => row.productId === selectedRow)
     outwardsValidation.setFieldValue('productId', selectedRowData?.productId)
     outwardsValidation.setFieldValue('fees', selectedRowData?.fees)
     outwardsValidation.setFieldValue('baseAmount', selectedRowData?.baseAmount)
     outwardsValidation.setFieldValue('net', selectedRowData?.fees + selectedRowData?.baseAmount)
-    setProductsWindowOpen(false);
+    setProductsWindowOpen(false)
   }
 
   const getGridData = () => {
@@ -132,7 +143,6 @@ const OutwardsTransfer = () => {
     //     setErrorMessage(error)
     //   })
   }
-
 
   const fillCountryStore = () => {
     var parameters = '_filter='
@@ -162,7 +172,7 @@ const OutwardsTransfer = () => {
       })
   }
 
-  const onCountrySelection = (countryId) => {
+  const onCountrySelection = countryId => {
     //get dispersals list
     var parameters = `_countryId=${countryId}`
     getRequest({
@@ -208,93 +218,86 @@ const OutwardsTransfer = () => {
   }
 
   //_type=2&_functionId=1&_plantId=1&_countryId=124&_currencyId=90&_dispersalType=2&_amount=200&_agentId=4
-  const onAmountDataFill = (formFields) => {
-
+  const onAmountDataFill = formFields => {
     //get products list
     // type, functionId, plantId, countryId, dispersalType, currencyId, amount, agentId
-    var type = 2;
-    var functionId = 1;
-    var plant = formFields?.plantId;
+    var type = 2
+    var functionId = 1
+    var plant = formFields?.plantId
     var countryId = formFields?.countryId
     var currencyId = formFields?.currencyId
     var dispersalType = formFields?.dispersalType
-    var agentId = formFields?.agentId ?? 0;
-    var amount = formFields?.amount ?? 0;
+    var agentId = formFields?.agentId ?? 0
+    var amount = formFields?.amount ?? 0
 
-
-    var parameters = `_type=${type}&_functionId=${functionId}&_plantId=${plant}&_countryId=${countryId}&_dispersalType=${dispersalType}&_currencyId=${currencyId}&_agentId=${agentId}&_amount=${amount}` 
+    var parameters = `_type=${type}&_functionId=${functionId}&_plantId=${plant}&_countryId=${countryId}&_dispersalType=${dispersalType}&_currencyId=${currencyId}&_agentId=${agentId}&_amount=${amount}`
 
     getRequest({
       extension: RemittanceOutwardsRepository.ProductDispersalEngine.qry,
       parameters: parameters
     })
       .then(res => {
-        setProductsStore(res);
+        setProductsStore(res)
       })
       .catch(error => {
         setErrorMessage(error)
       })
   }
 
-  const onIdNoBlur = (idNo) => {
-    var parameters = `_idNo=${idNo}` 
+  const onIdNoBlur = idNo => {
+    var parameters = `_idNo=${idNo}`
 
     getRequest({
       extension: CurrencyTradingClientRepository.Identity.get,
       parameters: parameters
     })
       .then(res => {
-        if(res?.record?.clientId){
-          var clientParameters = `_recordId=${res?.record?.clientId}` 
+        if (res?.record?.clientId) {
+          var clientParameters = `_recordId=${res?.record?.clientId}`
           getRequest({
             extension: CurrencyTradingClientRepository.Client.get,
             parameters: clientParameters
+          }).then(clientRes => {
+            console.log(clientRes)
+            if (clientRes?.record) {
+              outwardsValidation.setFieldValue('cl_reference', clientRes?.record?.reference)
+              outwardsValidation.setFieldValue('cl_name', clientRes?.record?.name)
+              outwardsValidation.setFieldValue('idType', res?.record?.idtId)
+              outwardsValidation.setFieldValue('nationalityId', clientRes?.record?.nationalityId)
+            }
           })
-            .then(clientRes => {
-              console.log(clientRes);
-              if(clientRes?.record){
-                outwardsValidation.setFieldValue('cl_reference', clientRes?.record?.reference)
-                outwardsValidation.setFieldValue('cl_name', clientRes?.record?.name)
-                outwardsValidation.setFieldValue('idType', res?.record?.idtId)
-                outwardsValidation.setFieldValue('nationalityId', clientRes?.record?.nationalityId)
-              }
-            })
-        }
-        else //clear the id field or show a message that there isn't any client with this ID
-        {
+        } //clear the id field or show a message that there isn't any client with this ID
+        else {
           outwardsValidation.setFieldValue('idNo', '')
           outwardsValidation.setFieldValue('cl_reference', '')
           outwardsValidation.setFieldValue('cl_name', '')
           outwardsValidation.setFieldValue('idType', '')
           outwardsValidation.setFieldValue('nationalityId', '')
         }
-
       })
       .catch(error => {
         setErrorMessage(error)
       })
   }
-  
+
   const lookupCorrespondent = searchQry => {
-
     setCorrespondentStore([])
-    if(searchQry){
-    var parameters = `_filter=${searchQry}`
-    getRequest({
-      extension: RemittanceSettingsRepository.Correspondent.snapshot,
-      parameters: parameters
-    })
-      .then(res => {
-        setCorrespondentStore(res.list)
+    if (searchQry) {
+      var parameters = `_filter=${searchQry}`
+      getRequest({
+        extension: RemittanceSettingsRepository.Correspondent.snapshot,
+        parameters: parameters
       })
-      .catch(error => {
-        setErrorMessage(error)
-      })}
+        .then(res => {
+          setCorrespondentStore(res.list)
+        })
+        .catch(error => {
+          setErrorMessage(error)
+        })
+    }
   }
 
-  const delOutwards = obj => {
-
-  }
+  const delOutwards = obj => {}
 
   const addOutwards = () => {
     console.log(getNewOutwards())
@@ -307,7 +310,7 @@ const OutwardsTransfer = () => {
   }
 
   const editOutwards = obj => {
-     outwardsValidation.setValues(populateOutwards(obj))
+    outwardsValidation.setValues(populateOutwards(obj))
 
     // fillCountryStore()
     // setEditMode(true)
@@ -323,7 +326,6 @@ const OutwardsTransfer = () => {
         fillCountryStore()
 
         //getLabels(ResourceIds.Currencies, setLabels)
-
       } else {
         setErrorMessage({ message: "YOU DON'T HAVE ACCESS TO THIS SCREEN" })
       }
