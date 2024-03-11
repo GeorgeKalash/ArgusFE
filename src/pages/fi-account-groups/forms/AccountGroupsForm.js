d748a2ca762624003e5c6f8d9fb2ba6a1f524872
Alinadimmokdad <alimokdad961@gmail.com>
2024-03-11 17:11:01 +0200

    mistake branch

---
+++ src/pages/fi-account-groups/forms/AccountGroupsForm.js
@@ -0,0 +1,190 @@
+// ** MUI Imports
+import { Grid } from '@mui/material'
+import { useContext, useEffect, useState } from 'react'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+import FormShell from 'src/components/Shared/FormShell'
+import toast from 'react-hot-toast'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useInvalidate } from 'src/hooks/resource'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { ResourceLookup } from 'src/components/Shared/ResourceLookup'
+import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
+import { DataSets } from 'src/resources/DataSets'
+
+// ** Custom Imports
+import CustomTextField from 'src/components/Inputs/CustomTextField'
+
+
+import { FinancialRepository } from 'src/repositories/FinancialRepository'
+import { SystemRepository } from 'src/repositories/SystemRepository'
+
+
+export default function AccountGroupsForm({ labels, maxAccess, recordId }) {
+    const [isLoading, setIsLoading] = useState(false)
+    const [editMode, setEditMode] = useState(!!recordId)
+    
+    const [initialValues, setInitialData] = useState({
+        recordId: null,
+        reference: '',
+        name: '',
+        type:'',
+        nraRef:'',
+        nraDescription:'',
+        nraId:''
+
+
+
+      })
+
+    const { getRequest, postRequest } = useContext(RequestsContext)
+
+    //const editMode = !!recordId
+
+    const invalidate = useInvalidate({
+        endpointId: FinancialRepository.Group.qry
+      })
+  
+    const formik = useFormik({
+        initialValues,
+        enableReinitialize: true,
+        validateOnChange: true,
+        validationSchema: yup.object({
+          reference: yup.string().required('This field is required'),
+          name: yup.string().required('This field is required'),
+          type: yup.string().required('This field is required'),
+          nraRef: yup.string().required('This field is required'),
+          nraDescription: yup.string().required('This field is required'),
+        }),
+        onSubmit: async obj => {
+          const recordId = obj.recordId
+
+          const response = await postRequest({
+            extension: FinancialRepository.Group.set,
+            record: JSON.stringify(obj)
+          })
+          
+          if (!recordId) {
+            toast.success('Record Added Successfully')
+            setInitialData({
+              ...obj, // Spread the existing properties
+              recordId: response.recordId, // Update only the recordId field
+            });
+          }
+          else toast.success('Record Edited Successfully')
+          setEditMode(true)
+
+          invalidate()
+        }
+      })
+    
+      useEffect(() => {
+        ;(async function () {
+          try {
+            if (recordId) {
+              setIsLoading(true)
+    
+              const res = await getRequest({
+                extension: FinancialRepository.Group.get,
+                parameters: `_recordId=${recordId}`
+              })
+              
+              setInitialData(res.record)
+            }
+          } catch (exception) {
+            setErrorMessage(error)
+          }
+          setIsLoading(false)
+        })()
+      }, [])
+      
+    return (
+        <FormShell 
+            resourceId={ResourceIds.FlAccountGroups}
+            form={formik} 
+            height={300} 
+            maxAccess={maxAccess} 
+            editMode={editMode}
+        >
+            <Grid container spacing={4}>
+                <Grid item xs={12}>
+                    <CustomTextField
+                    name='reference'
+                    label={labels.reference}
+                    value={formik.values.reference}
+                    required
+                    maxAccess={maxAccess}
+                    maxLength='10'
+                    onChange={formik.handleChange}
+                    onClear={() => formik.setFieldValue('reference', '')}
+                    error={formik.touched.reference && Boolean(formik.errors.reference)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <CustomTextField
+                    name='name'
+                    label={labels.name}
+                    value={formik.values.name}
+                    required
+                    maxLength='50'
+                    maxAccess={maxAccess}
+                    onChange={formik.handleChange}
+                    onClear={() => formik.setFieldValue('name', '')}
+                    error={formik.touched.name && Boolean(formik.errors.name)}
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                <ResourceLookup
+             endpointId={SystemRepository.NumberRange.snapshot}
+             form={formik}
+             valueField='reference'
+             displayField='description'
+             name='nraRef'
+             label={labels.numberRange}
+             secondDisplayField={true}
+             secondValue={formik.values.nraDescription}
+             onChange={(event, newValue) => {
+
+              if (newValue) {
+                formik.setFieldValue('nraId', newValue?.recordId)
+                formik.setFieldValue('nraRef', newValue?.reference)
+                formik.setFieldValue('nraDescription', newValue?.description)
+                
+              } else {
+                formik.setFieldValue('nraId', null)
+                formik.setFieldValue('nraRef', '')
+                formik.setFieldValue('nraDescription', '')
+
+              }
+            }}
+            helperText=""
+            error={Boolean(formik.touched.nraDescription && formik.errors.nraDescription)}
+       
+            /> 
+  </Grid>
+  <Grid item xs={12}>
+          <ResourceComboBox
+              datasetId={DataSets.FI_GROUP_TYPE}
+              name='type'
+              label={labels.type}
+              valueField='key'
+              required
+              displayField='value'
+              values={formik.values} 
+              onChange={(event, newValue) => {
+                  if (newValue) {
+                      formik.setFieldValue('type', newValue?.key)
+                  } else {
+                      formik.setFieldValue('type', '')
+                  }
+              }}
+              error={formik.touched.type && Boolean(formik.errors.type)}
+
+              
+          />
+        </Grid>
+
+            </Grid>
+        </FormShell>
+  )
+}
