ee1eeb846149344ef3c7413c89255fce801d21b9
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-03-19 12:30:16 +0200

    icon hover

--- src/components/Shared/DataGrid/components/button/index.js
+++ src/components/Shared/DataGrid/components/button/index.js
@@ -7,8 +7,12 @@ export default {
 
 return (
       <Button
-
-        // sx={{ p:1 }}
+      sx={{
+        opacity: 1,  '&:hover': { opacity: 0.7 },
+        width: 15,
+        height: 35,
+        objectFit: 'contain'
+      }}
         autoFocus
         onClick={(e) => column.onClick(e, row)}
         variant='contained'
