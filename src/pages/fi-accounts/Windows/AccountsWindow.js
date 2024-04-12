09fa0f0bf4b420775bf17a4377a96c1b08cf3376
RebeccaFrem02 <155422197+RebeccaFrem02@users.noreply.github.com>
2024-04-12 08:38:15 +0300

    REM-202

--- src/pages/fi-accounts/Windows/AccountsWindow.js
+++ src/pages/fi-accounts/Windows/AccountsWindow.js
@@ -21,7 +21,7 @@ const AccountsWindow = ({
 
   const [store , setStore] = useState({
     recordId : recordId || null,
-    list: null,
+    currencies: null,
   })
 
   return (
@@ -77,3 +77,61 @@ const AccountsWindow = ({
 }
 
 export default AccountsWindow
+
+
+// // ** Custom Imports
+// import CustomTabPanel from 'src/components/Shared/CustomTabPanel'
+// import { CustomTabs } from 'src/components/Shared/CustomTabs'
+// import { useState } from 'react'
+// import IdTypesForm from '../forms/IdTypesForm'
+// import IdFieldsForm from '../forms/IdFieldsForm'
+
+// const IdTypesWindow = ({
+//   height,
+//   recordId,
+//   labels,
+//   maxAccess,
+//   expanded
+// }) => {
+//   const [activeTab , setActiveTab] = useState(0)
+//   const [editMode, setEditMode] = useState(recordId)
+
+//   const [store , setStore] = useState({
+//     recordId : recordId || null,
+//     IdField: null,
+//   })
+
+//   const tabs = [
+//     { label: labels.main },
+//     { label: labels.idFields, disabled: !store.recordId },
+//   ]
+
+//   return (
+//     <>
+//       <CustomTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+//       <CustomTabPanel height={height} index={0} value={activeTab}>
+//         <IdTypesForm
+//           labels={labels}
+//           setEditMode={setEditMode}
+//           setStore={setStore}
+//           store={store}
+//           editMode={editMode}
+//           maxAccess={maxAccess}
+//         />
+//       </CustomTabPanel>
+//       <CustomTabPanel height={height} index={1} value={activeTab}>
+//         <IdFieldsForm
+//           store={store}
+//           setStore={setStore}
+//           labels={labels}
+//           maxAccess={maxAccess}
+//           height={height}
+//           expanded={expanded}
+//           editMode={editMode}
+//         />
+//       </CustomTabPanel>
+//     </>
+//   )
+// }
+
+// export default IdTypesWindow
--- src/pages/fi-accounts/forms/CreditLimitsForm.js
+++ src/pages/fi-accounts/forms/CreditLimitsForm.js
@@ -45,19 +45,21 @@ const CreditLimitsForm = ({
         ]
       },
       onSubmit: values => {
-        postCreditLimits(values.currencies)
+        postCurrencies(values.currencies)
       }
     })
 
-    const postCreditLimits = obj => {
+    const postCurrencies = obj => {
 
       const data = {
-        accountId: accountId,
-        CreditLimits : obj.map(
-          ({accountId,...rest} ) => ({
-              accountId: accountId,
-              ...rest
-          }))}
+        currencies : obj.map(
+          ({ accountId, currencyId,currencyName,limit} ) => ({
+                accountId: accountId,
+                currencyId: currencyId,
+                currencyName: currencyName,
+                limit: limit
+            })
+        )}
       postRequest({
         extension: FinancialRepository.AccountCreditLimit.set,
         record: JSON.stringify(data)
@@ -73,7 +75,7 @@ const CreditLimitsForm = ({
     const column = [
       {
         component: 'textfield',
-        label: labels.name,
+        label: labels.currency,
         name: 'currencyName',
         mandatory: false,
         readOnly: true
@@ -99,30 +101,29 @@ const CreditLimitsForm = ({
       })
         .then(res => {
           if (res.list.length > 0){
-            const list = res.list.map(({ currencyId, currencyName, limit, ...rest } , index) => ({
-               id : index,
-              currencyId,
-              currencyName,
-              limit,
-               ...rest
+            const currencies = res.list.map(({ currencyId, currencyName, limit, ...rest } , index) => ({
+                id : index,
+                currencyId,
+                currencyName,
+                limit,
+                ...rest
             }))
-            formik.setValues({ currencies: list})
+            formik.setValues({ currencies: currencies})
 
           setStore(prevStore => ({
             ...prevStore,
-            list: list,
+            currencies: currencies,
           }));
-          console.log(list)
-
           }
-
         })
         .catch(error => {
         })
+        console.log(formik.values)
     }
 
   return (
-    <FormShell form={formik}
+    <FormShell 
+      form={formik}
       resourceId={ResourceIds.Accounts}
       maxAccess={maxAccess}
       infoVisible={false}
@@ -142,3 +143,155 @@ const CreditLimitsForm = ({
 }
 
 export default CreditLimitsForm
+
+
+// import { Box } from '@mui/material'
+// import { useFormik } from 'formik'
+// import { useContext, useEffect } from 'react'
+// import { DataGrid } from 'src/components/Shared/DataGrid'
+// import FormShell from 'src/components/Shared/FormShell'
+
+// // ** Custom Imports
+// import * as yup from 'yup'
+// import toast from 'react-hot-toast'
+// import { RequestsContext } from 'src/providers/RequestsContext'
+// import { CurrencyTradingSettingsRepository } from 'src/repositories/CurrencyTradingSettingsRepository'
+// import { ResourceIds } from 'src/resources/ResourceIds'
+// import { DataSets } from 'src/resources/DataSets'
+
+// const IdFieldsForm = ({
+//   store,
+//   setStore,
+//   labels,
+//   editMode,
+//   height,
+//   expanded,
+//   maxAccess
+// }) => {
+//   const { getRequest, postRequest } = useContext(RequestsContext)
+//   const {recordId : idtId } = store
+
+//   const formik = useFormik({
+//       enableReinitialize: false,
+//       validateOnChange: true,
+//       validationSchema: yup.object({ IdField: yup
+//         .array()
+//         .of(
+//           yup.object().shape({
+//             accessLevel: yup.string().required('Access Level recordId is required')
+//           })
+//         ).required('Operations array is required') }),
+//       initialValues: {
+//         IdField: [
+//           { id :1,
+//             idtId: idtId,
+//             accessLevel: null,
+//             accessLevel: null,
+//             accessLevelName: '',
+//             controlId: ''
+//           }
+//         ]
+//       },
+//       onSubmit: values => {
+//         postIdFields(values.IdField)
+//       }
+//     })
+
+//     const postIdFields = obj => {
+
+//       const data = {
+//         idtId: idtId,
+//         items : obj.map(
+//           ({ accessLevel, controlId} ) => ({
+//               idtId: idtId,
+//               accessLevel: Number(accessLevel),
+//               controlId: controlId
+//           })
+//         )
+//       }
+//       postRequest({
+//         extension: CurrencyTradingSettingsRepository.IdFields.set2,
+//         record: JSON.stringify(data)
+//       })
+//         .then(res => {
+//           if (res) toast.success('Record Edited Successfully')
+//           getIdField(idtId)
+//         })
+//         .catch(error => {
+//         })
+//     }
+
+//     const column = [
+//       {
+//         component: 'textfield',
+//         label: labels.control,
+//         name: 'controlId',
+//         mandatory: true
+//       },
+//       {
+//         component: 'resourcecombobox',
+//         label: labels.accessLevel,
+//         name: 'accessLevel',
+//         props: {
+//           datasetId: DataSets.AU_RESOURCE_CONTROL_ACCESS_LEVEL,
+//           valueField: 'key',
+//           displayField: 'value',
+//           mapping: [ 
+//             { from: 'value', to: 'accessLevelName' },
+//             { from: 'key', to: 'accessLevel' } 
+//           ]
+//         }
+//       }    
+//     ]
+//     useEffect(()=>{
+//       idtId  && getIdField(idtId)
+//     }, [idtId])
+
+//     const getIdField = idtId => {
+//       const defaultParams = `_idtId=${idtId}`
+//       var parameters = defaultParams
+//       getRequest({
+//         extension: CurrencyTradingSettingsRepository.IdFields.qry,
+//         parameters: parameters
+//       })
+//         .then(res => {
+//           if (res.list.length > 0){
+//              const IdField = res.list.map(({ accessLevel, ...rest } , index) => ({
+//                id : index,
+//                accessLevel: accessLevel.toString(),
+//                ...rest
+//             }))
+//             formik.setValues({ IdField: IdField})
+
+//           setStore(prevStore => ({
+//             ...prevStore,
+//             IdField: IdField,
+//           }));
+//           }
+//         })
+//         .catch(error => {
+//         })
+//     }
+
+//   return (
+//     <FormShell 
+//       form={formik}
+//       resourceId={ResourceIds.IdTypes}
+//       maxAccess={maxAccess}
+//       infoVisible={false}
+//       editMode={editMode}
+//     >
+//       <Box sx={{ flex: 1, display: 'flex', justifyContent: 'center', scroll: 'none', overflow:'hidden' }}>
+//         <DataGrid
+//           onChange={value => formik.setFieldValue('IdField', value)}
+//           value={formik.values.IdField}
+//           error={formik.errors.IdField}
+//           columns={column}
+//           height={`${expanded ? `calc(100vh - 280px)` : `${height-100}px`}`}
+//         />
+//       </Box>
+//     </FormShell>
+//   )
+// }
+
+// export default IdFieldsForm
