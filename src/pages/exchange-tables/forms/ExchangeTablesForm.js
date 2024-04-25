3bdb9900ab74b881e426a6987eaac6637b7456b6
Alinadimmokdad <alimokdad961@gmail.com>
2024-04-25 10:59:14 +0300

    add flName to currencyId

--- src/pages/exchange-tables/forms/ExchangeTablesForm.js
+++ src/pages/exchange-tables/forms/ExchangeTablesForm.js
@@ -17,195 +17,198 @@ import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
 import { DataSets } from 'src/resources/DataSets'
 import { SystemRepository } from 'src/repositories/SystemRepository'
 
-
 export default function ExchangeTablesForm({ labels, maxAccess, recordId }) {
-    const [isLoading, setIsLoading] = useState(false)
-    const [editMode, setEditMode] = useState(!!recordId)
-    
-    const [initialValues, setInitialData] = useState({
-        recordId: null,
-        reference: '',
-        name: '',
-        currencyId: '',
-        rateCalcMethod: '',
-        rateAgainst: '',
-        rateAgainstCurrencyId: '',
+  const [isLoading, setIsLoading] = useState(false)
+  const [editMode, setEditMode] = useState(!!recordId)
+
+  const [initialValues, setInitialData] = useState({
+    recordId: null,
+    reference: '',
+    name: '',
+    currencyId: '',
+    rateCalcMethod: '',
+    rateAgainst: '',
+    rateAgainstCurrencyId: ''
+  })
+
+  const { getRequest, postRequest } = useContext(RequestsContext)
+
+  //const editMode = !!recordId
+
+  const invalidate = useInvalidate({
+    endpointId: MultiCurrencyRepository.ExchangeTable.page
+  })
+
+  const formik = useFormik({
+    initialValues,
+    enableReinitialize: true,
+    validateOnChange: true,
+    validationSchema: yup.object({
+      reference: yup.string().required(' '),
+      name: yup.string().required(' '),
+      currencyId: yup.string().required(' '),
+      rateCalcMethod: yup.string().required(' '),
+      rateAgainst: yup.string().required(' '),
+      rateAgainstCurrencyId: yup.string().required(' ')
+    }),
+    onSubmit: async obj => {
+      const recordId = obj.recordId
+
+      const response = await postRequest({
+        extension: MultiCurrencyRepository.ExchangeTable.set,
+        record: JSON.stringify(obj)
       })
 
-    const { getRequest, postRequest } = useContext(RequestsContext)
+      if (!recordId) {
+        toast.success('Record Added Successfully')
+        setInitialData({
+          ...obj, // Spread the existing properties
+          recordId: response.recordId // Update only the recordId field
+        })
+      } else toast.success('Record Edited Successfully')
+      setEditMode(true)
 
-    //const editMode = !!recordId
+      invalidate()
+    }
+  })
 
-    const invalidate = useInvalidate({
-        endpointId: MultiCurrencyRepository.ExchangeTable.page
-      })
-  
-    const formik = useFormik({
-        initialValues,
-        enableReinitialize: true,
-        validateOnChange: true,
-        validationSchema: yup.object({
-          reference: yup.string().required(' '),
-          name: yup.string().required(' '),
-          currencyId: yup.string().required(' '),
-          rateCalcMethod: yup.string().required(' '),
-          rateAgainst: yup.string().required(' '),
-          rateAgainstCurrencyId: yup.string().required(' '),   
-        }),
-        onSubmit: async obj => {
-          const recordId = obj.recordId
+  useEffect(() => {
+    ;(async function () {
+      try {
+        if (recordId) {
+          setIsLoading(true)
 
-          const response = await postRequest({
-            extension: MultiCurrencyRepository.ExchangeTable.set,
-            record: JSON.stringify(obj)
+          const res = await getRequest({
+            extension: MultiCurrencyRepository.ExchangeTable.get,
+            parameters: `_recordId=${recordId}`
           })
-          
-          if (!recordId) {
-            toast.success('Record Added Successfully')
-            setInitialData({
-              ...obj, // Spread the existing properties
-              recordId: response.recordId, // Update only the recordId field
-            });
-          }
-          else toast.success('Record Edited Successfully')
-          setEditMode(true)
 
-          invalidate()
+          setInitialData(res.record)
         }
-      })
-    
-      useEffect(() => {
-        ;(async function () {
-          try {
-            if (recordId) {
-              setIsLoading(true)
-    
-              const res = await getRequest({
-                extension: MultiCurrencyRepository.ExchangeTable.get,
-                parameters: `_recordId=${recordId}`
-              })
-              
-              setInitialData(res.record)
-            }
-          } catch (exception) {
-            setErrorMessage(error)
-          }
-          setIsLoading(false)
-        })()
-      }, [])
-      
-    return (
-        <FormShell 
-            resourceId={ResourceIds.ExchangeTable}
-            form={formik} 
-            height={300} 
-            maxAccess={maxAccess} 
-            editMode={editMode}
-        >
-          <Grid container spacing={4}>
-            <Grid item xs={12}>
-              <CustomTextField
-                name='reference'
-                label={labels.reference}
-                value={formik.values.reference}
-                required
-                maxAccess={maxAccess}
-                maxLength='10'
-                onChange={formik.handleChange}
-                onClear={() => formik.setFieldValue('reference', '')}
-                error={formik.touched.reference && Boolean(formik.errors.reference)}
-                
-                // helperText={formik.touched.reference && formik.errors.reference}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <CustomTextField
-                name='name'
-                label={labels.name}
-                value={formik.values.name}
-                required
-                maxAccess={maxAccess}
-                maxLength='50'
-                onChange={formik.handleChange}
-                onClear={() => formik.setFieldValue('name', '')}
-                error={formik.touched.name && Boolean(formik.errors.name)}
+      } catch (exception) {
+        setErrorMessage(error)
+      }
+      setIsLoading(false)
+    })()
+  }, [])
 
-                // helperText={formik.touched.name && formik.errors.name}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <ResourceComboBox
-                endpointId={SystemRepository.Currency.qry}
-                name='currencyId'
-                label={labels.currencyId}
-                valueField='recordId'
-                displayField='name'
-                values={formik.values}
-                required
-                maxAccess={maxAccess}
-                onChange={(event, newValue) => {
-                  formik.setFieldValue('currencyId', newValue?.recordId)
-                }}
-                error={formik.touched.currencyId && Boolean(formik.errors.currencyId)}
+  return (
+    <FormShell
+      resourceId={ResourceIds.ExchangeTables}
+      form={formik}
+      height={300}
+      maxAccess={maxAccess}
+      editMode={editMode}
+    >
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <CustomTextField
+            name='reference'
+            label={labels.reference}
+            value={formik.values.reference}
+            required
+            maxAccess={maxAccess}
+            maxLength='10'
+            onChange={formik.handleChange}
+            onClear={() => formik.setFieldValue('reference', '')}
+            error={formik.touched.reference && Boolean(formik.errors.reference)}
 
-                // helperText={formik.touched.currencyId && formik.errors.currencyId}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <ResourceComboBox
-                datasetId={DataSets.MC_RATE_CALC_METHOD}
-                name='rateCalcMethod'
-                label={labels.rateCalcMethod}
-                valueField='key'
-                displayField='value'
-                values={formik.values}
-                required
-                maxAccess={maxAccess}
-                onChange={(event, newValue) => {
-                  formik.setFieldValue('rateCalcMethod', newValue?.key)
-                }}
-                error={formik.touched.rateCalcMethod && Boolean(formik.errors.rateCalcMethod)}
+            // helperText={formik.touched.reference && formik.errors.reference}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <CustomTextField
+            name='name'
+            label={labels.name}
+            value={formik.values.name}
+            required
+            maxAccess={maxAccess}
+            maxLength='50'
+            onChange={formik.handleChange}
+            onClear={() => formik.setFieldValue('name', '')}
+            error={formik.touched.name && Boolean(formik.errors.name)}
 
-                // helperText={formik.touched.rateCalcMethod && formik.errors.rateCalcMethod}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <ResourceComboBox
-                datasetId={DataSets.MC_RATE_AGAINST}
-                name='rateAgainst'
-                label={labels.rateAgainst}
-                valueField='key'
-                displayField='value'
-                values={formik.values}
-                required
-                maxAccess={maxAccess}
-                onChange={(event, newValue) => {
-                  formik.setFieldValue('rateAgainst', newValue?.key)
-                }}
-                error={formik.touched.rateAgainst && Boolean(formik.errors.rateAgainst)}
+            // helperText={formik.touched.name && formik.errors.name}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <ResourceComboBox
+            endpointId={SystemRepository.Currency.qry}
+            name='currencyId'
+            label={labels.currencyId}
+            valueField='recordId'
+            displayField={['reference', 'name', 'flName']}
+            columnsInDropDown={[
+              { key: 'reference', value: 'Currency Ref' },
+              { key: 'name', value: 'Name' },
+              { key: 'flName', value: 'Foreign Language' }
+            ]}
+            values={formik.values}
+            required
+            maxAccess={maxAccess}
+            onChange={(event, newValue) => {
+              formik.setFieldValue('currencyId', newValue?.recordId)
+            }}
+            error={formik.touched.currencyId && Boolean(formik.errors.currencyId)}
 
-                // helperText={formik.touched.rateAgainst && formik.errors.rateAgainst}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <ResourceComboBox
-                endpointId={SystemRepository.Currency.qry}
-                name='rateAgainstCurrencyId'
-                label={labels.rateAgainstCurrencyId}
-                valueField='recordId'
-                displayField='name'
-                values={formik.values}
-                required
-                maxAccess={maxAccess}
-                onChange={(event, newValue) => {
-                  formik.setFieldValue('rateAgainstCurrencyId', newValue?.recordId)
-                }}
-                error={formik.touched.rateAgainstCurrencyId && Boolean(formik.errors.rateAgainstCurrencyId)}
+            // helperText={formik.touched.currencyId && formik.errors.currencyId}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <ResourceComboBox
+            datasetId={DataSets.MC_RATE_CALC_METHOD}
+            name='rateCalcMethod'
+            label={labels.rateCalcMethod}
+            valueField='key'
+            displayField='value'
+            values={formik.values}
+            required
+            maxAccess={maxAccess}
+            onChange={(event, newValue) => {
+              formik.setFieldValue('rateCalcMethod', newValue?.key)
+            }}
+            error={formik.touched.rateCalcMethod && Boolean(formik.errors.rateCalcMethod)}
 
-                // helperText={formik.touched.rateAgainstCurrencyId && formik.errors.rateAgainstCurrencyId}
-              />
-            </Grid>
-          </Grid>
-        </FormShell>
+            // helperText={formik.touched.rateCalcMethod && formik.errors.rateCalcMethod}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <ResourceComboBox
+            datasetId={DataSets.MC_RATE_AGAINST}
+            name='rateAgainst'
+            label={labels.rateAgainst}
+            valueField='key'
+            displayField='value'
+            values={formik.values}
+            required
+            maxAccess={maxAccess}
+            onChange={(event, newValue) => {
+              formik.setFieldValue('rateAgainst', newValue?.key)
+            }}
+            error={formik.touched.rateAgainst && Boolean(formik.errors.rateAgainst)}
+
+            // helperText={formik.touched.rateAgainst && formik.errors.rateAgainst}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <ResourceComboBox
+            endpointId={SystemRepository.Currency.qry}
+            name='rateAgainstCurrencyId'
+            label={labels.rateAgainstCurrencyId}
+            valueField='recordId'
+            displayField='name'
+            values={formik.values}
+            required
+            maxAccess={maxAccess}
+            onChange={(event, newValue) => {
+              formik.setFieldValue('rateAgainstCurrencyId', newValue?.recordId)
+            }}
+            error={formik.touched.rateAgainstCurrencyId && Boolean(formik.errors.rateAgainstCurrencyId)}
+
+            // helperText={formik.touched.rateAgainstCurrencyId && formik.errors.rateAgainstCurrencyId}
+          />
+        </Grid>
+      </Grid>
+    </FormShell>
   )
 }
