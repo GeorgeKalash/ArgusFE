57b6574bd146e78486844c4634f0ba4548e2966c
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-04-30 08:28:12 +0300

    error message in client-list

--- src/pages/clients-list/forms/ClientTemplateForm.js
+++ src/pages/clients-list/forms/ClientTemplateForm.js
@@ -36,10 +36,10 @@ import { AddressFormShell } from 'src/components/Shared/AddressFormShell'
 import { CTCLRepository } from 'src/repositories/CTCLRepository'
 import BeneficiaryWindow from '../Windows/BeneficiaryWindow'
 import { useInvalidate } from 'src/hooks/resource'
-import SystemDefaults from 'src/pages/system-defaults'
 import { SystemFunction } from 'src/resources/SystemFunction'
+import { useError } from 'src/error'
 
-const ClientTemplateForm = ({ setErrorMessage, recordId, labels, plantId, maxAccess }) => {
+const ClientTemplateForm = ({ recordId, labels, plantId, maxAccess }) => {
   const { stack } = useWindow()
   const { getRequest, postRequest } = useContext(RequestsContext)
   const [showAsPassword, setShowAsPassword] = useState(false)
@@ -53,6 +53,7 @@ const ClientTemplateForm = ({ setErrorMessage, recordId, labels, plantId, maxAcc
   const [idTypeStore, setIdTypeStore] = useState([])
   const [otpShow, setOtpShow] = useState(false)
   const [isClosed, setIsClosed] = useState(false)
+  const { stack: stackError } = useError()
 
   const [initialValues, setInitialData] = useState({
     //clientIDView
@@ -341,7 +342,7 @@ const ClientTemplateForm = ({ setErrorMessage, recordId, labels, plantId, maxAcc
       })
         .then(res => {
           if (res.record) {
-            setErrorMessage('the ID number exists.')
+            stackError({ message: 'the ID number exists.' })
           }
         })
         .catch(error => {})
@@ -568,7 +569,6 @@ const ClientTemplateForm = ({ setErrorMessage, recordId, labels, plantId, maxAcc
           formValidation: clientIndividualFormik,
           functionId: clientIndividualFormik.values.functionId,
           setEditMode: setEditMode,
-          setErrorMessage: setErrorMessage,
           getData: getClient
         },
         width: 400,
@@ -785,7 +785,6 @@ const ClientTemplateForm = ({ setErrorMessage, recordId, labels, plantId, maxAcc
                           props: {
                             idTypeStore: idTypeStore,
                             formik: clientIndividualFormik,
-                            setErrorMessage: setErrorMessage,
                             labels: labels
                           },
                           title: labels.fetch,
