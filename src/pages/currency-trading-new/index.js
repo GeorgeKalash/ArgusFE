0dc9b850d0aec821f535d38d24207dc933735907
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-05-08 11:26:42 +0300

    clear console.log

--- src/pages/currency-trading-new/index.js
+++ src/pages/currency-trading-new/index.js
@@ -9,62 +9,58 @@ import useResourceParams from 'src/hooks/useResourceParams'
 export default function CurrencyTrading() {
   const { getRequest } = useContext(RequestsContext)
 
+  //error
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [plantId, setPlantId] = useState(null)
 
- //error
- const [errorMessage, setErrorMessage] = useState(null)
- const [plantId, setPlantId] = useState(null)
+  const { labels: _labelsADJ, access: accessADJ } = useResourceParams({
+    datasetId: 35208
+  })
 
- const { labels: _labelsADJ, access: accessADJ } = useResourceParams({
-  datasetId: 35208
-})
+  const getPlantId = async () => {
+    const userData = window.sessionStorage.getItem('userData')
+      ? JSON.parse(window.sessionStorage.getItem('userData'))
+      : null
 
- const getPlantId = async () => {
-  const userData = window.sessionStorage.getItem('userData')
-    ? JSON.parse(window.sessionStorage.getItem('userData'))
-    : null;
+    const parameters = `_userId=${userData && userData.userId}&_key=plantId`
 
-    console.log(userData)
-  const parameters = `_userId=${userData && userData.userId}&_key=plantId`;
-
-  try {
-    const res = await getRequest({
-      extension: SystemRepository.UserDefaults.get,
-      parameters: parameters,
-    });
-
-    if (res.record.value) {
-      return res.record.value;
-    }
-
-    return '';
-  } catch (error) {
-    setErrorMessage(error);
-
-     return '';
-  }
-};
- async function openForm() {
     try {
-      const plantId = await getPlantId();
+      const res = await getRequest({
+        extension: SystemRepository.UserDefaults.get,
+        parameters: parameters
+      })
+
+      if (res.record.value) {
+        return res.record.value
+      }
+
+      return ''
+    } catch (error) {
+      setErrorMessage(error)
+
+      return ''
+    }
+  }
+  async function openForm() {
+    try {
+      const plantId = await getPlantId()
       if (plantId !== '') {
         setPlantId(plantId)
       } else {
-        setErrorMessage({ error: 'The user does not have a default plant' });
+        setErrorMessage({ error: 'The user does not have a default plant' })
       }
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-
   }
 
-
-  useEffect(()=>{
+  useEffect(() => {
     openForm()
-   },[accessADJ])
+  }, [accessADJ])
 
   return (
-    <Box sx={{height: `calc(100vh - 48px)` , display: 'flex',flexDirection: 'column' , zIndex:1}}>
-   {plantId && accessADJ &&  <TransactionForm labels={_labelsADJ} maxAccess={accessADJ}  plantId={plantId} />}
+    <Box sx={{ height: `calc(100vh - 48px)`, display: 'flex', flexDirection: 'column', zIndex: 1 }}>
+      {plantId && accessADJ && <TransactionForm labels={_labelsADJ} maxAccess={accessADJ} plantId={plantId} />}
     </Box>
   )
 }
