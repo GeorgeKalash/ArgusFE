9cb564d8845533e203d0d4d35d25078a038643be
alihajjhassan <Ali.HajjHassan@SoftMachine.co>
2024-05-10 16:38:47 +0300

    cbBankAccount

--- src/providers/AuthContext.js
+++ src/providers/AuthContext.js
@@ -67,7 +67,7 @@ const AuthProvider = ({ children }) => {
     const fetchData = async () => {
       const matchHostname = window.location.hostname.match(/^(.+)\.softmachine\.co$/)
 
-      const accountName = matchHostname ? matchHostname[1] : 'cil2'
+      const accountName = matchHostname ? matchHostname[1] : 'burger'
 
       try {
         const response = await axios({
