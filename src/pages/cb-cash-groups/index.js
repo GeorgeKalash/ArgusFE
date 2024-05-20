9c1ab0ea0af351350dc5d000a17874650abb63bb
alihajjhassan <Ali.HajjHassan@SoftMachine.co>
2024-05-20 11:53:35 +0300

    REM-255

--- src/pages/cb-cash-groups/index.js
+++ src/pages/cb-cash-groups/index.js
@@ -4,19 +4,17 @@ import Table from 'src/components/Shared/Table'
 import GridToolbar from 'src/components/Shared/GridToolbar'
 import { RequestsContext } from 'src/providers/RequestsContext'
 import { CashBankRepository } from 'src/repositories/CashBankRepository'
-import CbCashGroupsWindow from './Windows/CbCashGroupsWindow'
-import ErrorWindow from 'src/components/Shared/ErrorWindow'
 import { useInvalidate, useResourceQuery } from 'src/hooks/resource'
 import { ResourceIds } from 'src/resources/ResourceIds'
 import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
 import { Fixed } from 'src/components/Shared/Layouts/Fixed'
 import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useWindow } from 'src/windows'
+import CbCashGroupsForms from './forms/CbCashGroupsForm'
 
 const CbCashGroup = () => {
   const { getRequest, postRequest } = useContext(RequestsContext)
-  const [selectedRecordId, setSelectedRecordId] = useState(null)
-  const [windowOpen, setWindowOpen] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
+  const { stack } = useWindow()
 
   async function fetchGridData(options = {}) {
     const { _startAt = 0, _pageSize = 50 } = options
@@ -55,12 +53,11 @@ const CbCashGroup = () => {
   ]
 
   const add = () => {
-    setWindowOpen(true)
+    openForm()
   }
 
   const edit = obj => {
-    setSelectedRecordId(obj.recordId)
-    setWindowOpen(true)
+    openForm(obj?.recordId)
   }
 
   const del = async obj => {
@@ -71,7 +68,21 @@ const CbCashGroup = () => {
     invalidate()
     toast.success('Record Deleted Successfully')
   }
-  
+  function openForm(recordId) {
+    stack({
+      Component: CbCashGroupsForms,
+      props: {
+        labels: _labels,
+        recordId: recordId ? recordId : null,
+        maxAccess: access,
+        invalidate: invalidate
+      },
+      width: 600,
+      height: 600,
+      title: _labels.accountGroup
+    })
+  }
+
   return (
     <VertLayout>
       <Fixed>
@@ -90,19 +101,6 @@ const CbCashGroup = () => {
           maxAccess={access}
         />
       </Grow>
-      {windowOpen && (
-        <CbCashGroupsWindow
-          onClose={() => {
-            setWindowOpen(false)
-            setSelectedRecordId(null)
-          }}
-          labels={_labels}
-          maxAccess={access}
-          recordId={selectedRecordId}
-          setSelectedRecordId={setSelectedRecordId}
-        />
-      )}
-      <ErrorWindow open={errorMessage} onClose={() => setErrorMessage(null)} message={errorMessage} />
     </VertLayout>
   )
 }
