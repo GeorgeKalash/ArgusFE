a7d4fce057800c01fe0b288d025a4035f787c705
Hadi Chahine <hadi.n.chahine@gmail.com>
2024-05-20 12:00:49 +0300

    Refactor names, remove redundant state, and fix reload logic.

--- src/providers/TabsContext.js
+++ src/providers/TabsContext.js
@@ -18,7 +18,15 @@ function CustomTabPanel(props) {
       role='tabpanel'
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}
-      sx={{display: value !== index ? 'none !important' : 'flex !important', position: 'relative', flexDirection: 'column', width: '100%',flex :'1 !important', overflow:'auto',paddingTop:'5px'}}
+      sx={{
+        display: value !== index ? 'none !important' : 'flex !important',
+        position: 'relative',
+        flexDirection: 'column',
+        width: '100%',
+        flex: '1 !important',
+        overflow: 'auto',
+        paddingTop: '5px'
+      }}
       {...other}
     >
       {children}
@@ -66,40 +74,34 @@ const TabsProvider = ({ children }) => {
     return null
   }
 
-  const [activeTabs, setActiveTabs] = useState([])
+  const [openTabs, setOpenTabs] = useState([])
   const [initialLoadDone, setInitialLoadDone] = useState(false)
-  const [value, setValue] = useState(0)
+  const [currentTabIndex, setCurrentTabIndex] = useState(0)
   const [TabsIndex, setTabsIndex] = useState()
-  const [length, setLength] = useState(1)
-  const [closing, setClosing] = useState(false)
 
   const handleChange = (event, newValue) => {
-    setValue(newValue)
+    setCurrentTabIndex(newValue)
   }
 
   const handleCloseAllTabs = () => {
     router.push('/default/')
-    setActiveTabs([])
-    setLength(0)
-    setValue()
+    setOpenTabs([])
+    setCurrentTabIndex()
   }
 
   const handleCloseOtherTab = Tab => {
-    const tab = activeTabs[Tab]
+    const tab = openTabs[Tab]
     router.push(tab.route)
-    setActiveTabs([])
-    setActiveTabs([tab])
-    setLength(0)
-    setValue(0)
+    setOpenTabs([])
+    setOpenTabs([tab])
+    setCurrentTabIndex(0)
   }
 
   const closeTab = tabRoute => {
-    setClosing(true)
+    const index = openTabs.findIndex(tab => tab.route === tabRoute)
+    const activeTabsLength = openTabs.length
 
-    const index = activeTabs.findIndex(tab => tab.route === tabRoute)
-    const activeTabsLength = activeTabs.length
-
-    setActiveTabs(prevState => {
+    setOpenTabs(prevState => {
       return prevState.filter(tab => tab.route !== tabRoute)
     })
 
@@ -109,33 +111,24 @@ const TabsProvider = ({ children }) => {
       return
     }
 
-    if (value === index) {
+    if (currentTabIndex === index) {
       const newValue = index === activeTabsLength - 1 ? index - 1 : index + 1
-      setValue(newValue)
+      setCurrentTabIndex(newValue)
 
-      router.push(activeTabs[newValue].route)
-    } else if (index < value) {
-      setValue(currentValue => currentValue - 1)
+      router.push(openTabs[newValue].route)
+    } else if (index < currentTabIndex) {
+      setCurrentTabIndex(currentValue => currentValue - 1)
     }
-
-    setClosing(false)
   }
 
   useEffect(() => {
     if (router.asPath === '/default/') {
-      setActiveTabs([])
-      setLength(1)
+      setOpenTabs([])
     } else {
       if (initialLoadDone && router.asPath != '/default/') {
-        if (closing && value) {
-          if (activeTabs[value]?.route != router.asPath) {
-            router.push(activeTabs[value]?.route)
-          }
-        }
-
-        const isTabOpen = activeTabs.some((activeTab, index) => {
+        const isTabOpen = openTabs.some((activeTab, index) => {
           if (activeTab.page === children || activeTab.route === router.asPath) {
-            setValue(index)
+            setCurrentTabIndex(index)
 
             return true
           }
@@ -144,8 +137,8 @@ const TabsProvider = ({ children }) => {
         })
         if (isTabOpen) return
         else {
-          const newValueState = activeTabs.length
-          setActiveTabs(prevState => {
+          const newValueState = openTabs.length
+          setOpenTabs(prevState => {
             return [
               ...prevState,
               {
@@ -157,7 +150,7 @@ const TabsProvider = ({ children }) => {
               }
             ]
           })
-          setValue(newValueState)
+          setCurrentTabIndex(newValueState)
         }
       }
     }
@@ -167,8 +160,8 @@ const TabsProvider = ({ children }) => {
     if (router.asPath === '/default/') {
       return
     } else {
-      if (!activeTabs[0] && router.route != '/default/' && router.asPath && menu.length > 0) {
-        setActiveTabs([
+      if (!openTabs[0] && router.route != '/default/' && router.asPath && menu.length > 0) {
+        setOpenTabs([
           {
             page: children,
             route: router.asPath,
@@ -179,24 +172,31 @@ const TabsProvider = ({ children }) => {
         ])
         setInitialLoadDone(true)
       }
-      setClosing(false)
     }
-  }, [activeTabs, router, menu, gear])
+  }, [openTabs, router, menu, gear])
 
   return (
     <>
-      <Box sx={{ display: 'flex !important', flexDirection: 'column', width: '100%',flex :'1 !important', overflow:'auto' }}>
-        <Box sx={{ borderBottom: 1, borderColor: 'divider'}}>
+      <Box
+        sx={{
+          display: 'flex !important',
+          flexDirection: 'column',
+          width: '100%',
+          flex: '1 !important',
+          overflow: 'auto'
+        }}
+      >
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
           <Tabs
-            value={value}
+            value={currentTabIndex}
             onChange={handleChange}
             variant='scrollable'
             scrollButtons='auto'
             aria-label='scrollable auto tabs example'
             sx={{ maxHeight: '40px' }}
           >
-            {activeTabs.length > 0 &&
-              activeTabs.map((activeTab, i) => {
+            {openTabs.length > 0 &&
+              openTabs.map((activeTab, i) => {
                 return (
                   !activeTab.isDefault && (
                     <Tab
@@ -223,9 +223,9 @@ const TabsProvider = ({ children }) => {
               })}
           </Tabs>
         </Box>
-        {activeTabs.length > 0 &&
-          activeTabs.map((activeTab, i) => (
-            <CustomTabPanel key={i} index={i} value={value}>
+        {openTabs.length > 0 &&
+          openTabs.map((activeTab, i) => (
+            <CustomTabPanel key={activeTab.route} index={i} value={currentTabIndex}>
               {activeTab.page}
             </CustomTabPanel>
           ))}
@@ -241,7 +241,7 @@ const TabsProvider = ({ children }) => {
       >
         <MenuItem
           onClick={event => {
-            closeTab(activeTabs[TabsIndex]?.route)
+            closeTab(openTabs[TabsIndex]?.route)
             event.stopPropagation()
             handleClose()
           }}
