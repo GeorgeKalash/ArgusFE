1041b80e4abd02f3fbd055d8d87e6c7ccd4485fb
alihajjhassan <Ali.HajjHassan@SoftMachine.co>
2024-05-21 15:15:51 +0300

    REM-255ResolveConflict

--- src/components/ConfirmationDialog.js
+++ src/components/ConfirmationDialog.js
@@ -1,3 +1,4 @@
+// ** MUI Imports
 import { Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, Button, Box } from '@mui/material'
 import Window from './Shared/Window'
 
@@ -9,47 +10,46 @@ const ConfirmationDialog = ({
   cancelButtonAction,
   fullScreen = true
 }) => {
-  return !fullScreen ? (
-    openCondition && (
-      <Window Title='Confirmation' width={450} height={120} canExpand={false} onClose={closeCondition}>
+  return !fullScreen ? ( //dialog full screen or page
+    // openCondition && (
 
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            height: '120px'
-          }}
-        >
-          <Box
-            sx={{
-              flex: '1',
-              overflow: 'auto',
-              p: 5
-            }}
-          >
-            {DialogText}
-          </Box>
-          <Box
-            sx={{
-              flexShrink: 0,
-              py: 1,
-              px: 5,
-              display: 'flex',
-              justifyContent: 'flex-end'
-            }}
-          >
-            <Button onClick={okButtonAction} color='primary'>
-              OK
-            </Button>
-            <Button onClick={cancelButtonAction} color='primary'>
-              Cancel
-            </Button>
-          </Box>
-        </Box>
-
-      // </Window>
-    // )
+    // <Window Title='Confirmation' width={450} height={120} canExpand={false} onClose={closeCondition}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: '120px'
+      }}
+    >
+      <Box
+        sx={{
+          flex: '1',
+          overflow: 'auto',
+          p: 5
+        }}
+      >
+        {DialogText}
+      </Box>
+      <Box
+        sx={{
+          flexShrink: 0,
+          py: 1,
+          px: 5,
+          display: 'flex',
+          justifyContent: 'flex-end'
+        }}
+      >
+        <Button onClick={okButtonAction} color='primary'>
+          OK
+        </Button>
+        <Button onClick={cancelButtonAction} color='primary'>
+          Cancel
+        </Button>
+      </Box>
+    </Box>
   ) : (
+    // </Window>
+    // )
     <Dialog open={openCondition} onClose={closeCondition} fullWidth={true} maxWidth='xs'>
       <DialogTitle>Confirmation</DialogTitle>
       <DialogContent>
