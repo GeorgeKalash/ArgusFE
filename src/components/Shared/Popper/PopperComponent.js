e9f3e47b692103a8b896c0979829770c7c02872e
alihajjhassan <Ali.HajjHassan@SoftMachine.co>
2024-05-23 13:48:20 +0300

    textField

--- src/components/Shared/Popper/PopperComponent.js
+++ src/components/Shared/Popper/PopperComponent.js
@@ -1,29 +1,57 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDOM from 'react-dom'
 import { Box } from '@mui/material'
 
 const PopperComponent = ({ children, anchorEl, open }) => {
+  const [rect, setRect] = useState(anchorEl?.getBoundingClientRect())
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (anchorEl) {
+        setRect(anchorEl.getBoundingClientRect())
+      }
+    }
+
+    const handleResize = () => {
+      if (anchorEl) {
+        setRect(anchorEl.getBoundingClientRect())
+      }
+    }
+
+    const mutationObserver = new MutationObserver(() => {
+      handleResize()
+    })
+    if (anchorEl) {
+      mutationObserver.observe(anchorEl, { attributes: true, childList: true, subtree: true })
+    }
+
+    window.addEventListener('scroll', handleScroll, true)
+
+    return () => {
+      if (anchorEl) {
+        mutationObserver.disconnect()
+      }
+      window.removeEventListener('scroll', handleScroll, true)
+    }
+  }, [anchorEl])
+
   const zoom = parseFloat(getComputedStyle(document.documentElement).getPropertyValue('--zoom'))
   const thresholdPercentage = 0.25
 
-  const canRenderBelow =
-    window.innerHeight / zoom - (anchorEl && anchorEl.getBoundingClientRect().bottom) >
-    window.innerHeight * thresholdPercentage
-
-  const rect = anchorEl && anchorEl.getBoundingClientRect()
+  const canRenderBelow = window.innerHeight / zoom - (rect && rect.bottom) > window.innerHeight * thresholdPercentage
 
   return ReactDOM.createPortal(
     <Box
       sx={{
-        zIndex:'2 !important',
+        zIndex: '2 !important',
         display: open ? 'block' : 'none'
       }}
       style={{
         position: 'absolute',
         minWidth: anchorEl ? anchorEl.clientWidth : 'auto',
-        top: rect.bottom,
-        left: rect.left,
-        transform: !canRenderBelow ? `translateY(calc(-100% - 10px - ${rect.height}px))` : 'none'
+        top: rect?.bottom,
+        left: rect?.left,
+        transform: !canRenderBelow ? `translateY(calc(-100% - 10px - ${rect?.height}px))` : 'none'
       }}
     >
       {children}
