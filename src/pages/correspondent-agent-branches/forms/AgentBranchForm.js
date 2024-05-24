6b907f5d94ccb7eca988250a17caa5d9841e640f
FatimaSalami <fatima.salami@softmachine.co>
2024-05-24 16:39:30 +0300

    TOCREATEREQ

--- src/pages/correspondent-agent-branches/forms/AgentBranchForm.js
+++ src/pages/correspondent-agent-branches/forms/AgentBranchForm.js
@@ -50,6 +50,7 @@ export default function AgentBranchForm({ _labels, maxAccess, store, setStore, e
       if (response.recordId) {
         toast.success('Record Added Successfully')
         setStore(prevStore => ({
+          //store
           ...prevStore,
           agentBranch: obj,
           recordId: response.recordId
