54bcb53a01387bb11f076eefb8b29641feb14f57
RebeccaFrem02 <155422197+RebeccaFrem02@users.noreply.github.com>
2024-05-24 13:07:19 +0300

    tt

--- src/pages/user-dashboard/index.js
+++ src/pages/user-dashboard/index.js
@@ -1,8 +1,8 @@
-import { useEffect, useState, useContext } from 'react'
+import { useEffect, useState, useContext, useRef } from 'react'
 import { RequestsContext } from 'src/providers/RequestsContext'
 import { DashboardRepository } from 'src/repositories/DashboardRepository'
 import styled, { createGlobalStyle } from 'styled-components'
-import { BarChart } from '@mui/x-charts/BarChart'
+import Chart from 'chart.js/auto'
 
 const GlobalStyle = createGlobalStyle`
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:700,600,300');
@@ -175,8 +175,95 @@ const UserDashboard = () => {
     newClientsAcquired: 0
   })
 
+  const chart1Ref = useRef(null)
+  const chart2Ref = useRef(null)
+
   useEffect(() => {
     getDataResult()
+  }, [])
+
+  useEffect(() => {
+    if (chart1Ref.current && chart2Ref.current) {
+      const ctx1 = chart1Ref.current.getContext('2d')
+      const ctx2 = chart2Ref.current.getContext('2d')
+
+      const chart1 = new Chart(ctx1, {
+        type: 'bar',
+        data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr'],
+          datasets: [
+            {
+              label: 'Units Sold',
+              data: [10, 20, 30, 40],
+              backgroundColor: ['#93C6E0', '#5DA9D4', '#378DC8', '#176FB5'],
+              borderColor: ['#176FB5', '#176FB5', '#176FB5', '#176FB5'],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: 'Units Sold',
+              font: {
+                size: 18,
+                weight: 'bold'
+              },
+              color: '#FFFFFF'
+            }
+          }
+        }
+      })
+
+      const chart2 = new Chart(ctx2, {
+        type: 'bar',
+        data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr'],
+          datasets: [
+            {
+              label: 'Clients Acquired',
+              data: [5, 15, 25, 35],
+              backgroundColor: ['#93C6E0', '#5DA9D4', '#378DC8', '#176FB5'],
+              borderColor: ['#176FB5', '#176FB5', '#176FB5', '#176FB5'],
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          },
+          plugins: {
+            title: {
+              display: true,
+              text: 'Clients Acquired',
+              font: {
+                size: 18,
+                weight: 'bold'
+              },
+              color: '#FFFFFF'
+            }
+          }
+        }
+      })
+
+      return () => {
+        chart1.destroy()
+        chart2.destroy()
+      }
+    }
   }, [])
 
   const getDataResult = () => {
@@ -225,28 +312,9 @@ const UserDashboard = () => {
           </Profile>
           <SideData className='right-data'>
             <DataHalf>
-              <Span className='big'>Yearly Growth</Span>
               <ChartContainer>
-                <BarChart
-                  series={[
-                    {
-                      label: 'Units Sold',
-                      data: data.myYearlyGrowthInUnitsSoldList.map(item => ({
-                        x: item.month,
-                        y: item.unitsSold
-                      }))
-                    },
-                    {
-                      label: 'Clients Acquired',
-                      data: data.myYearlyGrowthInClientsAcquiredList.map(item => ({
-                        x: item.month,
-                        y: item.clientsAcquired
-                      }))
-                    }
-                  ]}
-                  xAxis={[{ scaleType: 'band', dataKey: 'x' }]}
-                  yAxis={[{ scaleType: 'linear', dataKey: 'y' }]}
-                />
+                <canvas ref={chart1Ref} id='chart1'></canvas>
+                <canvas ref={chart2Ref} id='chart2'></canvas>
               </ChartContainer>
             </DataHalf>
             <DataHalf>
