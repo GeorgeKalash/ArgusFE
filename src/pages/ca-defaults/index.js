ecbb09ef9eb23248ba7f38011ad37458bacea129
Alinadimmokdad <alimokdad961@gmail.com>
2024-05-27 00:21:39 +0300

    miss label name

---
+++ src/pages/ca-defaults/index.js
@@ -0,0 +1,98 @@
+import { useEffect, useState, useContext } from 'react'
+import { Grid, Box } from '@mui/material'
+import { useFormik } from 'formik'
+import toast from 'react-hot-toast'
+import WindowToolbar from 'src/components/Shared/WindowToolbar'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { SystemRepository } from 'src/repositories/SystemRepository'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { useResourceQuery } from 'src/hooks/resource'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Fixed } from 'src/components/Shared/Layouts/Fixed'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import CustomNumberField from 'src/components/Inputs/CustomNumberField'
+import { useForm } from 'src/hooks/form'
+
+const CADefault = () => {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+
+  useEffect(() => {
+    getDataResult()
+  }, [])
+
+  const getDataResult = () => {
+    const myObject = {}
+    var parameters = `_filter=`
+    getRequest({
+      extension: SystemRepository.Defaults.qry,
+      parameters: parameters
+    })
+      .then(res => {
+        const filteredList = res.list.filter(obj => {
+          return obj.key === 'min_catfr_approval'
+        })
+        filteredList.forEach(obj => (myObject[obj.key] = obj.value ? parseInt(obj.value) : null))
+        formik.setValues(myObject)
+      })
+      .catch(error => {})
+  }
+
+  const { labels: _labels, access } = useResourceQuery({
+    datasetId: ResourceIds.CADefaults
+  })
+
+  const { formik } = useForm({
+    enableReinitialize: true,
+    validateOnChange: true,
+    initialValues: {
+      min_catfr_approval: null
+    },
+    onSubmit: values => {
+      postCADefault(values)
+    }
+  })
+
+  const postCADefault = obj => {
+    var data = []
+    Object.entries(obj).forEach(([key, value]) => {
+      const newObj = { key: key, value: value }
+      data.push(newObj)
+    })
+    postRequest({
+      extension: SystemRepository.Defaults.set,
+      record: JSON.stringify({ sysDefaults: data })
+    })
+      .then(res => {
+        if (res) toast.success('Record Successfully')
+      })
+      .catch(error => {})
+  }
+
+  const handleSubmit = () => {
+    formik.handleSubmit()
+  }
+
+  return (
+    <VertLayout>
+      <Grow>
+        <Grid container spacing={5} sx={{ pl: '10px', pt: '10px' }} lg={4} md={7} sm={7} xs={12}>
+          <Grid item xs={12}>
+            <CustomNumberField
+              onClear={() => formik.setFieldValue('min_catfr_approval', '')}
+              name='min_catfr_approval'
+              onChange={formik.handleChange}
+              label={_labels.netWgt}
+              value={formik.values.min_catfr_approval}
+              error={formik.touched.min_catfr_approval && Boolean(formik.errors.min_catfr_approval)}
+            />
+          </Grid>
+        </Grid>
+      </Grow>
+      <Fixed>
+        <WindowToolbar onSave={handleSubmit} isSaved={true} />
+      </Fixed>
+    </VertLayout>
+  )
+}
+
+export default CADefault
