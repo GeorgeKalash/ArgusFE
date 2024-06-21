b38ec134596154867eaa31485e8870ab3b5f8c40
Alinadimmokdad <alimokdad961@gmail.com>
2024-06-21 10:53:46 +0300

    fi-dim-values

---
+++ src/pages/fi-dim-values/form/DimValuesForm.js
@@ -0,0 +1,99 @@
+import { Grid } from '@mui/material'
+import { useContext, useEffect, useState } from 'react'
+import * as yup from 'yup'
+import FormShell from 'src/components/Shared/FormShell'
+import toast from 'react-hot-toast'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useInvalidate } from 'src/hooks/resource'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import CustomTextField from 'src/components/Inputs/CustomTextField'
+import { CurrencyTradingSettingsRepository } from 'src/repositories/CurrencyTradingSettingsRepository'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useForm } from 'src/hooks/form'
+import CustomNumberField from 'src/components/Inputs/CustomNumberField'
+import { FinancialRepository } from 'src/repositories/FinancialRepository'
+
+export default function DimValuesForm({ labels, maxAccess, dimensionId, id, invalidate }) {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+
+  const { formik } = useForm({
+    initialValues: { id: id, name: '', dimension: dimensionId },
+    maxAccess,
+    enableReinitialize: true,
+    validateOnChange: true,
+    validationSchema: yup.object({
+      name: yup.string().required(' '),
+      id: yup.string().required(' ')
+    }),
+    onSubmit: async obj => {
+      const id = formik.values.id
+
+      const response = await postRequest({
+        extension: FinancialRepository.DimensionValue.set,
+        record: JSON.stringify(obj)
+      })
+
+      if (!id) {
+        toast.success('Record Added Successfully')
+      } else toast.success('Record Edited Successfully')
+      formik.setValues({
+        ...obj,
+        id: obj.id
+      })
+
+      invalidate()
+    }
+  })
+
+  useEffect(() => {
+    ;(async function () {
+      try {
+        if (dimensionId && id) {
+          const res = await getRequest({
+            extension: FinancialRepository.DimensionValue.get,
+            parameters: `_Id=${formik.values.id}&_dimension=${dimensionId}`
+          })
+
+          formik.setValues(res.record)
+        }
+      } catch (exception) {}
+    })()
+  }, [])
+
+  return (
+    <FormShell resourceId={ResourceIds.DimensionsValues} form={formik} maxAccess={maxAccess} infoVisible={false}>
+      <VertLayout>
+        <Grow>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <CustomNumberField
+                name='id'
+                label={labels.id}
+                value={formik.values.id}
+                required
+                maxAccess={maxAccess}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('id', '')}
+                error={formik.touched.id && Boolean(formik.errors.id)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomTextField
+                name='name'
+                label={labels.name}
+                value={formik.values.name}
+                required
+                rows={2}
+                maxAccess={maxAccess}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('name', '')}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+              />
+            </Grid>
+          </Grid>
+        </Grow>
+      </VertLayout>
+    </FormShell>
+  )
+}
---
+++ src/pages/fi-dim-values/index.js
@@ -0,0 +1,176 @@
+import { useContext, useEffect, useState } from 'react'
+import toast from 'react-hot-toast'
+import Table from 'src/components/Shared/Table'
+import GridToolbar from 'src/components/Shared/GridToolbar'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { useResourceQuery } from 'src/hooks/resource'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Fixed } from 'src/components/Shared/Layouts/Fixed'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useWindow } from 'src/windows'
+import { ControlContext } from 'src/providers/ControlContext'
+import { FinancialRepository } from 'src/repositories/FinancialRepository'
+import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
+import { SystemRepository } from 'src/repositories/SystemRepository'
+import DimValuesForm from './form/DimValuesForm'
+import { Grid } from '@mui/material'
+
+const DimensionsValues = () => {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
+  const { stack } = useWindow()
+  const [tpaValues, setTpaValues] = useState([])
+  const [error, setError] = useState(false)
+
+  const [selectedTpaValue, setSelectedTpaValue] = useState({ key: null, value: '' })
+  const formatedRecordId = typeof selectedTpaValue?.key == 'string' ? selectedTpaValue.key.match(/\d+/)?.[0] : null
+
+  const {
+    query: { data },
+    labels: _labels,
+    paginationParameters,
+    refetch,
+    invalidate,
+    filterBy,
+    clearFilter,
+    access
+  } = useResourceQuery({
+    datasetId: ResourceIds.DimensionsValues,
+    filter: {
+      endpointId: FinancialRepository.DimensionValue.qry,
+      filterFn: fetchWithSearch
+    }
+  })
+
+  useEffect(() => {
+    if (formatedRecordId !== null) {
+      filterBy('qry', formatedRecordId)
+    } else {
+      clearFilter('qry')
+    }
+  }, [formatedRecordId])
+
+  async function fetchWithSearch({ filters }) {
+    if (formatedRecordId) {
+      const data = await getRequest({
+        extension: FinancialRepository.DimensionValue.qry,
+        parameters: `_filter=${filters.qry}&_dimension=${formatedRecordId}`
+      })
+
+      return data
+    }
+  }
+
+  const columns = [
+    {
+      field: 'id',
+      headerName: _labels.id,
+      flex: 1
+    },
+    {
+      field: 'name',
+      headerName: _labels.name,
+      flex: 1
+    }
+  ]
+
+  const del = async obj => {
+    try {
+      await postRequest({
+        extension: FinancialRepository.DimensionValue.del,
+        record: JSON.stringify(obj)
+      })
+      invalidate()
+      toast.success(platformLabels.Deleted)
+    } catch (error) {
+      toast.error(platformLabels.Error)
+    }
+  }
+
+  const add = () => {
+    if (formatedRecordId) {
+      openForm()
+    } else {
+      setError(true)
+    }
+  }
+
+  const edit = obj => {
+    openForm(obj?.id)
+  }
+
+  function openForm(id) {
+    stack({
+      Component: DimValuesForm,
+      props: {
+        labels: _labels,
+        id: id,
+        maxAccess: access,
+        invalidate,
+        dimensionId: formatedRecordId
+      },
+      width: 600,
+      height: 500,
+      title: _labels.dimValues
+    })
+  }
+  const emptyValues = item => item.value !== null && item.value !== ''
+
+  useEffect(() => {
+    ;(async () => {
+      const data = await getRequest({
+        extension: SystemRepository.Defaults.qry,
+        parameters: `_filter=tpaDimension`
+      })
+
+      if (data) {
+        const result = data.list.filter(emptyValues)
+        setTpaValues(result)
+        setSelectedTpaValue(result[0])
+      }
+    })()
+  }, [])
+
+  return (
+    <VertLayout>
+      <Fixed>
+        <GridToolbar onAdd={add} maxAccess={access} labels={_labels} />
+        <Grid container>
+          <Grid item sx={{ marginLeft: '1rem' }} xs={6}>
+            <ResourceComboBox
+              label={_labels.dimensions}
+              filter={emptyValues}
+              valueField='key'
+              displayField={['value']}
+              store={tpaValues}
+              value={selectedTpaValue}
+              maxAccess={access}
+              onChange={(event, newValue) => {
+                setSelectedTpaValue(newValue)
+                setError(false)
+              }}
+              error={error}
+            />
+          </Grid>
+        </Grid>
+      </Fixed>
+      <Grow>
+        <Table
+          columns={columns}
+          gridData={formatedRecordId ? data : { list: [], count: 0 }}
+          rowId={['id']}
+          onEdit={edit}
+          onDelete={del}
+          pageSize={50}
+          maxAccess={access}
+          refetch={refetch}
+          paginationParameters={paginationParameters}
+          paginationType='api'
+        />
+      </Grow>
+    </VertLayout>
+  )
+}
+
+export default DimensionsValues
