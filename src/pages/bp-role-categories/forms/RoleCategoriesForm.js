4958010cf68af84647b6d6d31b58f766967468ed
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-07-04 11:04:09 +0300

    removing unused useState

--- src/pages/bp-role-categories/forms/RoleCategoriesForm.js
+++ src/pages/bp-role-categories/forms/RoleCategoriesForm.js
@@ -1,5 +1,5 @@
 import { Grid } from '@mui/material'
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import * as yup from 'yup'
 import FormShell from 'src/components/Shared/FormShell'
 import toast from 'react-hot-toast'
@@ -57,7 +57,7 @@ export default function RoleCategoriesForm({ labels, maxAccess, recordId }) {
   })
 
   const editMode = !!formik.values.recordId || !!recordId;
-  
+
   useEffect(() => {
     ;(async function () {
       try {
