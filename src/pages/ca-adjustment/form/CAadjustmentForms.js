6bbb95805dbd73a0de8c438c868cbab4eeb97ced
Alinadimmokdad <alimokdad961@gmail.com>
2024-07-05 10:53:25 +0300

    hadi's changes request

--- src/pages/ca-adjustment/form/CAadjustmentForms.js
+++ src/pages/ca-adjustment/form/CAadjustmentForms.js
@@ -21,6 +21,7 @@ import CustomTextArea from 'src/components/Inputs/CustomTextArea'
 import { useDocumentType } from 'src/hooks/documentReferenceBehaviors'
 import { useWindow } from 'src/windows'
 import WorkFlow from 'src/components/Shared/WorkFlow'
+import { ControlContext } from 'src/providers/ControlContext'
 
 export default function CAadjustmentForm({ labels, access, recordId, functionId }) {
   const { documentType, maxAccess, changeDT } = useDocumentType({
@@ -28,6 +29,8 @@ export default function CAadjustmentForm({ labels, access, recordId, functionId 
     access: access,
     enabled: !recordId
   })
+  const { platformLabels } = useContext(ControlContext)
+
   const { stack } = useWindow()
 
   const { getRequest, postRequest } = useContext(RequestsContext)
@@ -78,7 +81,7 @@ export default function CAadjustmentForm({ labels, access, recordId, functionId 
       })
 
       if (!recordId) {
-        toast.success('Record Added Successfully')
+        toast.success(platformLabels.Added)
         formik.setValues({
           ...obj,
           baseAmount: obj.amount,
@@ -86,7 +89,7 @@ export default function CAadjustmentForm({ labels, access, recordId, functionId 
           recordId: response.recordId
         })
       } else {
-        toast.success('Record Edited Successfully')
+        toast.success(platformLabels.Edited)
       }
 
       try {
@@ -129,7 +132,7 @@ export default function CAadjustmentForm({ labels, access, recordId, functionId 
       })
 
       if (res?.recordId) {
-        toast.success('Record Posted Successfully')
+        toast.success(platformLabels.Posted)
         invalidate()
 
         const getRes = await getRequest({
