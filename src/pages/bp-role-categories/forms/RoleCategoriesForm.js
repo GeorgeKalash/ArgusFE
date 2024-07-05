e1b6896b63b1259645c1996551a9b1b26f7c806a
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-07-05 08:46:16 +0300

    adding VertLayout and Grow

--- src/pages/bp-role-categories/forms/RoleCategoriesForm.js
+++ src/pages/bp-role-categories/forms/RoleCategoriesForm.js
@@ -11,6 +11,8 @@ import CustomTextField from 'src/components/Inputs/CustomTextField'
 import { BusinessPartnerRepository } from 'src/repositories/BusinessPartnerRepository'
 import { useForm } from 'src/hooks/form'
 import { ControlContext } from 'src/providers/ControlContext'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
 
 export default function RoleCategoriesForm({ labels, maxAccess, recordId }) {
   const { getRequest, postRequest } = useContext(RequestsContext)
@@ -75,73 +77,78 @@ export default function RoleCategoriesForm({ labels, maxAccess, recordId }) {
 
   return (
     <FormShell resourceId={ResourceIds.RoleCategories} form={formik} maxAccess={maxAccess} editMode={editMode}>
-      <Grid container spacing={4}>
-        <Grid item xs={12}>
-          <CustomTextField
-            name='reference'
-            label={labels.reference}
-            value={formik.values.reference}
-            required
-            maxAccess={maxAccess}
-            onChange={formik.handleChange}
-            onClear={() => formik.setFieldValue('reference', '')}
-            error={formik.touched.reference && Boolean(formik.errors.reference)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <CustomTextField
-            name='name'
-            label={labels.name}
-            value={formik.values.name}
-            required
-            maxAccess={maxAccess}
-            onChange={formik.handleChange}
-            onClear={() => formik.setFieldValue('name', '')}
-            error={formik.touched.name && Boolean(formik.errors.name)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-                <Checkbox 
-                    name='org' 
-                    checked={formik.values.org} 
-                    onChange={formik.handleChange} 
-                    maxAccess={maxAccess} 
-                />
-            }
-            label={labels.organization}
-          />
-        </Grid>
-
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                name='person'
-                checked={formik.values.person}
-                onChange={formik.handleChange}
+      <VertLayout>
+        <Grow>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <CustomTextField
+                name='reference'
+                label={labels.reference}
+                value={formik.values.reference}
+                required
                 maxAccess={maxAccess}
-              />
-            }
-            label={labels.person}
-          />
-        </Grid>
-
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                name='group'
-                checked={formik.values.group}
                 onChange={formik.handleChange}
-                maxAccess={maxAccess}
+                onClear={() => formik.setFieldValue('reference', '')}
+                error={formik.touched.reference && Boolean(formik.errors.reference)}
               />
-            }
-            label={labels.group}
-          />
-        </Grid>
-      </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <CustomTextField
+                name='name'
+                label={labels.name}
+                value={formik.values.name}
+                required
+                maxAccess={maxAccess}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('name', '')}
+                error={formik.touched.name && Boolean(formik.errors.name)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                    <Checkbox 
+                        name='org' 
+                        checked={formik.values.org} 
+                        onChange={formik.handleChange} 
+                        maxAccess={maxAccess} 
+                    />
+                }
+                label={labels.organization}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name='person'
+                    checked={formik.values.person}
+                    onChange={formik.handleChange}
+                    maxAccess={maxAccess}
+                  />
+                }
+                label={labels.person}
+              />
+            </Grid>
+
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name='group'
+                    checked={formik.values.group}
+                    onChange={formik.handleChange}
+                    maxAccess={maxAccess}
+                  />
+                }
+                label={labels.group}
+              />
+            </Grid>
+          </Grid>
+        </Grow>
+      </VertLayout>
+      
     </FormShell>
   )
 }
