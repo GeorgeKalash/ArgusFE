d38b61a4d3e4ae04e017f1ed71733752a46543f4
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-07-17 12:17:59 +0300

    removing console log

--- src/pages/fi-pv-expenses/forms/PaymentVoucherExpensesForm.js
+++ src/pages/fi-pv-expenses/forms/PaymentVoucherExpensesForm.js
@@ -474,7 +474,6 @@ export default function FiPaymentVoucherExpensesForm({ labels, maxAccess: access
     formik.setFieldValue('baseAmount', amountSum);
   }, [formik?.values?.expenses]);
 
-  console.log(formik)
 
   return (
     <FormShell 
