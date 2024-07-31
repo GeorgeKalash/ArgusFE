11e697b047e43f07cb3bad46ba2320af0539d1a9
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-07-31 17:04:11 +0300

    remove console.log

--- src/components/Shared/Table.js
+++ src/components/Shared/Table.js
@@ -466,7 +466,6 @@ const Table = ({
   const onFirstDataRendered = async params => {
     params.api.sizeColumnsToFit()
     await params.api.forEachNode(node => {
-      console.log('node?.data?.checked', node?.data)
       if (rowSelection === 'single') {
         const checked = node.data?.checked || false
         node.setDataValue('checked', checked)
