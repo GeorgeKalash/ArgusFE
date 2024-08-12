4a3f898664b0559e9f96f2682326a56526c58aba
Alinadimmokdad <alimokdad961@gmail.com>
2024-08-12 09:14:54 +0300

    purchase order status

---
+++ src/pages/pu-order-status/index.js
@@ -0,0 +1,112 @@
+import { useContext } from 'react'
+import toast from 'react-hot-toast'
+import Table from 'src/components/Shared/Table'
+import GridToolbar from 'src/components/Shared/GridToolbar'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useWindow } from 'src/windows'
+import { useResourceQuery } from 'src/hooks/resource'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Fixed } from 'src/components/Shared/Layouts/Fixed'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { ControlContext } from 'src/providers/ControlContext'
+import { PurchaseRepository } from 'src/repositories/PurchaseRepository'
+import PUOrderStatusForm from './forms/OrderStatusForm'
+
+const PUOrderStatus = () => {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
+  const { stack } = useWindow()
+
+  async function fetchGridData(options = {}) {
+    const { _startAt = 0, _pageSize = 50 } = options
+
+    try {
+      const response = await getRequest({
+        extension: PurchaseRepository.PUOrderStatus.qry,
+        parameters: `_startAt=${_startAt}&_pageSize=${_pageSize}&_filter=&_sortField=`
+      })
+
+      return { ...response, _startAt: _startAt }
+    } catch (error) {}
+  }
+
+  const {
+    query: { data },
+    labels: _labels,
+    invalidate,
+    paginationParameters,
+    refetch,
+    access
+  } = useResourceQuery({
+    queryFn: fetchGridData,
+    endpointId: PurchaseRepository.PUOrderStatus.qry,
+    datasetId: ResourceIds.PUOrderStatus
+  })
+
+  const columns = [
+    {
+      field: 'name',
+      headerName: _labels.name,
+      flex: 1
+    }
+  ]
+
+  const add = () => {
+    openForm()
+  }
+
+  const del = async obj => {
+    try {
+      await postRequest({
+        extension: PurchaseRepository.PUOrderStatus.del,
+        record: JSON.stringify(obj)
+      })
+      invalidate()
+      toast.success(platformLabels.Deleted)
+    } catch (error) {}
+  }
+
+  function openForm(recordId) {
+    stack({
+      Component: PUOrderStatusForm,
+      props: {
+        labels: _labels,
+        recordId: recordId,
+        maxAccess: access
+      },
+      width: 600,
+      height: 250,
+      title: _labels.pos
+    })
+  }
+
+  const edit = obj => {
+    openForm(obj?.recordId)
+  }
+
+  return (
+    <VertLayout>
+      <Fixed>
+        <GridToolbar onAdd={add} maxAccess={access} />
+      </Fixed>
+      <Grow>
+        <Table
+          columns={columns}
+          gridData={data}
+          rowId={['recordId']}
+          onEdit={edit}
+          onDelete={del}
+          isLoading={false}
+          pageSize={50}
+          refetch={refetch}
+          paginationParameters={paginationParameters}
+          paginationType='api'
+          maxAccess={access}
+        />
+      </Grow>
+    </VertLayout>
+  )
+}
+
+export default PUOrderStatus
--- src/repositories/PurchaseRepository.js
+++ src/repositories/PurchaseRepository.js
@@ -7,5 +7,12 @@ export const PurchaseRepository = {
     get: service + 'getGRP',
     set: service + 'setGRP',
     del: service + 'delGRP'
+  },
+  PUOrderStatus: {
+    qry: service + 'qryPOS',
+    page: service + 'pagePOS',
+    get: service + 'getPOS',
+    set: service + 'setPOS',
+    del: service + 'delPOS'
   }
 }
