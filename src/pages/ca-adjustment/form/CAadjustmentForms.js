78639c627bdb5f35443195e9d56948804e0986e0
Alinadimmokdad <alimokdad961@gmail.com>
2024-08-13 14:13:21 +0300

    rem changes

--- src/pages/ca-adjustment/form/CAadjustmentForms.js
+++ src/pages/ca-adjustment/form/CAadjustmentForms.js
@@ -48,7 +48,7 @@ export default function CAadjustmentForm({ labels, access, recordId, functionId 
       plantId: '',
       date: new Date(),
       currencyId: '',
-      status: 1,
+      status: '',
       cashAccountId: '',
       amount: '',
       baseAmount: '',
@@ -64,7 +64,6 @@ export default function CAadjustmentForm({ labels, access, recordId, functionId 
       amount: yup.string().required(' '),
       currencyId: yup.string().required(' '),
       cashAccountId: yup.string().required(' '),
-      dtId: yup.string().required(' '),
       date: yup.string().required(' ')
     }),
     onSubmit: async obj => {
@@ -225,6 +224,7 @@ export default function CAadjustmentForm({ labels, access, recordId, functionId 
                 onChange={(event, newValue) => {
                   changeDT(newValue)
                   formik && formik.setFieldValue('dtId', newValue?.recordId)
+                  formik && formik.setFieldValue('status', newValue?.activeStatus)
                 }}
                 error={formik.touched.dtId && Boolean(formik.errors.dtId)}
               />
