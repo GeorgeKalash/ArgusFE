d39e03b52f0da431f9cdec77005f7a39a2b34ee1
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2024-08-16 12:16:42 +0300

    removeDuplicateRepository-RTOW

--- src/repositories/RemittanceOutwardsRepository.js
+++ src/repositories/RemittanceOutwardsRepository.js
@@ -76,5 +76,16 @@ export const RemittanceOutwardsRepository = {
   },
   OutwardGLInformation: {
     get: service + 'getOWI'
+  },
+  OutwardsModification: {
+    qry: service + 'qryOWM',
+    page: service + 'pageOWM',
+    snapshot: service + 'snapshotOWM',
+    get: service + 'getOWM',
+    set2: service + 'set2OWM',
+    del: service + 'delOWM',
+    post: service + 'postOWM',
+    reopen: service + 'reopenOWM',
+    close: service + 'closeOWM'
   }
 }
