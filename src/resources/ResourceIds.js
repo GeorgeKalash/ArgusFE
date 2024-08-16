3816679ffda08e672f70f380d39aeb2ac140fedc
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-08-16 10:04:02 +0300

    adding rt-cor-group page

--- src/resources/ResourceIds.js
+++ src/resources/ResourceIds.js
@@ -219,5 +219,6 @@ export const ResourceIds = {
   NotificationTransaction: 23301, // Notification center
   MFJobOrders: 42302,
   IVDimension: 41106,
-  FIDimValues: 31111
+  FIDimValues: 31111,
+  CorrespondentGroup: 36125
 }
