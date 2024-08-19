107e3128da8f17af9788d81ff53852f5d757b972
Alinadimmokdad <alimokdad961@gmail.com>
2024-08-19 10:13:33 +0300

    fix clear

--- src/pages/sa-ord-dtd/forms/DocumentTypeDefaultForm.js
+++ src/pages/sa-ord-dtd/forms/DocumentTypeDefaultForm.js
@@ -16,7 +16,7 @@ import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
 import { SystemRepository } from 'src/repositories/SystemRepository'
 import { DataSets } from 'src/resources/DataSets'
 
-export default function DocumentTypeDefaultForm({ labels, maxAccess, dtId }) {
+export default function DocumentTypeDefaultForm({ labels, maxAccess, recordId }) {
   const { getRequest, postRequest } = useContext(RequestsContext)
   const { platformLabels } = useContext(ControlContext)
 
@@ -26,8 +26,8 @@ export default function DocumentTypeDefaultForm({ labels, maxAccess, dtId }) {
 
   const { formik } = useForm({
     initialValues: {
-      dtId: dtId || null,
-      recordId: '',
+      dtId: '',
+      recordId: recordId || null,
       spId: '',
       commitItems: false,
       disableSKULookup: false,
@@ -56,16 +56,17 @@ export default function DocumentTypeDefaultForm({ labels, maxAccess, dtId }) {
     }
   })
   const editMode = !!formik.values.recordId
+  console.log(recordId)
   useEffect(() => {
     ;(async function () {
       try {
-        if (dtId) {
+        if (recordId) {
           const res = await getRequest({
             extension: SaleRepository.DocumentTypeDefault.get,
-            parameters: `_dtId=${dtId}`
+            parameters: `_dtId=${recordId}`
           })
 
-          formik.setValues({ ...res.record, recordId: dtId })
+          formik.setValues({ ...res.record, recordId: recordId })
         }
       } catch (error) {}
     })()
