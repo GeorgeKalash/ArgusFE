0417f0de9689e48e53c7ab5dc6bc708633e4e78e
FatimaSalami <fatima.salami@softmachine.co>
2024-08-22 15:56:00 +0300

    REM-128: release code refactoring to solve clear button bug

--- src/pages/release-codes/forms/ReleaseCodeForm.js
+++ src/pages/release-codes/forms/ReleaseCodeForm.js
@@ -1,66 +1,55 @@
 // ** MUI Imports
 import { Grid } from '@mui/material'
-import { useContext, useEffect, useState } from 'react'
-import { useFormik } from 'formik'
+import { useContext, useEffect } from 'react'
+import { useForm } from 'src/hooks/form'
 import * as yup from 'yup'
 import FormShell from 'src/components/Shared/FormShell'
 import toast from 'react-hot-toast'
 import { RequestsContext } from 'src/providers/RequestsContext'
 import { useInvalidate } from 'src/hooks/resource'
 import { ResourceIds } from 'src/resources/ResourceIds'
-
-// ** Custom Imports
 import CustomTextField from 'src/components/Inputs/CustomTextField'
-import CustomTextArea from 'src/components/Inputs/CustomTextArea'
-
 import { DocumentReleaseRepository } from 'src/repositories/DocumentReleaseRepository'
 import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
 import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { ControlContext } from 'src/providers/ControlContext'
 
 export default function ReleaseCodeForm({ labels, maxAccess, recordId }) {
-  const [isLoading, setIsLoading] = useState(false)
-  const [editMode, setEditMode] = useState(!!recordId)
-
-  const [initialValues, setInitialData] = useState({
-    recordId: null,
-    name: '',
-    reference: ''
-  })
-
+  const { platformLabels } = useContext(ControlContext)
   const { getRequest, postRequest } = useContext(RequestsContext)
 
-  //const editMode = !!recordId
+  const editMode = !!recordId
 
   const invalidate = useInvalidate({
     endpointId: DocumentReleaseRepository.ReleaseCode.page
   })
 
-  const formik = useFormik({
-    initialValues,
+  const { formik } = useForm({
+    initialValues: {
+      recordId: recordId || null,
+      reference: '',
+      name: ''
+    },
     enableReinitialize: true,
     validateOnChange: true,
     validationSchema: yup.object({
-      reference: yup.string().required(' '),
-      name: yup.string().required(' ')
+      reference: yup.string().required(),
+      name: yup.string().required()
     }),
     onSubmit: async obj => {
-      const recordId = obj.recordId
-
-      const response = await postRequest({
+      await postRequest({
         extension: DocumentReleaseRepository.ReleaseCode.set,
         record: JSON.stringify(obj)
       })
+        .then(response => {
+          if (!obj.recordId) {
+            toast.success(platformLabels.Added)
+            formik.setFieldValue('recordId', response.recordId)
+          } else toast.success(platformLabels.Edited)
 
-      if (!recordId) {
-        toast.success('Record Added Successfully')
-        setInitialData({
-          ...obj, // Spread the existing properties
-          recordId: response.recordId // Update only the recordId field
+          invalidate()
         })
-      } else toast.success('Record Edited Successfully')
-      setEditMode(true)
-
-      invalidate()
+        .catch(error => {})
     }
   })
 
@@ -68,19 +57,14 @@ export default function ReleaseCodeForm({ labels, maxAccess, recordId }) {
     ;(async function () {
       try {
         if (recordId) {
-          setIsLoading(true)
-
           const res = await getRequest({
             extension: DocumentReleaseRepository.ReleaseCode.get,
             parameters: `_recordId=${recordId}`
           })
 
-          setInitialData(res.record)
+          formik.setValues(res.record)
         }
-      } catch (exception) {
-        setErrorMessage(error)
-      }
-      setIsLoading(false)
+      } catch (error) {}
     })()
   }, [])
 
