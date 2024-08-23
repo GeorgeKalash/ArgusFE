8d88ba130a6eec97c38a7b678ffcc5086a652cfd
Alinadimmokdad <alimokdad961@gmail.com>
2024-08-23 12:23:25 +0300

    fix style

--- src/pages/group-legal-document/forms/GroupLegalDocumentForm.js
+++ src/pages/group-legal-document/forms/GroupLegalDocumentForm.js
@@ -9,6 +9,8 @@ import { useInvalidate } from 'src/hooks/resource'
 import { ResourceIds } from 'src/resources/ResourceIds'
 import { BusinessPartnerRepository } from 'src/repositories/BusinessPartnerRepository'
 import { useForm } from 'src/hooks/form'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
 
 export default function GroupLegalDocumentForm({ labels, maxAccess, recordId, record }) {
   const { getRequest, postRequest } = useContext(RequestsContext)
@@ -78,78 +80,82 @@ export default function GroupLegalDocumentForm({ labels, maxAccess, recordId, re
       maxAccess={maxAccess}
       editMode={editMode}
     >
-      <Grid container spacing={4}>
-        <Grid item xs={12}>
-          <ResourceComboBox
-            readOnly={editMode}
-            endpointId={BusinessPartnerRepository.Group.qry}
-            name='groupId'
-            label={labels.group}
-            valueField='recordId'
-            displayField={['reference', 'name']}
-            columnsInDropDown={[
-              { key: 'reference', value: ' Ref' },
-              { key: 'name', value: 'Name' }
-            ]}
-            values={formik.values}
-            required
-            maxAccess={maxAccess}
-            onChange={(event, newValue) => {
-              formik && formik.setFieldValue('groupId', newValue?.recordId)
-            }}
-            error={formik.touched.groupId && Boolean(formik.errors.groupId)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <ResourceComboBox
-            readOnly={editMode}
-            endpointId={BusinessPartnerRepository.CategoryID.qry}
-            name='incId'
-            label={labels.categoryId}
-            valueField='recordId'
-            displayField={'name'}
-            values={formik.values}
-            required
-            maxAccess={maxAccess}
-            onChange={(event, newValue) => {
-              formik && formik.setFieldValue('incId', newValue?.recordId)
-            }}
-            error={formik.touched.incId && Boolean(formik.errors.incId)}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                name='required'
+      <VertLayout>
+        <Grow>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                readOnly={editMode}
+                endpointId={BusinessPartnerRepository.Group.qry}
+                name='groupId'
+                label={labels.group}
                 valueField='recordId'
+                displayField={['reference', 'name']}
+                columnsInDropDown={[
+                  { key: 'reference', value: ' Ref' },
+                  { key: 'name', value: 'Name' }
+                ]}
+                values={formik.values}
+                required
                 maxAccess={maxAccess}
-                checked={formik.values.required}
-                onChange={event => {
-                  formik && formik.setFieldValue('required', event.target.checked)
+                onChange={(event, newValue) => {
+                  formik && formik.setFieldValue('groupId', newValue?.recordId)
                 }}
+                error={formik.touched.groupId && Boolean(formik.errors.groupId)}
               />
-            }
-            label={labels.required}
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                name='mandatory'
+            </Grid>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                readOnly={editMode}
+                endpointId={BusinessPartnerRepository.CategoryID.qry}
+                name='incId'
+                label={labels.categoryId}
                 valueField='recordId'
+                displayField={'name'}
+                values={formik.values}
+                required
                 maxAccess={maxAccess}
-                checked={formik.values.mandatory}
-                onChange={event => {
-                  formik && formik.setFieldValue('mandatory', event.target.checked)
+                onChange={(event, newValue) => {
+                  formik && formik.setFieldValue('incId', newValue?.recordId)
                 }}
+                error={formik.touched.incId && Boolean(formik.errors.incId)}
               />
-            }
-            label={labels.mandatory}
-          />
-        </Grid>
-      </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name='required'
+                    valueField='recordId'
+                    maxAccess={maxAccess}
+                    checked={formik.values.required}
+                    onChange={event => {
+                      formik && formik.setFieldValue('required', event.target.checked)
+                    }}
+                  />
+                }
+                label={labels.required}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    name='mandatory'
+                    valueField='recordId'
+                    maxAccess={maxAccess}
+                    checked={formik.values.mandatory}
+                    onChange={event => {
+                      formik && formik.setFieldValue('mandatory', event.target.checked)
+                    }}
+                  />
+                }
+                label={labels.mandatory}
+              />
+            </Grid>
+          </Grid>
+        </Grow>
+      </VertLayout>
     </FormShell>
   )
 }
