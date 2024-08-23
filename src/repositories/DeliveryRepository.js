76226586dadf3d9442a87231e91cf011490a86db
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-08-23 15:18:36 +0300

    vehicles page

--- src/repositories/DeliveryRepository.js
+++ src/repositories/DeliveryRepository.js
@@ -8,6 +8,7 @@ export const DeliveryRepository = {
     del: service + 'delDRV'
   },
   Vehicle: {
+    page: service + 'pageVEH',
     set: service + 'setVEH',
     get: service + 'getVEH',
     qry: service + 'qryVEH',
