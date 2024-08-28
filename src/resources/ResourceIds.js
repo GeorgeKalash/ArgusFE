add84519390c05c89da43a064fc020e6938e4117
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-08-28 12:24:02 +0300

    returning resource id

--- src/resources/ResourceIds.js
+++ src/resources/ResourceIds.js
@@ -136,7 +136,7 @@ export const ResourceIds = {
   CbBankAccounts: 33105,
   CashNote: 37101,
   OutwardsModification: 36308,
-  POSUsers: 54101,
+  POSUsers: 54104,
   TaxCodes: 31109,
   TaxSchedules: 31107,
   LOShipments: 40301,
