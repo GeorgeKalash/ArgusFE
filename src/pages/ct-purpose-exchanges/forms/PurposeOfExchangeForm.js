8945ba4cd7fb7b8dcc2699c4a08efb874cfb647b
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-08-28 15:29:36 +0300

    removing readOnly

--- src/pages/ct-purpose-exchanges/forms/PurposeOfExchangeForm.js
+++ src/pages/ct-purpose-exchanges/forms/PurposeOfExchangeForm.js
@@ -118,7 +118,6 @@ export default function PurposeOfExchangeForm({ labels, maxAccess, recordId, set
                 valueField='recordId'
                 displayField='name'
                 values={formik.values}
-                readOnly={editMode}
                 maxAccess={maxAccess}
                 onChange={(event, newValue) => {
                   formik.setFieldValue('groupId', newValue?.recordId)
