057c4062b758c6ed2e70ea78e266ab8b79c35d50
Alinadimmokdad <alimokdad961@gmail.com>
2024-09-06 10:10:12 +0300

    decrease height

--- src/pages/iv-settings/index.js
+++ src/pages/iv-settings/index.js
@@ -3,7 +3,9 @@ import { ResourceIds } from 'src/resources/ResourceIds'
 import IvSettings from './form/IvSettings'
 
 const IvSettingsIndex = () => {
-  return <ImmediateWindow datasetId={ResourceIds.IvSettings} labelKey={'ivSettings'} Component={IvSettings} />
+  return (
+    <ImmediateWindow datasetId={ResourceIds.IvSettings} labelKey={'ivSettings'} Component={IvSettings} height={300} />
+  )
 }
 
 export default IvSettingsIndex
