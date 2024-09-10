df508e9c71d584de49c2bdaf2311850f788ce495
Alinadimmokdad <alimokdad961@gmail.com>
2024-09-10 09:04:12 +0300

    preview report

--- src/pages/iv-categories/index.js
+++ src/pages/iv-categories/index.js
@@ -1,23 +1,28 @@
-import { useContext } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import toast from 'react-hot-toast'
 import Table from 'src/components/Shared/Table'
 import { RequestsContext } from 'src/providers/RequestsContext'
+import { Button, Grid, Tooltip } from '@mui/material'
 import { useWindow } from 'src/windows'
 import { useResourceQuery } from 'src/hooks/resource'
 import { ResourceIds } from 'src/resources/ResourceIds'
 import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
 import { Fixed } from 'src/components/Shared/Layouts/Fixed'
 import { Grow } from 'src/components/Shared/Layouts/Grow'
+import PreviewReport from 'src/components/Shared/PreviewReport'
 import { ControlContext } from 'src/providers/ControlContext'
-
 import GridToolbar from 'src/components/Shared/GridToolbar'
 import { InventoryRepository } from 'src/repositories/InventoryRepository'
+import { SystemRepository } from 'src/repositories/SystemRepository'
 import CategoryWindow from './window/CategoryWindow'
+import CustomComboBox from 'src/components/Inputs/CustomComboBox'
 
 const Category = () => {
   const { getRequest, postRequest } = useContext(RequestsContext)
   const { platformLabels } = useContext(ControlContext)
   const { stack } = useWindow()
+  const [selectedReport, setSelectedReport] = useState(null)
+  const [reportStore, setReportStore] = useState([])
 
   async function fetchGridData(options = {}) {
     const { _startAt = 0, _pageSize = 50 } = options
@@ -112,10 +117,97 @@ const Category = () => {
     openForm(obj?.recordId)
   }
 
+  useEffect(() => {
+    getReportLayout()
+  }, [ResourceIds.IvCategories])
+
+  useEffect(() => {
+    if (reportStore.length > 0) {
+      setSelectedReport(reportStore[0])
+    } else {
+      setSelectedReport(null)
+    }
+  }, [reportStore])
+
+  const getReportLayout = () => {
+    setReportStore([])
+    if (ResourceIds.IvCategories) {
+      var parameters = `_resourceId=${ResourceIds.IvCategories}`
+      getRequest({
+        extension: SystemRepository.ReportLayout,
+        parameters: parameters
+      })
+        .then(res => {
+          if (res?.list) {
+            const formattedReports = res.list.map(item => ({
+              api_url: item.api,
+              reportClass: item.instanceName,
+              parameters: item.parameters,
+              layoutName: item.layoutName,
+              assembly: 'ArgusRPT.dll'
+            }))
+            setReportStore(formattedReports)
+            if (formattedReports.length > 0) {
+              setSelectedReport(formattedReports[0])
+            }
+          }
+        })
+        .catch(error => {})
+    }
+  }
+
   return (
     <VertLayout>
       <Fixed>
-        <GridToolbar onAdd={add} maxAccess={access} />
+        <GridToolbar
+          onAdd={add}
+          maxAccess={access}
+          rightSection={
+            <Grid item sx={{ display: 'flex', mr: 2 }}>
+              <CustomComboBox
+                label={platformLabels.SelectReport}
+                valueField='caption'
+                displayField='layoutName'
+                store={reportStore}
+                value={selectedReport}
+                onChange={(e, newValue) => setSelectedReport(newValue)}
+                sx={{ width: 250 }}
+                disableClearable
+              />
+              <Button
+                variant='contained'
+                disabled={!selectedReport}
+                onClick={() =>
+                  stack({
+                    Component: PreviewReport,
+                    props: {
+                      selectedReport: selectedReport
+                    },
+                    width: 1000,
+                    height: 500,
+                    title: platformLabels.PreviewReport
+                  })
+                }
+                sx={{
+                  ml: 2,
+                  backgroundColor: '#231F20',
+                  '&:hover': {
+                    backgroundColor: '#231F20',
+                    opacity: 0.8
+                  },
+                  width: 'auto',
+                  height: '35px',
+                  objectFit: 'contain'
+                }}
+                size='small'
+              >
+                <Tooltip title={platformLabels.Preview}>
+                  <img src='/images/buttonsIcons/preview.png' alt={platformLabels.Preview} />
+                </Tooltip>
+              </Button>
+            </Grid>
+          }
+        />
       </Fixed>
       <Grow>
         <Table
