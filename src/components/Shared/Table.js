eaba31290a40f9502a431d3c627110849fc162d0
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-09-18 16:14:34 +0300

    clear useRef

--- src/components/Shared/Table.js
+++ src/components/Shared/Table.js
@@ -1,4 +1,4 @@
-import React, { useContext, useRef } from 'react'
+import React, { useContext } from 'react'
 import { AgGridReact } from 'ag-grid-react'
 import 'ag-grid-community/styles/ag-grid.css'
 import 'ag-grid-community/styles/ag-theme-alpine.css'
