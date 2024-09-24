ca486d3767be28f243c01031515c10e55b237c6b
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-09-24 15:25:59 +0300

    space

--- src/components/Shared/DataGrid/index.js
+++ src/components/Shared/DataGrid/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useRef } from 'react'
 import { AgGridReact } from 'ag-grid-react'
 import { Box, IconButton } from '@mui/material'
 import components from './components'
