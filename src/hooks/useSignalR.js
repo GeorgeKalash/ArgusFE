e9befd9db4ff158c4a0f8cc496945d7b4a942803
HasanDiab <hasandiab.main@gmail.com>
2024-09-27 14:15:40 +0300

    transaction data intial value

--- src/hooks/useSignalR.js
+++ src/hooks/useSignalR.js
@@ -2,7 +2,19 @@ import { useState } from 'react'
 import signalRService from '../services/signalR/SignalRService'
 
 const useSignalR = () => {
-  const [transactionData, setTransactionData] = useState(null)
+  const [transactionData, setTransactionData] = useState({
+    msgId: '',
+    ecrno: '',
+    ecR_RCPT: '',
+    amount: '',
+    a1: '',
+    a2: '',
+    a3: '',
+    a4: '',
+    a5: '',
+    ipaddressOrPort: '',
+    log: 0
+  })
 
   const startConnection = async () => {
     await signalRService.startConnection()
