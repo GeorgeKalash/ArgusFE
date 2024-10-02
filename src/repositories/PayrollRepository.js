3d225b2f8ec8c05d81a7407d7b39634fb395aa05
RebeccaFrem02 <155422197+RebeccaFrem02@users.noreply.github.com>
2024-10-02 12:07:07 +0300

    REM-563

---
+++ src/repositories/PayrollRepository.js
@@ -0,0 +1,10 @@
+const service = 'PY.asmx/'
+
+export const PayrollRepository = {
+  PayrollFilters: {
+    qry: service + 'qryHE'
+  },
+  BankTransferFilters: {
+    qry: service + 'qrySB'
+  }
+}
