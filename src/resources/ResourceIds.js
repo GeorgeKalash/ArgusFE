5566df153b5a7aae057599d5322dd18b84dd87d1
Alinadimmokdad <alimokdad961@gmail.com>
2024-10-15 14:11:14 +0300

    rem duplicate

--- src/resources/ResourceIds.js
+++ src/resources/ResourceIds.js
@@ -275,7 +275,6 @@ export const ResourceIds = {
   RemittanceReceiptVoucher: 36323,
   ItemSizes: 41117,
   OutwardsTransfer: 36324,
-  MetalColor: 41110,
   OpenMultiCurrencyCashTransfer: 33315,
   LockedRecords: 23302
 }
