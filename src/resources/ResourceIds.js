8154f5b31a4252bee4916b1ed1f63ecf8782140e
Alinadimmokdad <alimokdad961@gmail.com>
2024-10-24 11:33:09 +0300

    resolve conflict

--- src/resources/ResourceIds.js
+++ src/resources/ResourceIds.js
@@ -283,6 +283,6 @@ export const ResourceIds = {
   OpenMultiCurrencyCashTransfer: 33315,
   LockedRecords: 23302,
   ProductionSheet: 42327,
-  ProdSheetDocumentTypeDefault: 42120
-  InventoryTransaction: 41209,
+  ProdSheetDocumentTypeDefault: 42120,
+  InventoryTransaction: 41209
 }
