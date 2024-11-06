0f0b7009bc8e3c7bc292463d7db3c6a77c0afd93
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-11-06 16:23:02 +0200

    fixing cancel

--- src/pages/gate-keeper/index.js
+++ src/pages/gate-keeper/index.js
@@ -6,12 +6,18 @@ import { ManufacturingRepository } from 'src/repositories/ManufacturingRepositor
 import { Grow } from 'src/components/Shared/Layouts/Grow'
 import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
 import Table from 'src/components/Shared/Table'
+import cancelIcon from '../../../public/images/TableIcons/cancel.png'
+import Image from 'next/image'
+import { Box, IconButton } from '@mui/material'
 import toast from 'react-hot-toast'
 import { ControlContext } from 'src/providers/ControlContext'
+import CancelDialog from 'src/components/Shared/CancelDialog'
+import { useWindow } from 'src/windows'
 
 const GateKeeper = () => {
   const { getRequest, postRequest } = useContext(RequestsContext)
   const { platformLabels } = useContext(ControlContext)
+  const { stack } = useWindow()
 
   async function fetchGridData(options = {}) {
     const { _startAt = 0, _pageSize = 50 } = options
@@ -42,7 +48,7 @@ const GateKeeper = () => {
   } = useResourceQuery({
     queryFn: fetchGridData,
     endpointId: ManufacturingRepository.LeanProductionPlanning.preview2,
-    datasetId:  ResourceIds.GateKeeper
+    datasetId: ResourceIds.GateKeeper
   })
 
   const columns = [
@@ -78,16 +84,51 @@ const GateKeeper = () => {
       label: _labels[6],
       flex: 2,
       type: 'date'
+    },
+    {
+      flex: 0.5,
+      headerName: 'Cancel',
+      cellRenderer: row => {
+        const { data } = row
+
+        return (
+          <Box sx={{ display: 'flex', width: '100%', justifyContent: 'center' }}>
+            <IconButton
+              size='small'
+              onClick={() => openCancel(data)}
+            >
+              <Image src={cancelIcon} width={18} height={18} alt={_labels.cancel} />
+            </IconButton>
+          </Box>
+        )
+      }
     }
   ]
 
-  const del = async obj => {
+  const del = async (data) => {
     await postRequest({
       extension: ManufacturingRepository.LeanProductionPlanning.cancel,
-      record: JSON.stringify(obj)
+      record: JSON.stringify(data)
+    }).then(res => {
+      if (res) {
+        invalidate()
+        toast.success(platformLabels.Cancelled)
+      }
     })
-    invalidate()
-    toast.success(platformLabels.Cancelled)
+  }
+
+  function openCancel(data) {
+    stack({
+      Component: CancelDialog,
+      props: {
+        open: [true, {}],
+        fullScreen: false,
+        onConfirm: () => del(data)
+      },
+      width: 450,
+      height: 170,
+      title: platformLabels.Clear
+    })
   }
 
   return (
@@ -97,7 +138,6 @@ const GateKeeper = () => {
           columns={columns}
           gridData={data}
           rowId={['recordId']}
-          onDelete={del}
           isLoading={false}
           pagination={false}
           refetch={refetch}
