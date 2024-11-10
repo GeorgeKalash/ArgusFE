172cdb2904f728f3101bad405a25e46c94918908
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2024-11-10 10:31:17 +0200

    suspense

--- src/components/Shared/CustomTabPanel.js
+++ src/components/Shared/CustomTabPanel.js
@@ -2,9 +2,31 @@
 import { Box } from '@mui/material'
 
 import PropTypes from 'prop-types'
+import { Suspense } from 'react'
 
 const CustomTabPanel = props => {
   const { children, value, height, index, ...other } = props
+
+  function LoadingOverlay() {
+    return (
+      <Box
+        style={{
+          position: 'absolute',
+          top: 0,
+          right: 0,
+          left: 0,
+          bottom: 0,
+          backgroundColor: 'rgba(0, 0, 0, 0.2)',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          zIndex: 9999
+        }}
+      >
+        <CircularProgress color='inherit' />
+      </Box>
+    )
+  }
 
   return (
     <Box
@@ -25,7 +47,7 @@ const CustomTabPanel = props => {
       }}
       {...other}
     >
-      {children}
+      <Suspense fallback={<LoadingOverlay />}>{children}</Suspense>
     </Box>
   )
 }
