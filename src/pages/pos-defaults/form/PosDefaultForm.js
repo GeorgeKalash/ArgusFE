187c56b880946f2e8b547f0d86c737280e6a91d4
Alinadimmokdad <alimokdad961@gmail.com>
2024-11-13 13:30:16 +0200

    RM-77

---
+++ src/pages/pos-defaults/form/PosDefaultForm.js
@@ -0,0 +1,97 @@
+import { useEffect, useState, useContext } from 'react'
+import { Grid } from '@mui/material'
+import { useFormik } from 'formik'
+import toast from 'react-hot-toast'
+import WindowToolbar from 'src/components/Shared/WindowToolbar'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { SystemRepository } from 'src/repositories/SystemRepository'
+import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { Fixed } from 'src/components/Shared/Layouts/Fixed'
+import { ControlContext } from 'src/providers/ControlContext'
+import { DataSets } from 'src/resources/DataSets'
+
+const PosDefaultForm = ({ _labels, access }) => {
+  const { postRequest } = useContext(RequestsContext)
+  const { platformLabels, defaultsData, updateDefaults } = useContext(ControlContext)
+
+  const [initialValues, setInitialValues] = useState({
+    posItemPK: null
+  })
+
+  useEffect(() => {
+    getDataResult()
+  }, [])
+
+  const getDataResult = () => {
+    const myObject = {}
+
+    const filteredList = defaultsData?.list?.filter(obj => {
+      return obj.key === 'posItemPK'
+    })
+    filteredList?.forEach(obj => (myObject[obj.key] = obj.value ? parseInt(obj.value) : null))
+    setInitialValues(myObject)
+  }
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    validateOnChange: true,
+    initialValues,
+    onSubmit: values => {
+      postPosDefault(values)
+    }
+  })
+
+  const postPosDefault = obj => {
+    const data = []
+
+    if (!Object.hasOwn(obj, 'posItemPK') || obj.posItemPK === null) {
+      data.push({ key: 'posItemPK' })
+    } else {
+      Object.entries(obj).forEach(([key, value]) => {
+        data.push({ key: key, value: value })
+      })
+    }
+
+    postRequest({
+      extension: SystemRepository.Defaults.set,
+      record: JSON.stringify({ SysDefaults: data })
+    }).then(res => {
+      if (res) toast.success(platformLabels.Edited)
+      updateDefaults(data)
+    })
+  }
+
+  const handleSubmit = () => {
+    formik.handleSubmit()
+  }
+
+  return (
+    <VertLayout>
+      <Grow>
+        <Grid container spacing={4} sx={{ pt: '0.5rem' }}>
+          <Grid item xs={12} sx={{ marginLeft: '0.5rem', marginRight: '0.5rem' }}>
+            <ResourceComboBox
+              datasetId={DataSets.POS_ITEM_PRIMARY}
+              name='posItemPK'
+              label={_labels.posItem}
+              valueField='key'
+              displayField='value'
+              values={formik.values}
+              maxAccess={access}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('posItemPK', newValue?.key)
+              }}
+            />
+          </Grid>
+        </Grid>
+      </Grow>
+      <Fixed>
+        <WindowToolbar onSave={handleSubmit} isSaved={true} />
+      </Fixed>
+    </VertLayout>
+  )
+}
+
+export default PosDefaultForm
