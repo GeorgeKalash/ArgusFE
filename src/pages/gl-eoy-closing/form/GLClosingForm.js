345815a387ba1b845215f23ae7bb03cf8df7e224
Alinadimmokdad <alimokdad961@gmail.com>
2024-11-20 15:53:31 +0200

    base

---
+++ src/pages/gl-eoy-closing/form/GLClosingForm.js
@@ -0,0 +1,78 @@
+import { Grid } from '@mui/material'
+import * as yup from 'yup'
+import { useContext } from 'react'
+import FormShell from 'src/components/Shared/FormShell'
+import toast from 'react-hot-toast'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useForm } from 'src/hooks/form'
+import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
+import { SystemRepository } from 'src/repositories/SystemRepository'
+import { RGGeneralRepository } from 'src/repositories/RGGeneralRepository'
+import CustomDatePicker from 'src/components/Inputs/CustomDatePicker'
+import { ControlContext } from 'src/providers/ControlContext'
+
+export default function GlEoyClosingForm({ _labels, access }) {
+  const { postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
+
+  const { formik } = useForm({
+    initialValues: { fiscalYear: '', recordId: 'N/A' },
+    enableReinitialize: true,
+    maxAccess: access,
+    validateOnChange: true,
+
+    validationSchema: yup.object({
+      fiscalYear: yup.string().required()
+    }),
+    onSubmit: async obj => {
+      const { recordId, ...rest } = obj
+
+      await postRequest({
+        extension: RGGeneralRepository.OriginBalance.GLDOE,
+        record: JSON.stringify(rest)
+      })
+
+      toast.success(platformLabels.Saved)
+      formik.setValues({
+        ...obj
+      })
+    }
+  })
+
+  return (
+    <FormShell
+      resourceId={ResourceIds.GlSync}
+      form={formik}
+      maxAccess={access}
+      editMode={true}
+      isSavedClear={false}
+      isCleared={false}
+    >
+      <VertLayout>
+        <Grow>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                endpointId={SystemRepository.FiscalYears.qry}
+                name='fiscalYear'
+                label={_labels.fiscalYear}
+                valueField='fiscalYear'
+                displayField='fiscalYear'
+                values={formik.values}
+                required
+                maxAccess={access}
+                onChange={(event, newValue) => {
+                  formik && formik.setFieldValue('fiscalYear', newValue?.fiscalYear)
+                }}
+                error={formik.touched.fiscalYear && Boolean(formik.errors.fiscalYear)}
+              />
+            </Grid>
+          </Grid>
+        </Grow>
+      </VertLayout>
+    </FormShell>
+  )
+}
---
+++ src/pages/gl-eoy-closing/index.js
@@ -0,0 +1,9 @@
+import { ImmediateWindow } from 'src/windows'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import GlEoyClosingForm from './form/GLClosingForm'
+
+const GlEoyClosing = () => {
+  return <ImmediateWindow datasetId={ResourceIds.GlEoyClosing} labelKey={'sync'} Component={GlEoyClosingForm} />
+}
+
+export default GlEoyClosing
