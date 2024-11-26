3280841d5cf188137e790a62c54e5af93e259868
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2024-11-26 08:55:56 +0200

    remove duplicate unitPrice

--- src/pages/sa-trx/[functionId]/forms/SaleTransactionForm.js
+++ src/pages/sa-trx/[functionId]/forms/SaleTransactionForm.js
@@ -457,10 +457,7 @@ export default function SaleTransactionForm({ labels, access, recordId, function
                 : 0,
             baseLaborPrice: 0,
             TotPricePerG: TotPricePerG,
-            unitPrice:
-              ItemConvertPrice?.priceType === 3
-                ? weight * TotPricePerG
-                : parseFloat(ItemConvertPrice?.unitPrice || 0).toFixed(3),
+            unitPrice: unitPrice,
             upo: parseFloat(ItemConvertPrice?.upo || 0).toFixed(2),
             priceType: ItemConvertPrice?.priceType || 1,
             qty: 0,
