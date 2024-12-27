9b992573e1b9d3fdf7a1be825da7dffae2068a82
Alinadimmokdad <alimokdad961@gmail.com>
2024-12-27 13:59:47 +0200

    pos dtd retail

---
+++ src/pages/rt-sa-dtd/[functionId]/form/RetailDocTypeForm.js
@@ -0,0 +1,122 @@
+import { Checkbox, FormControlLabel, Grid } from '@mui/material'
+import { useContext, useEffect } from 'react'
+import * as yup from 'yup'
+import FormShell from 'src/components/Shared/FormShell'
+import toast from 'react-hot-toast'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useInvalidate } from 'src/hooks/resource'
+import { ResourceIds } from 'src/resources/ResourceIds'
+
+import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useForm } from 'src/hooks/form'
+
+import { SystemRepository } from 'src/repositories/SystemRepository'
+import { ControlContext } from 'src/providers/ControlContext'
+import { PointofSaleRepository } from 'src/repositories/PointofSaleRepository'
+
+export default function RetailDocTypeForm({ labels, maxAccess, recordId, functionId, window }) {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
+
+  const invalidate = useInvalidate({
+    endpointId: PointofSaleRepository.DocumentTypeDefault.qry
+  })
+
+  const { formik } = useForm({
+    initialValues: {
+      dtId: '',
+      disableSKULookup: false
+    },
+    maxAccess,
+    enableReinitialize: true,
+    validateOnChange: true,
+    validationSchema: yup.object({
+      dtId: yup.string().required(' ')
+    }),
+    onSubmit: async obj => {
+      const recordId = obj.recordId
+      await postRequest({
+        extension: PointofSaleRepository.DocumentTypeDefault.set,
+        record: JSON.stringify(obj)
+      })
+      if (!recordId) {
+        formik.setFieldValue('recordId', obj.dtId)
+      }
+
+      toast.success(platformLabels.Updated)
+
+      invalidate()
+      window.close()
+    }
+  })
+  const editMode = !!formik.values.recordId
+
+  useEffect(() => {
+    ;(async function () {
+      try {
+        if (recordId) {
+          const res = await getRequest({
+            extension: PointofSaleRepository.DocumentTypeDefault.get,
+            parameters: `_dtId=${recordId}`
+          })
+
+          formik.setValues({ ...res.record, recordId: recordId })
+        }
+      } catch (exception) {}
+    })()
+  }, [])
+
+  return (
+    <FormShell
+      resourceId={ResourceIds.POSDocTypeDefault}
+      form={formik}
+      maxAccess={maxAccess}
+      editMode={editMode}
+      isCleared={false}
+    >
+      <VertLayout>
+        <Grow>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                endpointId={SystemRepository.DocumentType.qry}
+                parameters={`_startAt=0&_pageSize=1000&_dgId=${functionId}`}
+                name='dtId'
+                required
+                label={labels.doctype}
+                readOnly={editMode}
+                columnsInDropDown={[
+                  { key: 'reference', value: 'Reference' },
+                  { key: 'name', value: 'Name' }
+                ]}
+                valueField='recordId'
+                displayField={['reference', 'name']}
+                values={formik.values}
+                maxAccess={maxAccess}
+                onChange={(event, newValue) => {
+                  formik.setFieldValue('dtId', newValue?.recordId || '')
+                }}
+                error={formik.touched.dtId && Boolean(formik.errors.dtId)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    maxAccess={maxAccess}
+                    name='disableSKULookup'
+                    checked={formik.values?.disableSKULookup}
+                    onChange={formik.handleChange}
+                  />
+                }
+                label={labels.dsl}
+              />
+            </Grid>
+          </Grid>
+        </Grow>
+      </VertLayout>
+    </FormShell>
+  )
+}
---
+++ src/pages/rt-sa-dtd/[functionId]/index.js
@@ -0,0 +1,129 @@
+import { useContext } from 'react'
+import toast from 'react-hot-toast'
+import Table from 'src/components/Shared/Table'
+import GridToolbar from 'src/components/Shared/GridToolbar'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useResourceQuery } from 'src/hooks/resource'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Fixed } from 'src/components/Shared/Layouts/Fixed'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useWindow } from 'src/windows'
+
+import { useRouter } from 'next/router'
+
+import { ControlContext } from 'src/providers/ControlContext'
+import { PointofSaleRepository } from 'src/repositories/PointofSaleRepository'
+import RetailDocTypeForm from './form/RetailDocTypeForm'
+
+const RetailDtd = () => {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
+
+  const { stack } = useWindow()
+
+  const router = useRouter()
+  const { functionId } = router.query
+
+  async function fetchGridData(options = {}) {
+    const {
+      pagination: { _startAt = 0, _pageSize = 50 }
+    } = options
+
+    const response = await getRequest({
+      extension: PointofSaleRepository.DocumentTypeDefault.qry,
+      parameters: `_startAt=${_startAt}&_pageSize=${_pageSize}&_functionId=${functionId}`
+    })
+
+    return { ...response, _startAt: _startAt }
+  }
+
+  const {
+    query: { data },
+    labels: _labels,
+    access,
+    paginationParameters,
+    invalidate,
+    refetch
+  } = useResourceQuery({
+    endpointId: PointofSaleRepository.DocumentTypeDefault.qry,
+    datasetId: ResourceIds.POSDocTypeDefault,
+
+    filter: {
+      filterFn: fetchGridData,
+      default: { functionId }
+    }
+  })
+
+  const columns = [
+    {
+      field: 'dtName',
+      headerName: _labels.doctype,
+      flex: 1
+    },
+
+    {
+      field: 'disableSKULookup',
+      headerName: _labels.dsl,
+      type: 'checkbox',
+      flex: 1
+    }
+  ]
+
+  const add = () => {
+    openForm()
+  }
+
+  const edit = obj => {
+    openForm(obj)
+  }
+
+  const del = async obj => {
+    await postRequest({
+      extension: PointofSaleRepository.DocumentTypeDefault.del,
+      record: JSON.stringify(obj)
+    })
+    invalidate()
+    toast.success(platformLabels.Deleted)
+  }
+
+  function openForm(record) {
+    stack({
+      Component: RetailDocTypeForm,
+      props: {
+        labels: _labels,
+        functionId,
+        recordId: record?.dtId,
+        maxAccess: access
+      },
+      width: 500,
+      height: 360,
+      title: _labels.doctypeDefault
+    })
+  }
+
+  return (
+    <VertLayout>
+      <Fixed>
+        <GridToolbar onAdd={add} maxAccess={access} />
+      </Fixed>
+      <Grow>
+        <Table
+          columns={columns}
+          gridData={data}
+          rowId={['dtId']}
+          onEdit={edit}
+          onDelete={del}
+          isLoading={false}
+          pageSize={50}
+          paginationParameters={paginationParameters}
+          refetch={refetch}
+          paginationType='api'
+          maxAccess={access}
+        />
+      </Grow>
+    </VertLayout>
+  )
+}
+
+export default RetailDtd
