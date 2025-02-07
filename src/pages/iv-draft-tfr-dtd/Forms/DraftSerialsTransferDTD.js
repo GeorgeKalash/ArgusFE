bd2a145ed30295a47d96052496033ad1c9053b0e
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-02-07 16:42:23 +0200

    adding new screen iv-draft-tfr-dtd

---
+++ src/pages/iv-draft-tfr-dtd/Forms/DraftSerialsTransferDTD.js
@@ -0,0 +1,202 @@
+import { Grid } from '@mui/material'
+import { useContext, useEffect } from 'react'
+import * as yup from 'yup'
+import FormShell from 'src/components/Shared/FormShell'
+import toast from 'react-hot-toast'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useInvalidate } from 'src/hooks/resource'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useForm } from 'src/hooks/form'
+import { SystemRepository } from 'src/repositories/SystemRepository'
+import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
+import { ControlContext } from 'src/providers/ControlContext'
+import { InventoryRepository } from 'src/repositories/InventoryRepository'
+import CustomCheckBox from 'src/components/Inputs/CustomCheckBox'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { LogisticsRepository } from 'src/repositories/LogisticsRepository'
+import { SystemFunction } from 'src/resources/SystemFunction'
+
+export default function DraftSerialsTransferDTDForm({ labels, maxAccess, recordId, window }) {
+  const { platformLabels } = useContext(ControlContext)
+
+  const { getRequest, postRequest } = useContext(RequestsContext)
+
+  const invalidate = useInvalidate({
+    endpointId: InventoryRepository.DocumentTypeDefaults.page
+  })
+
+  const { formik } = useForm({
+    initialValues: {
+      recordId,
+      functionId: SystemFunction.DraftTransfer,
+      dtId: null,
+      siteId: null,
+      toSiteId: null,
+      carrierId: null,
+      plantId: null,
+      disableSKULookup: false
+    },
+    maxAccess,
+    enableReinitialize: false,
+    validationSchema: yup.object({
+      dtId: yup.string().required()
+    }),
+    onSubmit: async obj => {
+      await postRequest({
+        extension: InventoryRepository.DocumentTypeDefaults.set,
+        record: JSON.stringify(obj)
+      })
+
+      if (!recordId) {
+        formik.setFieldValue('recordId', formik.values.dtId)
+
+        toast.success(platformLabels.Added)
+      } else toast.success(platformLabels.Edited)
+
+      invalidate()
+      window.close()
+    }
+  })
+  const editMode = !!formik.values.recordId
+
+  useEffect(() => {
+    ;(async function () {
+      if (recordId) {
+        const res = await getRequest({
+          extension: InventoryRepository.DocumentTypeDefaults.get,
+          parameters: `_dtId=${recordId}`
+        })
+
+        formik.setValues({
+          ...res.record,
+          recordId: res.record.dtId
+        })
+      }
+    })()
+  }, [])
+
+  return (
+    <FormShell
+      resourceId={ResourceIds.MaterialTransferDTD}
+      form={formik}
+      maxAccess={maxAccess}
+      editMode={editMode}
+      functionId={SystemFunction.DraftTransfer}
+    >
+      <VertLayout>
+        <Grow>
+          <Grid container spacing={4}>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                endpointId={SystemRepository.DocumentType.qry}
+                parameters={`_startAt=0&_pageSize=1000&_dgId=${SystemFunction.DraftTransfer}`}
+                name='dtId'
+                required
+                label={labels.documentType}
+                columnsInDropDown={[
+                  { key: 'reference', value: 'Reference' },
+                  { key: 'name', value: 'Name' }
+                ]}
+                valueField='recordId'
+                displayField={['reference', 'name']}
+                readOnly={editMode}
+                values={formik.values}
+                maxAccess={maxAccess}
+                onChange={(event, newValue) => {
+                  formik.setFieldValue('dtId', newValue?.recordId)
+                }}
+                error={formik.touched.dtId && Boolean(formik.errors.dtId)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                endpointId={InventoryRepository.Site.qry}
+                name='siteId'
+                label={labels.fromSite}
+                columnsInDropDown={[
+                  { key: 'reference', value: 'Reference' },
+                  { key: 'name', value: 'Name' }
+                ]}
+                valueField='recordId'
+                displayField={['reference', 'name']}
+                values={formik.values}
+                maxAccess={maxAccess}
+                onChange={(event, newValue) => {
+                  formik.setFieldValue('siteId', newValue?.recordId)
+                }}
+                error={formik.touched.siteId && Boolean(formik.errors.siteId)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                endpointId={InventoryRepository.Site.qry}
+                name='toSiteId'
+                label={labels.toSite}
+                columnsInDropDown={[
+                  { key: 'reference', value: 'Reference' },
+                  { key: 'name', value: 'Name' }
+                ]}
+                valueField='recordId'
+                displayField={['reference', 'name']}
+                values={formik.values}
+                maxAccess={maxAccess}
+                onChange={(event, newValue) => {
+                  formik.setFieldValue('toSiteId', newValue?.recordId)
+                }}
+                error={formik.touched.toSiteId && Boolean(formik.errors.toSiteId)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomCheckBox
+                name='disableSKULookup'
+                value={formik.values?.disableSKULookup}
+                onChange={event => formik.setFieldValue('disableSKULookup', event.target.checked)}
+                label={labels.dsl}
+                maxAccess={maxAccess}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                endpointId={LogisticsRepository.LoCarrier.qry}
+                name='carrierId'
+                label={labels.carrier}
+                values={formik?.values}
+                valueField='recordId'
+                displayField={['reference', 'name']}
+                columnsInDropDown={[
+                  { key: 'reference', value: 'Reference' },
+                  { key: 'name', value: 'Name' }
+                ]}
+                maxAccess={maxAccess}
+                onChange={(event, newValue) => {
+                  formik.setFieldValue('carrierId', newValue?.recordId || '')
+                }}
+                error={formik.touched.carrierId && Boolean(formik.errors.carrierId)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <ResourceComboBox
+                endpointId={SystemRepository.Plant.qry}
+                name='plantId'
+                label={labels.plant}
+                valueField='recordId'
+                displayField={['reference', 'name']}
+                columnsInDropDown={[
+                  { key: 'reference', value: 'Ref' },
+                  { key: 'name', value: 'Name' }
+                ]}
+                values={formik.values}
+                maxAccess={maxAccess}
+                onChange={(event, newValue) => {
+                  formik.setFieldValue('plantId', newValue?.recordId)
+                }}
+                error={formik.touched.plantId && Boolean(formik.errors.plantId)}
+              />
+            </Grid>
+          </Grid>
+        </Grow>
+      </VertLayout>
+    </FormShell>
+  )
+}
