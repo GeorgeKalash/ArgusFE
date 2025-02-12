f602798bc8467dbbc7aa03b742c89bf385df459e
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-02-12 15:52:33 +0200

    wax dtd screen

---
+++ src/pages/mf-wax-dtd/index.js
@@ -0,0 +1,119 @@
+import { useContext } from 'react'
+import toast from 'react-hot-toast'
+import Table from 'src/components/Shared/Table'
+import GridToolbar from 'src/components/Shared/GridToolbar'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useWindow } from 'src/windows'
+import { useResourceQuery } from 'src/hooks/resource'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Fixed } from 'src/components/Shared/Layouts/Fixed'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { ControlContext } from 'src/providers/ControlContext'
+import { SystemFunction } from 'src/resources/SystemFunction'
+import WaxDTDForm from './Forms/WaxDTDForm'
+import { ManufacturingRepository } from 'src/repositories/ManufacturingRepository'
+
+const WaxDTD = () => {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
+  const { stack } = useWindow()
+
+  async function fetchGridData(options = {}) {
+    const { _startAt = 0, _pageSize = 50 } = options
+
+    const response = await getRequest({
+      extension: ManufacturingRepository.DocumentTypeDefault.page,
+      parameters: `_startAt=${_startAt}&_pageSize=${_pageSize}&_filter=&_functionId=${SystemFunction.Wax}`
+    })
+
+    return { ...response, _startAt: _startAt }
+  }
+
+  const {
+    query: { data },
+    labels,
+    invalidate,
+    refetch,
+    access,
+    paginationParameters
+  } = useResourceQuery({
+    queryFn: fetchGridData,
+    endpointId: ManufacturingRepository.DocumentTypeDefault.page,
+    datasetId: ResourceIds.WaxDocTypeDefaults
+  })
+
+  const columns = [
+    {
+      field: 'dtName',
+      headerName: labels.documentType,
+      flex: 1
+    },
+    {
+      field: 'workCenterName',
+      headerName: labels.workCenter,
+      flex: 1
+    },
+    {
+      field: 'lineName',
+      headerName: labels.productionLine,
+      flex: 1
+    }
+  ]
+
+  const add = () => {
+    openForm()
+  }
+
+  const del = async obj => {
+    await postRequest({
+      extension: ManufacturingRepository.DocumentTypeDefault.del,
+      record: JSON.stringify(obj)
+    })
+    invalidate()
+    toast.success(platformLabels.Deleted)
+  }
+
+  function openForm(record) {
+    stack({
+      Component: WaxDTDForm,
+      props: {
+        labels,
+        recordId: record?.dtId,
+        maxAccess: access
+      },
+      width: 600,
+      height: 400,
+      title: labels.WaxDTD
+    })
+  }
+
+  const edit = obj => {
+    openForm(obj)
+  }
+
+  return (
+    <VertLayout>
+      <Fixed>
+        <GridToolbar onAdd={add} maxAccess={access} />
+      </Fixed>
+      <Grow>
+        <Table
+          columns={columns}
+          gridData={data}
+          rowId={['dtId']}
+          onEdit={edit}
+          onDelete={del}
+          isLoading={false}
+          pageSize={50}
+          refetch={refetch}
+          paginationType='api'
+          paginationParameters={paginationParameters}
+          maxAccess={access}
+        />
+      </Grow>
+    </VertLayout>
+  )
+}
+
+export default WaxDTD
