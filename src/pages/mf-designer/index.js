816a7a724e7f3f1f1ad567324f6104fa913a9425
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-02-19 10:30:52 +0200

    adding new settings screen designer

---
+++ src/pages/mf-designer/index.js
@@ -0,0 +1,113 @@
+import { useContext } from 'react'
+import toast from 'react-hot-toast'
+import Table from 'src/components/Shared/Table'
+import GridToolbar from 'src/components/Shared/GridToolbar'
+import { RequestsContext } from 'src/providers/RequestsContext'
+import { useResourceQuery } from 'src/hooks/resource'
+import { ResourceIds } from 'src/resources/ResourceIds'
+import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
+import { Fixed } from 'src/components/Shared/Layouts/Fixed'
+import { Grow } from 'src/components/Shared/Layouts/Grow'
+import { useWindow } from 'src/windows'
+import { ControlContext } from 'src/providers/ControlContext'
+import { ProductModelingRepository } from 'src/repositories/ProductModelingRepository'
+import DesignerForm from './Forms/DesignerForm'
+
+const Designer = () => {
+  const { getRequest, postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
+  const { stack } = useWindow()
+
+  async function fetchGridData(options = {}) {
+    const { _startAt = 0, _pageSize = 50 } = options
+
+    const response = await getRequest({
+      extension: ProductModelingRepository.Designer.page,
+      parameters: `_startAt=${_startAt}&_pageSize=${_pageSize}&filter=`
+    })
+
+    return { ...response, _startAt: _startAt }
+  }
+
+  const {
+    query: { data },
+    labels,
+    paginationParameters,
+    refetch,
+    access,
+    invalidate
+  } = useResourceQuery({
+    queryFn: fetchGridData,
+    endpointId: ProductModelingRepository.Designer.page,
+    datasetId: ResourceIds.Designer
+  })
+
+  const columns = [
+    {
+      field: 'reference',
+      headerName: labels.reference,
+      flex: 1
+    },
+    {
+      field: 'name',
+      headerName: labels.name,
+      flex: 1
+    }
+  ]
+
+  const add = () => {
+    openForm()
+  }
+
+  const edit = obj => {
+    openForm(obj?.recordId)
+  }
+
+  function openForm(recordId) {
+    stack({
+      Component: DesignerForm,
+      props: {
+        labels,
+        recordId,
+        maxAccess: access
+      },
+      width: 600,
+      height: 250,
+      title: labels.designer
+    })
+  }
+
+  const del = async obj => {
+    await postRequest({
+      extension: ProductModelingRepository.Designer.del,
+      record: JSON.stringify(obj)
+    })
+    invalidate()
+    toast.success(platformLabels.Deleted)
+  }
+
+  return (
+    <VertLayout>
+      <Fixed>
+        <GridToolbar onAdd={add} maxAccess={access} />
+      </Fixed>
+      <Grow>
+        <Table
+          columns={columns}
+          gridData={data}
+          rowId={['recordId']}
+          onEdit={edit}
+          onDelete={del}
+          isLoading={false}
+          pageSize={50}
+          paginationType='api'
+          paginationParameters={paginationParameters}
+          refetch={refetch}
+          maxAccess={access}
+        />
+      </Grow>
+    </VertLayout>
+  )
+}
+
+export default Designer
