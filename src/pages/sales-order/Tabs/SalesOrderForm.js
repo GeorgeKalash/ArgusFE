0d79925e428b3cd8bf3f00bcfcea22358678187f
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2025-02-21 09:37:43 +0200

    lookupIssue

--- src/pages/sales-order/Tabs/SalesOrderForm.js
+++ src/pages/sales-order/Tabs/SalesOrderForm.js
@@ -282,7 +282,7 @@ export default function SalesOrderForm({ labels, access, recordId, currency, win
         endpointId: InventoryRepository.Item.snapshot,
         parameters: { _categoryId: 0, _msId: 0, _startAt: 0, _size: 1000 },
         displayField: 'sku',
-        valueField: 'recordId',
+        valueField: 'sku',
         mapping: [
           { from: 'recordId', to: 'itemId' },
           { from: 'sku', to: 'sku' },
