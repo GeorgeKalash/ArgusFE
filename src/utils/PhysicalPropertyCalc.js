56c2bae54adb8a876c0eb17e1402660f215bf4c0
RebeccaFrem02 <155422197+RebeccaFrem02@users.noreply.github.com>
2025-02-24 13:19:51 +0200

    making calculation local

---
+++ src/utils/PhysicalPropertyCalc.js
@@ -0,0 +1,133 @@
+// PhysicalPropertyCalculatorCtrl.js
+
+// Constants for identifying which field is dirty
+export const DirtyField = {
+  LENGTH: 1,
+  WIDTH: 2,
+  DEPTH: 3,
+  DIAMETER: 4,
+  VOLUME: 5,
+  WEIGHT: 6,
+  DENSITY: 7
+}
+
+// Constants for geometric shapes
+export const GeometricShape = {
+  CUBIC: 1,
+  CYLINDER: 2
+}
+
+// A simple container class to hold physical properties
+class PhysicalPropertyCalculator {
+  constructor() {
+    this.dirtyField = null
+    this.shape = null
+    this.length = 0
+    this.width = 0
+    this.depth = 0
+    this.diameter = 0
+    this.volume = 0
+    this.weight = 0
+    this.density = 0
+  }
+}
+
+// Controller class that does all the calculations
+export class PhysicalPropertyCalculatorCtrl {
+  recalWeight(rec) {
+    if (rec.density > 0) {
+      rec.weight = parseFloat((rec.volume * rec.density).toFixed(2))
+    }
+  }
+
+  recalVolume(rec) {
+    if (rec.density > 0) {
+      rec.volume = parseFloat((rec.weight / rec.density).toFixed(2))
+    }
+  }
+
+  recalDensity(rec) {
+    if (rec.volume > 0) {
+      rec.density = parseFloat((rec.weight / rec.volume).toFixed(2))
+    }
+  }
+
+  recalc(rec) {
+    switch (rec.dirtyField) {
+      case DirtyField.VOLUME:
+        if (rec.weight > 0) {
+          this.recalDensity(rec)
+        } else if (rec.density > 0) {
+          this.recalWeight(rec)
+        }
+        break
+      case DirtyField.WEIGHT:
+        if (rec.volume > 0) {
+          this.recalDensity(rec)
+        } else if (rec.density > 0) {
+          this.recalVolume(rec)
+        }
+        break
+      case DirtyField.DENSITY:
+        if (rec.volume > 0) {
+          this.recalWeight(rec)
+        } else if (rec.weight > 0) {
+          this.recalVolume(rec)
+        }
+        break
+    }
+  }
+
+  get(keys) {
+    // Create a new PhysicalPropertyCalculator object and fill it from 'keys'
+    const rec = new PhysicalPropertyCalculator()
+    rec.dirtyField = keys[0]
+    rec.shape = keys[1]
+    rec.length = keys[2]
+    rec.width = keys[3]
+    rec.depth = keys[4]
+    rec.diameter = keys[5]
+    rec.volume = keys[6]
+    rec.weight = keys[7]
+    rec.density = keys[8]
+
+    this.getRec(rec) // Perform the main calculation
+
+    return rec
+  }
+
+  getRec(rec) {
+    switch (rec.dirtyField) {
+      case DirtyField.DIAMETER:
+      case DirtyField.LENGTH:
+      case DirtyField.WIDTH:
+      case DirtyField.DEPTH:
+        // If a dimension changed, recalculate volume first
+        if (rec.length > 0) {
+          rec.dirtyField = DirtyField.VOLUME
+
+          if (rec.shape === GeometricShape.CUBIC) {
+            if (rec.width > 0 && rec.depth > 0) {
+              rec.volume = parseFloat((rec.length * rec.width * rec.depth).toFixed(2))
+            }
+          } else if (rec.diameter > 0) {
+            // Cylinder volume
+            rec.volume = parseFloat((rec.length * Math.PI * Math.pow(rec.diameter / 2, 2)).toFixed(2))
+          }
+          this.recalc(rec)
+        }
+        break
+
+      case DirtyField.VOLUME:
+      case DirtyField.WEIGHT:
+      case DirtyField.DENSITY:
+        // If volume, weight, or density is changed directly, recalc
+        this.recalc(rec)
+        break
+    }
+  }
+
+  log(object, trxType) {
+    console.log(`Logging: ${trxType}`, object)
+  }
+}
