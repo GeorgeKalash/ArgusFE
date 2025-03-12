2fe594289fa68db85574cde4e2f1c2133f8e7a26
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-03-12 17:20:48 +0200

    all cases for filtering

--- src/pages/rt-cor-disp-control/Forms/CorrespondentDispersalControlForm.js
+++ src/pages/rt-cor-disp-control/Forms/CorrespondentDispersalControlForm.js
@@ -1,6 +1,5 @@
 import { Grid } from '@mui/material'
 import { useContext } from 'react'
-import * as yup from 'yup'
 import { RequestsContext } from 'src/providers/RequestsContext'
 import { DataSets } from 'src/resources/DataSets'
 import { RemittanceSettingsRepository } from 'src/repositories/RemittanceRepository'
@@ -39,10 +38,7 @@ const CorrespondentDispersalForm = ({ recordId, labels, maxAccess, interfaceId }
     onSubmit: async obj => {
       const filteredObj = {
         ...obj,
-        items:
-          obj.items
-            ?.filter(item => item.corDeliveryMode) 
-            .map(({ items, ...rest }) => rest) || [] 
+        items: obj.items?.filter(item => item.corDeliveryMode).map(({ id, items, ...rest }) => rest) || []
       }
 
       await postRequest({
@@ -66,30 +62,361 @@ const CorrespondentDispersalForm = ({ recordId, labels, maxAccess, interfaceId }
     })
   }
 
+  const fetchPlants = async (plantId, plantName) => {
+    if (plantId) return [{ recordId: plantId, name: plantName }]
+    const plantRes = await getRequest({ extension: SystemRepository.Plant.qry })
+
+    return plantRes?.list || []
+  }
+
+  const fetchCountries = async (countryId, countryName) => {
+    if (countryId) return [{ recordId: countryId, name: countryName }]
+
+    const countryRes = await getRequest({
+      extension: RemittanceBankInterface.Countries.qry,
+      parameters: `_interfaceId=${interfaceId || 0}`
+    })
+
+    return countryRes?.list?.filter(country => country.recordId) || []
+  }
+
+  const fetchCurrencies = async countryId => {
+    const currencyRes = await getRequest({
+      extension: RemittanceBankInterface.Currencies.qry,
+      parameters: `_interfaceId=${interfaceId || 0}${countryId ? `&_countryId=${countryId}` : ''}`
+    })
+
+    return currencyRes?.list?.filter(currency => currency.recordId) || []
+  }
+
+  const fetchData = async (countryId, currencyId, plantId) => {
+    return await getRequest({
+      extension: RemittanceSettingsRepository.CorDispControl.qry,
+      parameters: `_corId=${recordId}&_countryId=${countryId || 0}&_currencyId=${currencyId || 0}&_plantId=${
+        plantId || 0
+      }`
+    })
+  }
+
   const onPreview = async () => {
-    if (formik.values.plantId && formik.values.countryId && formik.values.currencyId) {
-      const dispersalModes = await getDispersalMode()
+    const { plantId, countryId, currencyId, plantName, countryName } = formik.values
+    const dispersalModes = await getDispersalMode()
 
-      const res = await getRequest({
-        extension: RemittanceSettingsRepository.CorDispControl.qry,
-        parameters: `_corId=${formik.values.corId}&_countryId=${formik.values.countryId}&_currencyId=${formik.values.currencyId}&_plantId=${formik.values.plantId}`
+    const resData = (await fetchData(countryId, currencyId, plantId))?.list || []
+
+    if (plantId && countryId && currencyId) {
+      formik.setFieldValue(
+        'items',
+        dispersalModes.map((mode, index) => {
+          const existingItem = resData.find(item => parseInt(item.dispersalType) == mode.key)
+
+          return {
+            id: index + 1,
+            dispersalTypeName: mode.value,
+            dispersalType: mode.key,
+            corDeliveryModeName: existingItem?.deliveryModeDescription,
+            corDeliveryMode: existingItem?.deliveryMode,
+            ...formik.values,
+            ...existingItem
+          }
+        })
+      )
+
+      return
+    }
+
+    if (currencyId && !plantId && !countryId) {
+      const plants = await fetchPlants(plantId, plantName)
+      const countries = await fetchCountries(countryId, countryName)
+      if (!plants.length || !countries.length) return
+
+      formik.setFieldValue('items', [
+        ...new Map(
+          plants
+            .flatMap(plant =>
+              countries.flatMap(country =>
+                dispersalModes.map(mode => {
+                  const existingItem = resData.find(
+                    item =>
+                      parseInt(item.dispersalType) == mode.key &&
+                      item.plantId == plant.recordId &&
+                      item.countryId == country.recordId
+                  )
+
+                  return {
+                    id: `${plant.recordId}-${country.recordId}-${mode.key}`,
+                    ...formik.values,
+                    plantId: plant.recordId,
+                    plantName: plant.name,
+                    countryId: country.recordId,
+                    countryName: country.name,
+                    currencyId,
+                    dispersalTypeName: mode.value,
+                    dispersalType: mode.key,
+                    corDeliveryModeName: existingItem?.deliveryModeDescription || null,
+                    corDeliveryMode: existingItem?.deliveryMode || null,
+                    ...existingItem
+                  }
+                })
+              )
+            )
+            .map(i => [i.id, i])
+        ).values()
+      ])
+
+      return
+    }
+
+    if (countryId && !plantId && !currencyId) {
+      const plants = await fetchPlants(plantId, plantName)
+      if (!plants.length) return
+      const currencies = await fetchCurrencies(countryId)
+      if (!currencies.length) return
+
+      formik.setFieldValue(
+        'items',
+        plants.flatMap(plant =>
+          currencies.flatMap(currency =>
+            dispersalModes.map(mode => {
+              const existingItem = resData.find(
+                item =>
+                  parseInt(item.dispersalType) == mode.key &&
+                  item.plantId == plant.recordId &&
+                  item.currencyId == currency.recordId
+              )
+
+              return {
+                id: `${plant.recordId}-${currency.recordId}-${mode.key}`,
+                ...formik.values,
+                plantId: plant.recordId,
+                plantName: plant.name,
+                currencyId: currency.recordId,
+                currencyName: currency.name,
+                dispersalTypeName: mode.value,
+                dispersalType: mode.key,
+                corDeliveryModeName: existingItem?.deliveryModeDescription || null,
+                corDeliveryMode: existingItem?.deliveryMode || null,
+                ...existingItem
+              }
+            })
+          )
+        )
+      )
+
+      return
+    }
+
+    if (plantId && !countryId && !currencyId) {
+      const countries = await fetchCountries(countryId, countryName)
+      if (!countries.length) return
+
+      const currencies = (
+        await Promise.all(
+          countries.map(async country => {
+            return (
+              (await fetchCurrencies(country.recordId)).map(currency => ({
+                ...currency,
+                countryId: country.recordId
+              })) || []
+            )
+          })
+        )
+      ).flat()
+
+      if (!currencies.length) return
+
+      formik.setFieldValue(
+        'items',
+        countries.flatMap(country =>
+          currencies
+            .filter(currency => currency.countryId === country.recordId)
+            .flatMap(currency =>
+              dispersalModes
+                .map(mode => {
+                  if (!country.recordId || !currency.recordId) return null
+
+                  const existingItem = resData.find(
+                    item =>
+                      parseInt(item.dispersalType) == mode.key &&
+                      item.countryId == country.recordId &&
+                      item.currencyId == currency.recordId
+                  )
+
+                  return {
+                    id: `${plantId}-${country.recordId}-${currency.recordId}-${mode.key}`,
+                    ...formik.values,
+                    plantId,
+                    plantName,
+                    countryId: country.recordId,
+                    countryName: country.name,
+                    currencyId: currency.recordId,
+                    currencyName: currency.name,
+                    dispersalTypeName: mode.value,
+                    dispersalType: mode.key,
+                    corDeliveryModeName: existingItem?.deliveryModeDescription || null,
+                    corDeliveryMode: existingItem?.deliveryMode || null,
+                    ...existingItem
+                  }
+                })
+                .filter(Boolean)
+            )
+        )
+      )
+
+      return
+    }
+
+    if (plantId && countryId && !currencyId) {
+      const currencyRes = await getRequest({
+        extension: RemittanceBankInterface.Currencies.qry,
+        parameters: `_interfaceId=${interfaceId || 0}&_countryId=${countryId}`
       })
 
-      const resData = res?.list || []
+      const currencies = currencyRes?.list || []
+      if (!currencies.length) return
 
-      const items = dispersalModes.map((mode, index) => {
-        const existingItem = resData.find(item => parseInt(item.dispersalType) === mode.id)
+      formik.setFieldValue(
+        'items',
+        currencies.flatMap(currency =>
+          dispersalModes.map(mode => {
+            const existingItem = resData.find(
+              item =>
+                parseInt(item.dispersalType) == mode.key &&
+                item.plantId == plantId &&
+                item.countryId == countryId &&
+                item.currencyId == currency.recordId
+            )
 
-        return {
-          id: index + 1,
-          dispersalTypeName: mode.value,
-          dispersalType: mode.id,
-          ...formik.values,
-          ...existingItem
-        }
-      })
+            return {
+              id: `${plantId}-${countryId}-${currency.recordId}-${mode.key}`,
+              ...formik.values,
+              plantId,
+              plantName,
+              countryId,
+              countryName,
+              currencyId: currency.recordId,
+              currencyName: currency.name,
+              dispersalTypeName: mode.value,
+              dispersalType: mode.key,
+              corDeliveryModeName: existingItem?.deliveryModeDescription || null,
+              corDeliveryMode: existingItem?.deliveryMode || null,
+              ...existingItem
+            }
+          })
+        )
+      )
+    }
 
-      formik.setFieldValue('items', items)
+    if (plantId && !countryId && currencyId) {
+      const countries = (await fetchCountries(countryId, countryName)).filter(country => country.recordId)
+      if (!countries.length) return
+
+      formik.setFieldValue(
+        'items',
+        countries.flatMap(country =>
+          dispersalModes.map(mode => {
+            const existingItem = resData.find(
+              item =>
+                parseInt(item.dispersalType) == mode.key &&
+                item.plantId == plantId &&
+                item.currencyId == currencyId &&
+                item.countryId == country.recordId
+            )
+
+            return {
+              id: `${plantId}-${country.recordId}-${currencyId}-${mode.key}`,
+              ...formik.values,
+              plantId,
+              plantName,
+              countryId: country.recordId,
+              countryName: country.name,
+              currencyId,
+              dispersalTypeName: mode.value,
+              dispersalType: mode.key,
+              corDeliveryModeName: existingItem?.deliveryModeDescription || null,
+              corDeliveryMode: existingItem?.deliveryMode || null,
+              ...existingItem
+            }
+          })
+        )
+      )
+    }
+
+    if (!plantId && countryId && currencyId) {
+      const plants = await fetchPlants(plantId, plantName)
+      if (!plants.length) return
+
+      formik.setFieldValue(
+        'items',
+        plants.flatMap(plant =>
+          dispersalModes.map(mode => {
+            const existingItem = resData.find(
+              item => parseInt(item.dispersalType) == mode.key && item.plantId == plant.recordId
+            )
+
+            return {
+              id: `${plant.recordId}-${mode.key}`,
+              ...formik.values,
+              plantId: plant.recordId,
+              plantName: plant.name,
+              dispersalTypeName: mode.value,
+              dispersalType: mode.key,
+              corDeliveryModeName: existingItem?.deliveryModeDescription || null,
+              corDeliveryMode: existingItem?.deliveryMode || null,
+              ...existingItem
+            }
+          })
+        )
+      )
+
+      return
+    }
+
+    if (!plantId && !countryId && !currencyId) {
+      const plants = await fetchPlants(plantId, plantName)
+      const countries = await fetchCountries(countryId, countryName)
+
+      if (!plants.length || !countries.length) return
+
+      const currenciesMap = {}
+      for (const country of countries) {
+        currenciesMap[country.recordId] = await fetchCurrencies(country.recordId)
+      }
+
+      formik.setFieldValue(
+        'items',
+        plants.flatMap(plant =>
+          countries.flatMap(country =>
+            (currenciesMap[country.recordId] || []).flatMap(currency =>
+              dispersalModes.map(mode => {
+                const existingItem = resData.find(
+                  item =>
+                    parseInt(item.dispersalType) == mode.key &&
+                    item.plantId == plant.recordId &&
+                    item.countryId == country.recordId &&
+                    item.currencyId == currency.recordId
+                )
+
+                return {
+                  id: `${plant.recordId}-${country.recordId}-${currency.recordId}-${mode.key}`,
+                  ...formik.values,
+                  plantId: plant.recordId,
+                  plantName: plant.name,
+                  countryId: country.recordId,
+                  countryName: country.name,
+                  currencyId: currency.recordId,
+                  currencyName: currency.name,
+                  dispersalTypeName: mode.value,
+                  dispersalType: mode.key,
+                  corDeliveryModeName: existingItem?.deliveryModeDescription || null,
+                  corDeliveryMode: existingItem?.deliveryMode || null,
+                  ...existingItem
+                }
+              })
+            )
+          )
+        )
+      )
     }
   }
 
@@ -119,12 +446,11 @@ const CorrespondentDispersalForm = ({ recordId, labels, maxAccess, interfaceId }
       props: {
         endpointId: RemittanceBankInterface.DeliveryMode.qry,
         parameters: `_interfaceId=${interfaceId}&_corId=${recordId}`,
-        displayField: 'reference',
-        valueField: 'recordId',
+        displayField: 'deliveryModeDescription',
+        valueField: 'deliveryMode',
         mapping: [
-          { from: 'name', to: 'corDeliveryModeName' },
-          { from: 'reference', to: 'corDeliveryMode' },
-          { from: 'recordId', to: 'recordId' }
+          { from: 'deliveryModeDescription', to: 'corDeliveryModeName' },
+          { from: 'deliveryMode', to: 'corDeliveryMode' }
         ]
       }
     },
@@ -180,7 +506,6 @@ const CorrespondentDispersalForm = ({ recordId, labels, maxAccess, interfaceId }
               name='countryId'
               label={labels.country}
               valueField='recordId'
-              required
               displayField={['reference', 'name']}
               columnsInDropDown={[
                 { key: 'reference', value: 'Reference' },
@@ -209,7 +534,6 @@ const CorrespondentDispersalForm = ({ recordId, labels, maxAccess, interfaceId }
                 { key: 'name', value: 'Name' }
               ]}
               values={formik.values}
-              required
               maxAccess={maxAccess}
               onChange={(event, newValue) => {
                 formik.setFieldValue('currencyId', newValue?.recordId || 0)
@@ -250,6 +574,7 @@ const CorrespondentDispersalForm = ({ recordId, labels, maxAccess, interfaceId }
           value={formik.values.items}
           error={formik.errors.items}
           allowDelete={false}
+          allowAddNewLine={false}
           columns={columns}
         />
       </Grow>
