7ed6a4a79a6be4320bd6433d257eaf0999c64d28
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-03-21 12:57:28 +0200

    machines new screen

--- src/pages/mf-machines/Windows/MachinesWindow.js
+++ src/pages/mf-machines/Windows/MachinesWindow.js
@@ -1,32 +1,27 @@
-import Window from 'src/components/Shared/Window'
 import CustomTabPanel from 'src/components/Shared/CustomTabPanel'
+import { useState } from 'react'
+import { CustomTabs } from 'src/components/Shared/CustomTabs'
 import MachinesForm from '../forms/MachinesForm'
 import MachineSpecificationForm from '../forms/MachineSpecificationForm'
-import { useState } from 'react'
 
-const MachinesWindow = ({ onClose, labels, maxAccess, recordId }) => {
+export default function MachinesWindow({ labels, access, recordId }) {
   const [activeTab, setActiveTab] = useState(0)
 
-  const editMode = !!recordId
+  const [store, setStore] = useState({
+    recordId
+  })
+
+  const tabs = [{ label: labels.Machines }, { label: labels.MachineSpecification, disabled: !store.recordId }]
 
   return (
-    <Window
-      id='MachinesWindow'
-      Title={labels.Machines}
-      controlled={true}
-      onClose={onClose}
-      tabs={[{ label: labels.Machines }, { label: labels.MachineSpecification, disabled: !editMode }]}
-      activeTab={activeTab}
-      setActiveTab={setActiveTab}
-    >
+    <>
+      <CustomTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
       <CustomTabPanel index={0} value={activeTab}>
-        <MachinesForm labels={labels} maxAccess={maxAccess} recordId={recordId} />
+        <MachinesForm labels={labels} maxAccess={access} setStore={setStore} store={store} editMode={store.recordId}/>
       </CustomTabPanel>
       <CustomTabPanel index={1} value={activeTab}>
-        <MachineSpecificationForm labels={labels} maxAccess={maxAccess} recordId={recordId} />
+        <MachineSpecificationForm labels={labels} maxAccess={access} store={store} editMode={store.recordId}/>
       </CustomTabPanel>
-    </Window>
+    </>
   )
 }
-
-export default MachinesWindow
