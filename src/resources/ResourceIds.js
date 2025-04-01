818701fb199049c7a1dae32cd683a6f4e208f49c
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-04-01 09:52:23 +0300

    fixing tl

--- src/resources/ResourceIds.js
+++ src/resources/ResourceIds.js
@@ -388,5 +388,6 @@ export const ResourceIds = {
   SAVerifyTransIntegrity: 51601,
   PUDocumentTypeDefaults: 50103,
   SyncSalesTrx: 51603,
-  ProductionOrder: 42124
+  ProductionOrderItem: 42124,
+  ProductionOrder: 42337
 }
