f9b90abeacdca153bfa6ffcfc565d2972681bae0
FatimaSalami <fatima.salami@softmachine.co>
2025-04-23 12:16:03 +0300

    fixes

--- src/pages/shipments/forms/ShipmentsForm.js
+++ src/pages/shipments/forms/ShipmentsForm.js
@@ -45,8 +45,8 @@ export default function ShipmentsForm({ labels, maxAccess: access, recordId, inv
     objectName: 'header'
   })
 
-  const defplantId = parseInt(userDefaultsData?.list?.find(obj => obj.key === 'plantId')?.value)
-  const defSiteId = parseInt(userDefaultsData?.list?.find(obj => obj.key === 'siteId')?.value)
+  const defplantId = parseInt(userDefaultsData?.list?.find(obj => obj.key === 'plantId')?.value) || null
+  const defSiteId = parseInt(userDefaultsData?.list?.find(obj => obj.key === 'siteId')?.value) || null
   const marginDefault = parseInt(defaultsData?.list?.find(obj => obj.key === 'POSHPVarPct')?.value) || 0
 
   const { formik } = useForm({
@@ -111,7 +111,7 @@ export default function ShipmentsForm({ labels, maxAccess: access, recordId, inv
     validationSchema: yup.object({
       header: yup.object({
         plantId: yup.number().required(),
-        date: yup.string().required(),
+        date: yup.date().required(),
         siteId: yup.number().required(),
         vendorId: yup.number().required()
       }),
