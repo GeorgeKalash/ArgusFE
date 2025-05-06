808ae1bf81028c00c3334ad48cef3905c89390e0
NoraBitarSM <Nora.Bitar@SoftMachine.co>
2025-05-06 11:33:40 +0300

    user-dashboard

--- src/pages/user-dashboard/index.js
+++ src/pages/user-dashboard/index.js
@@ -5,6 +5,7 @@ import Table from 'src/components/Shared/Table'
 import WindowToolbar from 'src/components/Shared/WindowToolbar'
 import { RequestsContext } from 'src/providers/RequestsContext'
 import { ResourceIds } from 'src/resources/ResourceIds'
+import { Module } from 'src/resources/Module'
 import { SystemRepository } from 'src/repositories/SystemRepository'
 import { useResourceQuery } from 'src/hooks/resource'
 import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
@@ -25,18 +26,17 @@ const UserDashboard = () => {
 
   const {
     query: { data },
-    labels: _labels,
+    labels,
     access
   } = useResourceQuery({
     queryFn: fetchGridData,
-    endpointId: SystemRepository.SystemChecks.qry,
-    datasetId: ResourceIds.SystemChecks
+    datasetId: ResourceIds.UserDashboard
   })
 
   async function fetchGridData() {
     const appletsData = await getRequest({
       extension: AccessControlRepository.ModuleClass.qry2,
-      parameters: `_moduleId=60`
+      parameters: `_moduleId=${Module.Dashboard}`
     })
 
     const checkedApplets = await getRequest({
@@ -44,38 +44,32 @@ const UserDashboard = () => {
       parameters: `_userId=${_userId}`
     })
 
-    //console.log(appletsData, checkedApplets)
-
-    const mergedSYCheckedList = appletsData.list.map(systemCheckItem => {
+    const mergedList = appletsData.list.map(appletItem => {
       const item = {
-        appletId: systemCheckItem.key,
-        appletName: systemCheckItem.value,
+        appletId: appletItem.key,
+        appletName: appletItem.value,
         checked: false,
         value: false
       }
-      console.log(item)
       const matchingTemplate = checkedApplets.list.find(y => item.appletId == y.appletId)
-      console.log(matchingTemplate)
       matchingTemplate && (item.checked = true)
       matchingTemplate && (item.value = true)
 
       return item
     })
 
-    //console.log(mergedSYCheckedList)
-
-    return { list: mergedSYCheckedList }
+    return { list: mergedList }
   }
 
   const columns = [
     {
       field: 'appletId',
-      headerName: _labels.appletId,
+      headerName: labels.appletId,
       flex: 1
     },
     {
       field: 'appletName',
-      headerName: _labels.appletName,
+      headerName: labels.appletName,
       flex: 3
     }
   ]
@@ -92,30 +86,30 @@ const UserDashboard = () => {
     setSearch(event?.target?.value ?? '')
   }
 
-  const handleSubmit = () => {
-    postChecks()
-  }
+  const handleSubmit = async () => {
+    var seqNo = 0
 
-  const postChecks = async () => {
-    const checkedObjects = data.list.filter(obj => obj.checked)
-    console.log(checkedObjects)
+    const mergedSYCheckedList = data.list
+      .filter(obj => obj.checked)
+      .map(appletItem => {
+        const item = {
+          ...appletItem,
+          seqNo: ++seqNo,
+          userId: _userId
+        }
 
-    // checkedObjects.forEach(obj => {
-    //   obj.scope = 1
-    //   obj.masterId = 0
-    //   obj.value = true
-    // })
-    // const resultObject = {
-    //   scope: 1,
-    //   masterId: 0,
-    //   items: checkedObjects
-    // }
-    // await postRequest({
-    //   extension: SystemRepository.SystemChecks.set,
-    //   record: JSON.stringify(resultObject)
-    // }).then(() => {
-    //   toast.success(platformLabels.Updated)
-    // })
+        return item
+      })
+
+    await postRequest({
+      extension: SystemRepository.DynamicDashboard.set2,
+      record: JSON.stringify({
+        userId: _userId,
+        applets: mergedSYCheckedList
+      })
+    }).then(() => {
+      toast.success(platformLabels.Updated)
+    })
   }
 
   return (
@@ -133,7 +127,6 @@ const UserDashboard = () => {
               onChange={handleSearchChange}
               onSearch={e => setSearch(e)}
               search={true}
-              height={35}
             />
           </Grid>
         </Grid>
@@ -143,15 +136,13 @@ const UserDashboard = () => {
           columns={columns}
           gridData={{ list: filteredData }}
           rowId={['appletId']}
-          isLoading={false}
           maxAccess={access}
           showCheckboxColumn={true}
-          handleCheckedRows={() => {}}
           pagination={false}
         />
       </Grow>
       <Fixed>
-        <WindowToolbar isSaved={true} onSave={handleSubmit} smallBox={true} />
+        <WindowToolbar isSaved={true} onSave={handleSubmit} />
       </Fixed>
     </VertLayout>
   )
