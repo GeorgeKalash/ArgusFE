5bdf46ef22c3135dd4dd5f3ab7d9fea2031d1f04
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2025-05-22 10:04:44 +0300

    debugging

--- src/pages/rt-receipt-vouchers/forms/POSForm.js
+++ src/pages/rt-receipt-vouchers/forms/POSForm.js
@@ -63,7 +63,9 @@ export default function POSForm({ labels, form, maxAccess, amount }) {
     const res = await axios.post(`${process.env.NEXT_PUBLIC_POS_URL}/api/Ingenico/start_PUR`, formik.values)
     if (res.data) {
       setSubmitting(false)
+
       const formattedText = res.data.replace(/ /g, '\n')
+
       stack({
         Component: PopupDialog,
         props: {
@@ -93,16 +95,16 @@ export default function POSForm({ labels, form, maxAccess, amount }) {
     formik.setFieldValue('cashAccountName', res?.record?.name)
   }
 
-  useEffect(() => {
-    ;(async function () {
-      await fillCashAccount()
+  // useEffect(() => {
+  //   ;(async function () {
+  //     await fillCashAccount()
 
-      const response = await getRequestFullEndPoint({
-        endPoint: `${process.env.NEXT_PUBLIC_POS_URL}/api/Ingenico/checkDevice?_port=${process.env.NEXT_PUBLIC_POS_PORT}`
-      })
-      formik.setFieldValue('posSelected', response?.data ? 2 : 1)
-    })()
-  }, [])
+  //     const response = await getRequestFullEndPoint({
+  //       endPoint: `${process.env.NEXT_PUBLIC_POS_URL}/api/Ingenico/checkDevice?_port=${process.env.NEXT_PUBLIC_POS_PORT}`
+  //     })
+  //     formik.setFieldValue('posSelected', response?.data ? 2 : 1)
+  //   })()
+  // }, [])
 
   return (
     <FormShell
