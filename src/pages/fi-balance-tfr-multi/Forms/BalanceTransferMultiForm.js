3ecef2949574e0c5f0c156de5632f3c90038b62f
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-05-28 13:20:20 +0300

    balance transfer multi

--- src/pages/fi-balance-tfr-multi/Forms/BalanceTransferMultiForm.js
+++ src/pages/fi-balance-tfr-multi/Forms/BalanceTransferMultiForm.js
@@ -269,6 +269,7 @@ export default function BalanceTransferMultiForm({ labels, access, recordId, win
       key: 'GL',
       condition: true,
       onClick: 'onClickGL',
+      valuesPath: formik.values.header,
       datasetId: ResourceIds.GLBalanceTransferMultiAccounts,
       disabled: !editMode
     },
