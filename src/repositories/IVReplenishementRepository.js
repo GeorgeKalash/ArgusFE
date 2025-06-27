bea7b498e82880f8a46ce80fbb59819a7fd8245a
Imtithal Halalo <109606167+imtithalhalalo@users.noreply.github.com>
2025-06-27 16:07:40 +0300

    plant settings

--- src/repositories/IVReplenishementRepository.js
+++ src/repositories/IVReplenishementRepository.js
@@ -47,5 +47,9 @@ export const IVReplenishementRepository = {
   },
   PurchaseRequest: {
     generate: service + 'generatePR'
+  },
+  PlantSettings: {
+    get: service + 'getPLT',
+    set: service + 'setPLT'
   }
 }
