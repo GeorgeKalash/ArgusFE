9223a8bd7b175a9a6b900c6c7c55898ecfc676e5
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2025-07-09 16:45:12 +0300

    add new column

--- src/pages/mf-job-orders/form/MaterialsTab.js
+++ src/pages/mf-job-orders/form/MaterialsTab.js
@@ -40,6 +40,12 @@ export default function MaterialsTab({ store, maxAccess, labels }) {
       flex: 1
     },
     {
+      field: 'loss',
+      headerName: labels.loss,
+      flex: 1,
+      type: 'number'
+    },
+    {
       field: 'qty',
       headerName: labels.qty,
       flex: 1,
@@ -61,8 +67,9 @@ export default function MaterialsTab({ store, maxAccess, labels }) {
 
   const totQty = data?.list?.reduce((qtySum, row) => {
     const qtyValue = parseFloat(row?.qty?.toString().replace(/,/g, '')) || 0
+    const loss = parseFloat(row?.loss?.toString().replace(/,/g, '')) || 0
 
-    return qtySum + qtyValue
+    return qtySum + qtyValue - loss
   }, 0)
 
   async function fetchGridData() {
