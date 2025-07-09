2737fd5667bdf914140650c4ab083c0f0cb0d07f
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2025-07-09 16:45:12 +0300

    fixes

--- src/pages/mf-job-orders/form/MaterialsTab.js
+++ src/pages/mf-job-orders/form/MaterialsTab.js
@@ -46,6 +46,12 @@ export default function MaterialsTab({ store, maxAccess, labels }) {
       type: 'number'
     },
     {
+      field: 'loss',
+      headerName: labels.loss,
+      flex: 1,
+      type: 'number'
+    },
+    {
       field: 'unitCost',
       headerName: labels.unitCost,
       flex: 1,
@@ -61,8 +67,9 @@ export default function MaterialsTab({ store, maxAccess, labels }) {
 
   const totQty = data?.list?.reduce((qtySum, row) => {
     const qtyValue = parseFloat(row?.qty?.toString().replace(/,/g, '')) || 0
+    const loss = parseFloat(row?.loss?.toString().replace(/,/g, '')) || 0
 
-    return qtySum + qtyValue
+    return qtySum + qtyValue - loss
   }, 0)
 
   async function fetchGridData() {
