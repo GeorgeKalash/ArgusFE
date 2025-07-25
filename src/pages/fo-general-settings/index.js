7e3956d3c0ea028da46372361810653792f4e2be
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2025-07-25 13:45:46 +0300

    ne screen

--- src/pages/fo-general-settings/index.js
+++ src/pages/fo-general-settings/index.js
@@ -1,6 +1,5 @@
 import { Grid } from '@mui/material'
-import React, { useContext, useEffect, useRef } from 'react'
-import CustomTextField from 'src/components/Inputs/CustomTextField'
+import React, { useContext, useEffect } from 'react'
 import FormShell from 'src/components/Shared/FormShell'
 import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
 import useResourceParams from 'src/hooks/useResourceParams'
@@ -9,70 +8,89 @@ import { SystemRepository } from 'src/repositories/SystemRepository'
 import { ResourceIds } from 'src/resources/ResourceIds'
 import toast from 'react-hot-toast'
 import { useForm } from 'src/hooks/form'
-import { getStorageData } from 'src/storage/storage'
 import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
 import { InventoryRepository } from 'src/repositories/InventoryRepository'
 import { ManufacturingRepository } from 'src/repositories/ManufacturingRepository'
-import { SCRepository } from 'src/repositories/SCRepository'
+import { FoundryRepository } from 'src/repositories/FoundryRepository'
+import { ResourceLookup } from 'src/components/Shared/ResourceLookup'
+import { ControlContext } from 'src/providers/ControlContext'
 
 const FoGeneralSettings = () => {
   const { getRequest, postRequest } = useContext(RequestsContext)
+  const { platformLabels } = useContext(ControlContext)
 
   const { labels, access } = useResourceParams({
     datasetId: ResourceIds.GeneralSettings
   })
 
   useEffect(() => {
-    getData()
+    ;(async function () {
+      const res = await getRequest({
+        extension: SystemRepository.Defaults.qry,
+        parameters: `_filter=`
+      })
+
+      const keysToExtract = [
+        'waxSiteId',
+        'castingSiteId',
+        'meltingSiteId',
+        'mfCentralSiteId',
+        'castingWorkCenterId',
+        'mfRestoredMetalItem',
+        'waxOperationId',
+        'castingOperationId'
+      ]
+
+      const myObject = {}
+
+      for (const { key, value } of res.list) {
+        if (keysToExtract.includes(key)) {
+          myObject[key] = value ? parseInt(value) : null
+
+          if (key === 'mfRestoredMetalItem' && value) {
+            const itemRes = await getRequest({
+              extension: InventoryRepository.Item.get,
+              parameters: `_recordId=${parseInt(value)}`
+            })
+
+            myObject['mfRestoredMetalItemRef'] = itemRes.record?.sku || null
+            myObject['mfRestoredMetalItemName'] = itemRes.record?.name || ''
+          }
+        }
+      }
+
+      formik.setValues(myObject)
+    })()
   }, [])
-
-  async function getData() {
-    const res = await getRequest({
-      extension: SystemRepository.CompanyInfo.get,
-      parameters: `_filter=`
-    })
-
-    formik.setValues({
-      ...formik.values,
-      name: res.record.name,
-      webSite: res.record.taxNo,
-      taxNo: res.record.taxNo,
-      licenseNo: res.record.licenseNo,
-      plantId: res.record.plantId,
-      crNo: res.record.crNo,
-      logoUrl: res.record.logoUrl,
-      flName: res.record.flName
-    })
-  }
 
   const { formik } = useForm({
     maxAccess: access,
-    enableReinitialize: true,
     validateOnChange: true,
     initialValues: {
-      plantId: '',
-      accountId: getStorageData('userData')?.accountId,
-      name: '',
-      webSite: '',
-      taxNo: '',
-      licenseNo: '',
-      crNo: '',
-      logoUrl: '',
-      flName: ''
+      waxSiteId: null,
+      castingSiteId: null,
+      meltingSiteId: null,
+      mfCentralSiteId: null,
+      castingWorkCenterId: null,
+      mfRestoredMetalItem: null,
+      mfRestoredMetalItemRef: '',
+      mfRestoredMetalItemName: '',
+      waxOperationId: null,
+      castingOperationId: null
     },
-    onSubmit: async values => {
-      await post(values)
+    onSubmit: async obj => {
+      const data = Object.entries(obj).map(([key, value]) => ({
+        key,
+        value
+      }))
+
+      await postRequest({
+        extension: SystemRepository.Defaults.set,
+        record: JSON.stringify({ sysDefaults: data })
+      })
+      toast.success(platformLabels.Edited)
     }
   })
-
-  const post = async obj => {
-    await postRequest({
-      extension: SystemRepository.CompanyInfo.set,
-      record: JSON.stringify({ ...obj, attachment: null })
-    })
-
-    toast.success('Record Edited Successfully')
-  }
 
   return (
     <FormShell
@@ -84,12 +102,12 @@ const FoGeneralSettings = () => {
       maxAccess={access}
     >
       <VertLayout>
-        <Grid container spacing={2} xs={6}>
+        <Grid container spacing={3} xs={5}>
           <Grid item xs={12}>
             <ResourceComboBox
-              endpointId={SCRepository.Sites.qry}
-              name='siteId'
-              label={labels.site}
+              endpointId={InventoryRepository.Site.qry}
+              name='waxSiteId'
+              label={labels.waxSite}
               columnsInDropDown={[
                 { key: 'reference', value: 'Reference' },
                 { key: 'name', value: 'Name' }
@@ -99,37 +117,146 @@ const FoGeneralSettings = () => {
               values={formik.values}
               maxAccess={access}
               onChange={(event, newValue) => {
-                formik.setFieldValue('siteId', newValue?.recordId || null)
+                formik.setFieldValue('waxSiteId', newValue?.recordId || null)
               }}
-              required
-              error={formik.touched.siteId && Boolean(formik.errors.siteId)}
+              error={formik.touched.waxSiteId && Boolean(formik.errors.waxSiteId)}
             />
           </Grid>
-          {/* <Grid item xs={12}>
-            <ResourceComboBox
-              endpointId={FinancialRepository.Account.snapshot}
-              label={labels.module}
-              name='moduleId'
-              values={formik.values}
-              valueField='key'
-              displayField='value'
-              maxAccess={access}
-              required
-              readOnly={formik.values.moduleId}
-              onChange={(event, newValue) => {
-                formik.setFieldValue('moduleId', newValue?.key || null)
-              }}
-            />
-          </Grid> */}
           <Grid item xs={12}>
-            <CustomTextField
-              name='webSite'
-              label={labels.website}
-              value={formik.values?.webSite}
-              onChange={formik.handleChange}
-              onClear={() => formik.setFieldValue('webSite', '')}
-              error={formik.errors && Boolean(formik.errors.webSite)}
+            <ResourceComboBox
+              endpointId={InventoryRepository.Site.qry}
+              name='castingSiteId'
+              label={labels.castingSite}
+              columnsInDropDown={[
+                { key: 'reference', value: 'Reference' },
+                { key: 'name', value: 'Name' }
+              ]}
+              valueField='recordId'
+              displayField={['reference', 'name']}
+              values={formik.values}
               maxAccess={access}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('castingSiteId', newValue?.recordId || null)
+              }}
+              error={formik.touched.castingSiteId && Boolean(formik.errors.castingSiteId)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <ResourceComboBox
+              endpointId={InventoryRepository.Site.qry}
+              name='meltingSiteId'
+              label={labels.meltingSite}
+              columnsInDropDown={[
+                { key: 'reference', value: 'Reference' },
+                { key: 'name', value: 'Name' }
+              ]}
+              valueField='recordId'
+              displayField={['reference', 'name']}
+              values={formik.values}
+              maxAccess={access}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('meltingSiteId', newValue?.recordId || null)
+              }}
+              error={formik.touched.meltingSiteId && Boolean(formik.errors.meltingSiteId)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <ResourceComboBox
+              endpointId={InventoryRepository.Site.qry}
+              name='mfCentralSiteId'
+              label={labels.centralSite}
+              columnsInDropDown={[
+                { key: 'reference', value: 'Reference' },
+                { key: 'name', value: 'Name' }
+              ]}
+              valueField='recordId'
+              displayField={['reference', 'name']}
+              values={formik.values}
+              maxAccess={access}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('mfCentralSiteId', newValue?.recordId || null)
+              }}
+              error={formik.touched.mfCentralSiteId && Boolean(formik.errors.mfCentralSiteId)}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <ResourceComboBox
+              endpointId={FoundryRepository.WorkCenter.qry}
+              name='castingWorkCenterId'
+              label={labels.castingWorkCenter}
+              valueField='workCenterId'
+              displayField='workCenterName'
+              values={formik.values}
+              maxAccess={access}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('castingWorkCenterId', newValue?.workCenterId || null)
+              }}
+              error={formik.touched.castingWorkCenterId && Boolean(formik.errors.castingWorkCenterId)}
+            />
+          </Grid>
+
+          <Grid item xs={12}>
+            <ResourceLookup
+              endpointId={InventoryRepository.Item.snapshot}
+              name='mfRestoredMetalItem'
+              label={labels.restoredMetalLossItem}
+              valueField='sku'
+              displayField='name'
+              valueShow='mfRestoredMetalItemRef'
+              secondValueShow='mfRestoredMetalItemName'
+              form={formik}
+              columnsInDropDown={[
+                { key: 'sku', value: 'SKU' },
+                { key: 'name', value: 'Name' }
+              ]}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('mfRestoredMetalItem', newValue?.recordId || 0)
+                formik.setFieldValue('mfRestoredMetalItemName', newValue?.name || '')
+                formik.setFieldValue('mfRestoredMetalItemRef', newValue?.sku || '')
+              }}
+              displayFieldWidth={2}
+              maxAccess={access}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <ResourceComboBox
+              endpointId={ManufacturingRepository.Operation.qry}
+              parameters='_workCenterId=0'
+              name='waxOperationId'
+              label={labels.waxOperation}
+              columnsInDropDown={[
+                { key: 'reference', value: 'Reference' },
+                { key: 'name', value: 'Name' }
+              ]}
+              valueField='recordId'
+              displayField={['reference', 'name']}
+              values={formik.values}
+              maxAccess={access}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('waxOperationId', newValue?.recordId || null)
+              }}
+              error={formik.touched.waxOperationId && Boolean(formik.errors.waxOperationId)}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <ResourceComboBox
+              endpointId={ManufacturingRepository.Operation.qry}
+              parameters='_workCenterId=0'
+              name='castingOperationId'
+              label={labels.castingOperation}
+              columnsInDropDown={[
+                { key: 'reference', value: 'Reference' },
+                { key: 'name', value: 'Name' }
+              ]}
+              valueField='recordId'
+              displayField={['reference', 'name']}
+              values={formik.values}
+              maxAccess={access}
+              onChange={(event, newValue) => {
+                formik.setFieldValue('castingOperationId', newValue?.recordId || null)
+              }}
+              error={formik.touched.castingOperationId && Boolean(formik.errors.castingOperationId)}
             />
           </Grid>
         </Grid>
