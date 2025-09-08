24029f84701deb73f6dd5230d1d7b367de6dbc4e
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2025-09-08 13:38:55 +0300

    solebve merge conflict

--- src/repositories/RepairAndServiceRepository.js
+++ src/repositories/RepairAndServiceRepository.js
@@ -18,14 +18,11 @@ export const RepairAndServiceRepository = {
     get: service + 'getRT',
     set: service + 'setRT',
     del: service + 'delRT'
-<<<<<<< HEAD:src/repositories/RepairRepository.js
   },
   JobPosition: {
     page: service + 'pagePOS',
     get: service + 'getPOS',
     set: service + 'setPOS',
     del: service + 'delPOS'
-=======
->>>>>>> cc7556ead74301083a232cf5867098616f0b3b89:src/repositories/RepairAndServiceRepository.js
   }
 }
