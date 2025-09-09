4453e92a93bbf6baabcc6241d0f27bfe91c4c357
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2025-09-09 09:55:50 +0300

    taskTab

--- src/pages/maint-temp/Forms/TasksForm.js
+++ src/pages/maint-temp/Forms/TasksForm.js
@@ -14,11 +14,14 @@ import { RepairAndServiceRepository } from 'src/repositories/RepairAndServiceRep
 import { Grid } from '@mui/material'
 import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
 import { DataSets } from 'src/resources/DataSets'
+import CustomCheckBox from 'src/components/Inputs/CustomCheckBox'
+import CustomNumberField from 'src/components/Inputs/CustomNumberField'
+import CustomTextArea from 'src/components/Inputs/CustomTextArea'
 
 export default function TasksForm({ taskInfo, maxAccess, labels }) {
   const { getRequest, postRequest } = useContext(RequestsContext)
   const { platformLabels } = useContext(ControlContext)
-  const { templateId: recordId, taskId } = taskInfo
+  const { templateId: recordId, taskId } = taskInfo || {}
 
   const invalidate = useInvalidate({
     endpointId: RepairAndServiceRepository.MaintenanceTemplateTask.qry
@@ -40,7 +43,12 @@ export default function TasksForm({ taskInfo, maxAccess, labels }) {
       notes: ''
     },
     validationSchema: yup.object({
-      namePMT: yup.string().required()
+      namePMT: yup.string().required(),
+      tbdEvery: yup.number().min(1).required(),
+      tbdReminder: yup.number().min(1).required(),
+      tbhEvery: yup.number().min(1).required(),
+      tbhReminder: yup.number().min(1).required(),
+      expectedLaborHrs: yup.number().min(1).required()
     }),
     onSubmit: async obj => {
       const response = await postRequest({
@@ -76,9 +84,9 @@ export default function TasksForm({ taskInfo, maxAccess, labels }) {
     <FormShell resourceId={ResourceIds.MaintenanceTemplates} form={formik} maxAccess={maxAccess} editMode={editMode}>
       <VertLayout>
         <Grow>
-          <Grid container spacing={4}>
+          <Grid container spacing={2}>
             <Grid item xs={12}>
-              <ResourceComboBox
+              {/* <ResourceComboBox
                 endpointId={LogisticsRepository.LoCarrier.qry}
                 name='namePMT'
                 label={labels.name}
@@ -91,6 +99,118 @@ export default function TasksForm({ taskInfo, maxAccess, labels }) {
                   formik.setFieldValue('namePMT', newValue?.recordId || null)
                 }}
                 error={formik.touched.namePMT && Boolean(formik.errors.namePMT)}
+              /> */}
+            </Grid>
+            <Grid item xs={12}>
+              <CustomCheckBox
+                name='tbd'
+                value={formik.values?.tbd}
+                onChange={event => formik.setFieldValue('tbd', event.target.checked)}
+                label={labels.trackByDate}
+                maxAccess={maxAccess}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <CustomNumberField
+                name='tbdEvery'
+                label={labels.due}
+                value={formik?.values?.tbdEvery}
+                maxAccess={maxAccess}
+                readOnly={!formik.values.tbd}
+                required={formik.values.tbd}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('tbdEvery', null)}
+                error={formik.touched.tbdEvery && Boolean(formik.errors.tbdEvery)}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <ResourceComboBox
+                datasetId={DataSets.RS_FREQUENCY}
+                label={''}
+                name='tbdFrequency'
+                values={formik.values}
+                valueField='key'
+                displayField='value'
+                maxAccess={maxAccess}
+                readOnly={!formik.values.tbd}
+                required={formik.values.tbd}
+                onChange={(event, newValue) => {
+                  formik.setFieldValue('tbdFrequency', newValue?.key || null)
+                }}
+                error={formik.touched.tbdFrequency && Boolean(formik.errors.tbdFrequency)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomNumberField
+                name='tbdReminder'
+                label={labels.reminder}
+                value={formik?.values?.tbdReminder}
+                maxAccess={maxAccess}
+                readOnly={!formik.values.tbd}
+                required={formik.values.tbd}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('tbdReminder', null)}
+                error={formik.touched.tbdReminder && Boolean(formik.errors.tbdReminder)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomCheckBox
+                name='tbh'
+                value={formik.values?.tbh}
+                onChange={event => formik.setFieldValue('tbh', event.target.checked)}
+                label={labels.trackByHour}
+                maxAccess={maxAccess}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomNumberField
+                name='tbhEvery'
+                label={labels.due}
+                value={formik?.values?.tbhEvery}
+                maxAccess={maxAccess}
+                readOnly={!formik.values.tbh}
+                required={formik.values.tbh}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('tbhEvery', null)}
+                error={formik.touched.tbhEvery && Boolean(formik.errors.tbhEvery)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomNumberField
+                name='tbhReminder'
+                label={labels.reminder}
+                value={formik?.values?.tbhReminder}
+                maxAccess={maxAccess}
+                readOnly={!formik.values.tbh}
+                required={formik.values.tbh}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('tbhReminder', null)}
+                error={formik.touched.tbhReminder && Boolean(formik.errors.tbhReminder)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomNumberField
+                name='expectedLaborHrs'
+                label={labels.labor}
+                value={formik?.values?.expectedLaborHrs}
+                maxAccess={maxAccess}
+                readOnly={!formik.values.tbh}
+                required={formik.values.tbh}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('expectedLaborHrs', null)}
+                error={formik.touched.expectedLaborHrs && Boolean(formik.errors.expectedLaborHrs)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <CustomTextArea
+                name='notes'
+                label={labels.notes}
+                value={formik?.values?.notes}
+                maxAccess={maxAccess}
+                rows={3}
+                onChange={formik.handleChange}
+                onClear={() => formik.setFieldValue('notes', '')}
+                error={formik.touched.header?.notes && Boolean(formik.errors.header?.notes)}
               />
             </Grid>
           </Grid>
