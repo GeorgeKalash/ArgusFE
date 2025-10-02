efe5a3146953aaf8ff2b305db01f6db9537fd972
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2025-10-02 12:48:37 +0300

    fixes first tab active in editMode

--- src/components/Shared/CustomTabs.js
+++ src/components/Shared/CustomTabs.js
@@ -1,11 +1,27 @@
 import { Box, Tab, Tabs } from '@mui/material'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 export const CustomTabs = ({ tabs, activeTab, setActiveTab, maxAccess, name = 'tab' }) => {
   const indexes =
     maxAccess?.record?.controls
       ?.filter(c => c.accessLevel === 4 && c.controlId?.startsWith(`${name}.`))
       .map(c => c.controlId.split('.')[1]) || []
+
+  const _tabs = tabs
+    ?.map((tab, index) => ({
+      ...tab,
+      id: index
+    }))
+    ?.filter((_, index) => !indexes.includes(String(index)))
+
+  const _activeTab = _tabs[activeTab]?.id
+  const _disabledTab = _tabs[activeTab]?.disabled
+
+  useEffect(() => {
+    if (_activeTab >= 0 && !_disabledTab && activeTab != _activeTab) {
+      setActiveTab(_activeTab)
+    }
+  }, [])
 
   return (
     <>
@@ -39,31 +55,25 @@ export const CustomTabs = ({ tabs, activeTab, setActiveTab, maxAccess, name = 't
             }
           }}
         >
-          {tabs
-            ?.map((tab, index) => ({
-              ...tab,
-              id: index
-            }))
-            ?.filter((_, index) => !indexes.includes(String(index)))
-            ?.map((tab, i) => (
-              <Tab
-                key={tab.id}
-                value={tab.id}
-                label={tab.label}
-                disabled={tab?.disabled}
-                sx={{
-                  minHeight: '35px !important',
-                  borderTopLeftRadius: 5,
-                  borderTopRightRadius: 5,
-                  py: '0px !important',
-                  mb: '0px !important',
-                  borderBottom: '0px !important',
-                  mr: '2px !important',
-                  fontWeight: '1.5rem',
-                  px: '5px !important'
-                }}
-              />
-            ))}
+          {_tabs?.map((tab, i) => (
+            <Tab
+              key={tab.id}
+              value={tab.id}
+              label={tab.label}
+              disabled={tab?.disabled}
+              sx={{
+                minHeight: '35px !important',
+                borderTopLeftRadius: 5,
+                borderTopRightRadius: 5,
+                py: '0px !important',
+                mb: '0px !important',
+                borderBottom: '0px !important',
+                mr: '2px !important',
+                fontWeight: '1.5rem',
+                px: '5px !important'
+              }}
+            />
+          ))}
         </Tabs>
       </Box>
     </>
