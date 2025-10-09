5883775516a275bf07a32738ff565904bf2ae09a
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2025-10-09 08:52:03 +0300

    AR-3812

--- src/pages/pu-financial-integrator/index.js
+++ src/pages/pu-financial-integrator/index.js
@@ -23,10 +23,10 @@ const PUFinancialIntegrators = () => {
     })
     formik.setValues({
       ...formik.values,
-      items: res.list.map(({ ...rest }, index) => ({
+      items: res.list.map(({ sameNumber, ...rest }, index) => ({
+        ...rest,
         id: index + 1,
-        sameNumber: false,
-        ...rest
+        sameNumber: sameNumber || false
       }))
     })
   }
