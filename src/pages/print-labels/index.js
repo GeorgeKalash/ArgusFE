fb6be85ac50002c02e4d67259c6ec98dde8cafae
RebeccaFrem02 <155422197+RebeccaFrem02@users.noreply.github.com>
2025-10-20 10:02:37 +0300

    Refactor PrintLabels component: streamline form submission, enhance error handling, and improve field management

--- src/pages/print-labels/index.js
+++ src/pages/print-labels/index.js
@@ -6,7 +6,6 @@ import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
 import GridToolbar from 'src/components/Shared/GridToolbar'
 import { RequestsContext } from 'src/providers/RequestsContext'
 import { SystemRepository } from 'src/repositories/SystemRepository'
-import { ControlContext } from 'src/providers/ControlContext'
 import Form from 'src/components/Shared/Form'
 import * as yup from 'yup'
 import ResourceComboBox from 'src/components/Shared/ResourceComboBox'
@@ -20,13 +19,10 @@ import { SaleRepository } from 'src/repositories/SaleRepository'
 import { DataGrid } from 'src/components/Shared/DataGrid'
 import { InventoryRepository } from 'src/repositories/InventoryRepository'
 import PrintConfirmationDialog from '../iv-items/forms/PrintConfirmationDialog'
-import { useError } from 'src/error'
 
 const PrintLabels = () => {
   const { getRequest } = useContext(RequestsContext)
   const { stack } = useWindow()
-  const { platformLabels } = useContext(ControlContext)
-  const { stack: stackError } = useError()
 
   const { labels, access } = useResourceParams({
     datasetId: ResourceIds.PrintLabels
@@ -50,14 +46,24 @@ const PrintLabels = () => {
         .array()
         .of(
           yup.object().shape({
-            qty: yup.number().required(),
+            qty: yup.number().required().typeError().min(1),
             sku: yup.string().required()
           })
         )
         .required()
     }),
     onSubmit: async () => {
-      ConfirmationPrint()
+      const barcode = await Print()
+      if (barcode)
+        stack({
+          Component: PrintConfirmationDialog,
+          props: {
+            Print,
+            barcode
+          },
+          width: 400,
+          height: 200
+        })
     }
   })
 
@@ -112,55 +118,13 @@ const PrintLabels = () => {
           itemId: null,
           sku: '',
           itemName: '',
-          qty: 0
+          qty: null
         }
       ])
   }
 
-  async function ConfirmationPrint() {
-    const { barcode, error } = await Print()
-    if (!error && barcode)
-      stack({
-        Component: PrintConfirmationDialog,
-        props: {
-          Print,
-          barcode
-        },
-        width: 400,
-        height: 200
-      })
-  }
-
   async function Print() {
     const obj = formik.values
-    if (!obj.labelTemplateId) {
-      stackError({ message: platformLabels.defaultTemplateError })
-
-      return { error: true }
-    }
-
-    if (!obj.currencyId) {
-      stackError({ message: platformLabels.DefaultSalesCurrency })
-
-      return { error: true }
-    }
-
-    const templateRes = await getRequest({
-      extension: SCRepository.LabelTemplate.get,
-      parameters: `_recordId=${obj.labelTemplateId}`
-    })
-
-    if (!templateRes || !templateRes.record.format) {
-      stackError({ message: platformLabels.DefaultTemplateFormat })
-
-      return { error: true }
-    }
-
-    if (!obj.plId) {
-      stackError({ message: platformLabels.DefaultSalesPrice })
-
-      return { error: true }
-    }
 
     const res = await getRequest({
       extension: InventoryRepository.LabelString.md,
@@ -171,21 +135,21 @@ const PrintLabels = () => {
           .join(',')
 
         return `_templateId=${obj.labelTemplateId}&_currencyId=${obj.currencyId}&_printFormat=${encodeURIComponent(
-          templateRes.record.format
+          obj.format
         )}&_plId=${obj.plId}&_items=${itemsParam}`
       })()
     })
 
-    return { barcode: res.record.data }
+    return res?.record?.data
   }
 
   return (
-    <Form onPrint={formik.handleSubmit} isSaved={false} editMode={true}>
+    <Form onPrint={formik.handleSubmit} isSaved={false} editMode={true} maxAccess={access}>
       <VertLayout>
         <Fixed>
           <GridToolbar
             leftSection={
-              <Grid item>
+              <Grid item xs={5}>
                 <Grid container spacing={2}>
                   <Grid item xs={6}>
                     <ResourceComboBox
@@ -198,7 +162,7 @@ const PrintLabels = () => {
                       required
                       maxAccess={access}
                       onChange={(_, newValue) => {
-                        formik.setFieldValue('labelTemplateId', newValue?.recordId || null), clearGrid()
+                        clearGrid(), formik.setFieldValue('labelTemplateId', newValue?.recordId || null)
                       }}
                       error={formik.touched.labelTemplateId && Boolean(formik.errors.labelTemplateId)}
                     />
@@ -214,7 +178,7 @@ const PrintLabels = () => {
                       values={formik.values}
                       maxAccess={access}
                       onChange={(_, newValue) => {
-                        formik.setFieldValue('currencyId', newValue?.recordId || null), clearGrid()
+                        clearGrid(), formik.setFieldValue('currencyId', newValue?.recordId || null)
                       }}
                       error={formik.touched.currencyId && Boolean(formik.errors.currencyId)}
                     />
@@ -229,7 +193,7 @@ const PrintLabels = () => {
                       required
                       values={formik.values}
                       onChange={(_, newValue) => {
-                        formik.setFieldValue('format', newValue?.key || null), clearGrid()
+                        clearGrid(), formik.setFieldValue('format', newValue?.key || null)
                       }}
                       error={formik.touched.format && Boolean(formik.errors.format)}
                       maxAccess={access}
@@ -247,7 +211,7 @@ const PrintLabels = () => {
                       required
                       values={formik.values}
                       onChange={(_, newValue) => {
-                        formik.setFieldValue('plId', newValue?.recordId || null), clearGrid()
+                        clearGrid(), formik.setFieldValue('plId', newValue?.recordId || null)
                       }}
                       error={formik.touched.plId && Boolean(formik.errors.plId)}
                     />
@@ -264,6 +228,10 @@ const PrintLabels = () => {
             value={formik.values.items}
             error={formik.errors.items}
             columns={columns}
+            maxAccess={access}
+            allowDelete={
+              formik.values.currencyId && formik.values.labelTemplateId && formik.values.format && formik.values.plId
+            }
           />
         </Grow>
       </VertLayout>
