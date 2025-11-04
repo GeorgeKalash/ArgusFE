705bb42b0dfbbe1bdefefe323bbce599b0831a93
Imtithal Halalo <halaloimtithal@gmail.com>
2025-11-04 12:23:25 +0200

    3d drawing

--- src/pages/pm-3d-design/forms/ThreeDDesignForm.js
+++ src/pages/pm-3d-design/forms/ThreeDDesignForm.js
@@ -72,6 +72,8 @@ const ThreeDDesignForm = ({ recordId, window }) => {
       notes: '',
       date: new Date(),
       startDate: null,
+      designGroupId: null,
+      designFamilyId: null,
       endDate: null,
       fileReference: '',
       itemGroupId: null,
@@ -300,7 +302,9 @@ const ThreeDDesignForm = ({ recordId, window }) => {
                             productionStandardName: '',
                             collectionId: null,
                             metalPurity: null,
-                            metalId: null
+                            metalId: null,
+                            designGroupId: null,
+                            designFamilyId: null
                           })
                         }
                       }
@@ -396,10 +400,40 @@ const ThreeDDesignForm = ({ recordId, window }) => {
                         productionStandardName: newValue?.productionStandardName || '',
                         collectionId: newValue?.collectionId || null,
                         metalPurity: newValue?.metalPurity || null,
+                        designFamilyId: newValue?.designFamilyId || null,
+                        designGroupId: newValue?.designGroupId || null,
                         metalId: newValue?.metalId || null
                       })
                     }}
                     errorCheck={'sketchId'}
+                    maxAccess={maxAccess}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <ResourceComboBox
+                    endpointId={ManufacturingRepository.DesignFamily.qry}
+                    name='designFamilyId'
+                    label={labels.familyGroup}
+                    valueField='recordId'
+                    displayField='name'
+                    values={formik.values}
+                    readOnly
+                    onChange={(_, newValue) => formik.setFieldValue('designFamilyId', newValue?.recordId || null)}
+                    error={formik?.touched?.designFamilyId && Boolean(formik?.errors?.designFamilyId)}
+                    maxAccess={maxAccess}
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <ResourceComboBox
+                    endpointId={ManufacturingRepository.DesignGroup.qry}
+                    name='designGroupId'
+                    label={labels.designGroup}
+                    valueField='recordId'
+                    displayField='name'
+                    values={formik.values}
+                    readOnly
+                    onChange={(_, newValue) => formik.setFieldValue('designGroupId', newValue?.recordId || null)}
+                    error={formik?.touched?.designGroupId && Boolean(formik?.errors?.designGroupId)}
                     maxAccess={maxAccess}
                   />
                 </Grid>
