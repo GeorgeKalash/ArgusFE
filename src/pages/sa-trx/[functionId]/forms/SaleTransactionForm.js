c692a6d63996bb1794416885bf20c5406d0804ac
omarZrayka23 <150894801+omarZrayka23@users.noreply.github.com>
2025-11-07 13:47:49 +0200

    fixes in barcode

--- src/pages/sa-trx/[functionId]/forms/SaleTransactionForm.js
+++ src/pages/sa-trx/[functionId]/forms/SaleTransactionForm.js
@@ -554,7 +554,8 @@ export default function SaleTransactionForm({
         if (ItemConvertPrice) {
           const itemPhysProp = await getItemPhysProp(ItemConvertPrice?.itemId)
           const itemInfo = await getItem(ItemConvertPrice?.itemId)
-          await barcodeSkuSelection(update, newRow, ItemConvertPrice, itemPhysProp, itemInfo, true)
+          const defaultMu = measurements?.filter(item => item.recordId === itemInfo?.defSaleMUId)?.[0]
+          await barcodeSkuSelection(update, newRow, ItemConvertPrice, itemPhysProp, itemInfo, true, defaultMu)
         } else {
           update({
             barcode: null
