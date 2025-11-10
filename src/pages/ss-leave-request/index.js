fa508c915204f496f0552a9a063ae1a2c4c6fe36
Imtithal Halalo <halaloimtithal@gmail.com>
2025-11-10 13:36:49 +0200

    columns

--- src/pages/ss-leave-request/index.js
+++ src/pages/ss-leave-request/index.js
@@ -20,13 +20,6 @@ const SSLeaveRequest = () => {
   const { stack } = useWindow()
   const { user } = useAuth()
 
-  const parseDotNetDate = dateString => {
-    if (!dateString) return null
-    const match = dateString.match(/\d+/)
-
-    return match ? new Date(parseInt(match[0], 10)) : null
-  }
-
   async function fetchGridData(options = {}) {
     const { _startAt = 0, _pageSize = 50, params = [] } = options
 
@@ -37,20 +30,6 @@ const SSLeaveRequest = () => {
       parameters: `_size=30&_startAt=${_startAt}&_pageSize=${_pageSize}&_params=${params}&_employeeId=${user?.employeeId}&_sortBy=recordId`
     })
 
-    if (response && response?.list) {
-      response.list = response.list.map(item => {
-        const endDate = parseDotNetDate(item?.endDate)
-        const actualReturnDate = parseDotNetDate(item?.actualReturnDate)
-
-        let lateDays = ''
-        if (actualReturnDate && endDate) {
-          const diffInMs = endDate - actualReturnDate
-          lateDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24)) + 1
-        }
-
-        return { ...item, lateDays }
-      })
-    }
 
     return { ...response, _startAt }
   }
@@ -70,21 +49,6 @@ const SSLeaveRequest = () => {
 
   const columns = [
     {
-      field: 'reference',
-      headerName: labels.reference,
-      flex: 1
-    },
-    {
-      field: 'employeeRef',
-      headerName: labels.employeeRef,
-      flex: 1
-    },
-    {
-      field: 'employeeName',
-      headerName: labels.employeeName,
-      flex: 1
-    },
-    {
       field: 'startDate',
       headerName: labels.startDate,
       flex: 1,
@@ -97,40 +61,13 @@ const SSLeaveRequest = () => {
       type: 'date'
     },
     {
-      field: 'actualReturnDate',
-      headerName: labels.actualReturnDate,
-      flex: 1,
-      type: 'date'
-    },
-    {
-      field: 'leaveDays',
-      headerName: labels.days,
-      flex: 1,
-      type: 'number'
-    },
-    {
-      field: 'lateDays',
-      headerName: labels.lateDays,
-      flex: 1
-    },
-    {
-      field: 'ltName',
-      headerName: labels.leaveType,
+      field: 'destination',
+      headerName: labels.destination,
       flex: 1
     },
     {
       field: 'statusName',
       headerName: labels.statusName,
-      flex: 1
-    },
-    {
-      field: 'releaseStatus',
-      headerName: labels.statusRelease,
-      flex: 1
-    },
-    {
-      field: 'wipName',
-      headerName: labels.wipName,
       flex: 1
     }
   ]
