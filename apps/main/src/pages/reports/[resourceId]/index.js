774c9c47803c6d6f165b0ddd253faeeac6689d5f
Nora Bitar <Nora.Bitar@SoftMachine.co>
2025-12-11 15:51:46 +0200

    report/resourceId

--- apps/main/src/pages/reports/[resourceId]/index.js
+++ apps/main/src/pages/reports/[resourceId]/index.js
@@ -1,2 +1,2 @@
 // AUTO-GENERATED WRAPPER – uses code from @argus/module-system/src/pages/reports
-export { default } from '@argus/module-system/src/pages/reports';
+export { default } from '@argus/module-system/src/pages/reports/[resourceId]';
