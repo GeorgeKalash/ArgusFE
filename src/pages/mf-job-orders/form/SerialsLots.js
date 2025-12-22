062f8605bf923cbec3bb3352f92643cc40740477
Imtithal Halalo <halaloimtithal@gmail.com>
2025-12-22 15:40:19 +0200

    review

--- src/pages/mf-job-orders/form/SerialsLots.js
+++ src/pages/mf-job-orders/form/SerialsLots.js
@@ -2,14 +2,12 @@ import { useContext } from 'react'
 import { RequestsContext } from 'src/providers/RequestsContext'
 import { VertLayout } from 'src/components/Shared/Layouts/VertLayout'
 import { Grow } from 'src/components/Shared/Layouts/Grow'
-import { useForm } from 'src/hooks/form'
 import { ResourceIds } from 'src/resources/ResourceIds'
 import { Fixed } from 'src/components/Shared/Layouts/Fixed'
 import { Grid } from '@mui/material'
 import CustomNumberField from 'src/components/Inputs/CustomNumberField'
 import useSetWindow from 'src/hooks/useSetWindow'
 import { ControlContext } from 'src/providers/ControlContext'
-import Form from 'src/components/Shared/Form'
 import Table from 'src/components/Shared/Table'
 import { useResourceQuery } from 'src/hooks/resource'
 
@@ -17,17 +15,7 @@ export default function SerialsLots({ labels, maxAccess, recordId, api, paramete
   const { getRequest } = useContext(RequestsContext)
   const { platformLabels } = useContext(ControlContext)
 
-  const editMode = !!recordId
-
   useSetWindow({ title: platformLabels.serials, window })
-
-  const { formik } = useForm({
-    validateOnChange: true,
-    initialValues: {
-      jobId: recordId,
-      serials: []
-    }
-  })
 
   const columns = [
     {
@@ -87,36 +75,28 @@ export default function SerialsLots({ labels, maxAccess, recordId, api, paramete
   }, 0)
 
   return (
-    <Form
-      resourceId={ResourceIds.MFJobOrders}
-      onSave={formik.handleSubmit}
-      maxAccess={maxAccess}
-      editMode={editMode}
-      isParentWindow={false}
-    >
-      <VertLayout>
-        <Grow>
-          <Table
-            columns={columns}
-            gridData={data}
-            rowId={['recordId']}
-            maxAccess={maxAccess}
-            refetch={refetch}
-            pageSize={50}
-            paginationType='client'
-          />
-        </Grow>
-        <Fixed>
-          <Grid container>
-            <Grid item xs={4} sx={{ pt: 2 }}>
-              <CustomNumberField name='totalWeight' label={labels.totWeight} value={totWeight} readOnly />
-            </Grid>
+    <VertLayout>
+      <Grow>
+        <Table
+          columns={columns}
+          gridData={data}
+          rowId={['recordId']}
+          maxAccess={maxAccess}
+          refetch={refetch}
+          pageSize={50}
+          paginationType='client'
+        />
+      </Grow>
+      <Fixed>
+        <Grid container spacing={2}>
+          <Grid item xs={4} sx={{ p: 2, ml: 2 }}>
+            <CustomNumberField name='totalWeight' label={labels.totWeight} value={totWeight} readOnly />
           </Grid>
-        </Fixed>
-      </VertLayout>
-    </Form>
+        </Grid>
+      </Fixed>
+    </VertLayout>
   )
 }
 
-SerialsLots.width = 500
+SerialsLots.width = 680
 SerialsLots.height = 600
