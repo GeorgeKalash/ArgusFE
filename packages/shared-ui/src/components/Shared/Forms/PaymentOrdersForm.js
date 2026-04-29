ea78b6a4124bced7ebe01ed763ddd5bd63cc2c08
Imtithal Halalo <halaloimtithal@gmail.com>
2026-04-29 11:03:54 +0300

    payment order

--- packages/shared-ui/src/components/Shared/Forms/PaymentOrdersForm.js
+++ packages/shared-ui/src/components/Shared/Forms/PaymentOrdersForm.js
@@ -192,9 +192,11 @@ export default function PaymentOrdersForm({ recordId, window }) {
       parameters: `_recordId=${recordId}`
     })
 
-    formik.setValues({
-      ...res.record,
-      date: formatDateFromApi(res.record.date)
+    formik.resetForm({
+      values: {
+        ...res.record,
+        date: formatDateFromApi(res.record.date)
+      }
     })
   }
 
