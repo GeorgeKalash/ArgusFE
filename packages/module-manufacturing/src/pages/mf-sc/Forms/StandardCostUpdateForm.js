89a5de3c697f0808cfe20b3e8ab7ec7589f04685
Imtithal Halalo <halaloimtithal@gmail.com>
2026-05-04 16:11:03 +0300

    mf-sc number

--- packages/module-manufacturing/src/pages/mf-sc/Forms/StandardCostUpdateForm.js
+++ packages/module-manufacturing/src/pages/mf-sc/Forms/StandardCostUpdateForm.js
@@ -195,7 +195,7 @@ export default function StandardCostUpdateForm({ labels, access, recordId, windo
   ]
 
   const totalAmount = reCal ? (formik.values?.items || []).reduce((sum, item) => {
-      return sum + (parseFloat(item?.value) || 0)
+      return sum + item?.value
     }, 0) 
     : (formik.values?.header?.amount || 0)
 
