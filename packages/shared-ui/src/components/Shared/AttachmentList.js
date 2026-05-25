4b1a6a580faf7664ce91e9fe6322e7c4342be532
Ghinwa-Arab <ghinwa.arab@softmachine.co>
2026-05-25 08:32:24 +0300

    fix add attachment behavior

--- packages/shared-ui/src/components/Shared/AttachmentList.js
+++ packages/shared-ui/src/components/Shared/AttachmentList.js
@@ -18,7 +18,7 @@ import CustomButton from '../Inputs/CustomButton'
 import FolderForm from './FolderForm'
 import { useError } from '@argus/shared-providers/src/providers/error'
 
-const AttachmentList = ({ resourceId, recordId, isActive, window }) => {
+const AttachmentList = ({ resourceId, recordId, isActive = true, window }) => {
   const { getRequest, postRequest } = useContext(RequestsContext)
   const { platformLabels } = useContext(ControlContext)
   const { stack } = useWindow()
