0c6b6a744d1ee300c18169d64c608d97159aea4e
Imtithal Halalo <halaloimtithal@gmail.com>
2026-06-05 10:47:10 +0300

    adding poRef

--- packages/module-manufacturing/src/pages/mf-job-orders/form/JobOrderForm.js
+++ packages/module-manufacturing/src/pages/mf-job-orders/form/JobOrderForm.js
@@ -64,6 +64,7 @@ export default function JobOrderForm({
     recordId: recordId || null,
     dtId: null,
     reference: null,
+    poRef: null,
     date: new Date(),
     plantId: null,
     designId: null,
@@ -1081,10 +1082,19 @@ export default function JobOrderForm({
                       recordId={formik.values.recordId}
                       seqNo={0}
                       customWidth={300}
-                      customHeight={180}
+                      customHeight={160}
                       disabled={isCancelled || isPosted}
                       isAbsolutePath={true}
                       parentImage={parentImage}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <CustomTextField
+                      name='poRef'
+                      label={labels.productionOrder}
+                      value={formik?.values?.poRef}
+                      maxAccess={maxAccess}
+                      readOnly
                     />
                   </Grid>
                   <Grid item xs={12}>
