09266fd5435d2fdeb0da5c3d523cfa2f17bd3b98
imtithalhalaloSM <Imtithal.Halalo@softmachine.co>
2026-07-02 09:11:18 +0300

    fi-credit-limit-hold

--- packages/shared-domain/src/resources/SystemFunction.js
+++ packages/shared-domain/src/resources/SystemFunction.js
@@ -139,6 +139,7 @@ export const SystemFunction = {
   EventOrder: Module.BrokerageTrading * 100 + 3,
   ProductionSummary: Module.Manufacturing * 100 + 20,
   ProductionRequest: Module.Manufacturing * 100 + 19,
+  CreditLimitHold: Module.Financials * 100 + 14,
 }
 
 export const getSystemFunctionModule = functionId => {
